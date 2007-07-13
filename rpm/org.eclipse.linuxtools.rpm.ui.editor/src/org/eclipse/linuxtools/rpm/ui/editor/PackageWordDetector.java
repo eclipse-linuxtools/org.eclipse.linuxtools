47caac1809ae6921ee4c9a95773211fb5aa5384c
Andrew Overholt <overholt@redhat.com>
2007-07-13 21:12:49 +0000

    2007-07-13  Andrew Overholt  <overholt@redhat.com>
    	* src/org/eclipse/linuxtools/rpm/ui/editor/SpecfilePackagesScanner.java:
    	Fix copyright.
    	* src/org/eclipse/linuxtools/rpm/ui/editor/PackageWordDetector.java:
    	Likewise.

--- rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/rpm/ui/editor/PackageWordDetector.java
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/rpm/ui/editor/PackageWordDetector.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2007 Red Hat, Inc.
+ * Copyright (c) 2007 Alphonse Van Assche
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Red Hat - initial API and implementation
+ *    Alphonse Van Assche - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.linuxtools.rpm.ui.editor;
