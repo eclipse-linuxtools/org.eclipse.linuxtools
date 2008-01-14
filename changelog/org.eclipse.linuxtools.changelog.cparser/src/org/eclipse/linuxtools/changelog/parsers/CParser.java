318d85a3a236bd5d422e7a9431cd2dfef0757b73
Jeff Johnston <jjohnstn@redhat.com>
2008-01-14 20:54:19 +0000

    Tagging 2.6.1.

--- changelog/org.eclipse.linuxtools.changelog.cparser/src/org/eclipse/linuxtools/changelog/parsers/CParser.java
+++ changelog/org.eclipse.linuxtools.changelog.cparser/src/org/eclipse/linuxtools/changelog/parsers/CParser.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2006 Phil Muldoon <pkmuldoon@picobot.org>.
+ * Copyright (c) 2008 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Phil Muldoon <pmuldoon@redhat.com> - initial API and implementation, fixes. 
+ *    Phil Muldoon <pmuldoon@redhat.com> - initial API and implementation 
  *******************************************************************************/
 package org.eclipse.linuxtools.changelog.parsers;
 
@@ -19,7 +20,6 @@ import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.linuxtools.changelog.core.IParserChangeLogContrib;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
-
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 /**
@@ -32,21 +32,34 @@ public class CParser implements IParserChangeLogContrib {
 		super();
 	}
 
+	/**
+	 * @see IParserChangeLogContrib#parseCurrentFunction(IEditorPart)
+	 */
+	public String parseCurrentFunction(IEditorPart editor) throws CoreException {
+		
+		if (!(editor instanceof AbstractTextEditor)) {
+			return "";
+		}
+
+		AbstractTextEditor a_editor = (AbstractTextEditor) editor;
+
+		ITextSelection selection = (ITextSelection) (a_editor)
+				.getSelectionProvider().getSelection();
+
+		IEditorInput input = a_editor.getEditorInput();
+
+		return parseCurrentFunction(input, selection.getOffset());
+	}
 
 	/**
 	 * @see IParserChangeLogContrib#parseCurrentFunction(IEditorInput, int)
 	 */
 	public String parseCurrentFunction(IEditorInput input, int offset)
 			throws CoreException {
-
-		String currentElementName;
-
-		// Get the working copy and connect to input.		
 		IWorkingCopyManager manager = CUIPlugin.getDefault()
 				.getWorkingCopyManager();
 		manager.connect(input);
 
-		// Retrieve the C/C++ Element in question.
 		IWorkingCopy workingCopy = manager.getWorkingCopy(input);
 		ICElement method = workingCopy.getElementAtOffset(offset);
 
@@ -56,16 +69,13 @@ public class CParser implements IParserChangeLogContrib {
 		if (method == null)
 			return "";
 
-		// Get the current element name, to test it.
-		currentElementName = method.getElementName();
-		
-		// Element doesn't have a name. Can go no further.
-		if (currentElementName == null) {
+		String currentElementName = "";
+
+		if ((currentElementName = method.getElementName()) == null) {
 			// element doesn't have a name
 			return "";
 		}
 
-		// Get the Element Type to test.
 		int elementType = method.getElementType();
 
 		switch (elementType) {
@@ -76,23 +86,22 @@ public class CParser implements IParserChangeLogContrib {
 		case ICElement.C_MODEL:
 			return "";
 
-		// So it's not a method, field, function, or model. Where are we?			
 		default:
 			ICElement tmpMethodType;
-		if (((tmpMethodType = method.getAncestor(ICElement.C_FUNCTION)) == null)
-				&& ((tmpMethodType = method.getAncestor(ICElement.C_METHOD)) == null)
-				&& ((tmpMethodType = method.getAncestor(ICElement.C_CLASS)) == null)) {
-			return "";
-		} else {
-			// In a class, but not in a method. Return class name instead.
-			method = tmpMethodType;
-			currentElementName = method.getElementName();
-		}
+			if (((tmpMethodType = method.getAncestor(ICElement.C_FUNCTION)) == null)
+					&& ((tmpMethodType = method.getAncestor(ICElement.C_METHOD)) == null)
+					&& ((tmpMethodType = method.getAncestor(ICElement.C_CLASS)) == null)) {
+				return "";
+			} else {
+				// cursor is inside a class, but not method
+				method = tmpMethodType;
+				currentElementName = method.getElementName();
+			}
 
 		}
 
-		// Build all ancestor classes.
-		// Append all ancestor class names to string
+		// now append all ancestor class names to string
+
 		ICElement tmpParent = method.getParent();
 
 		while (tmpParent != null) {
@@ -111,24 +120,4 @@ public class CParser implements IParserChangeLogContrib {
 		return currentElementName;
 	}
 
-	/**
-	 * @see IParserChangeLogContrib#parseCurrentFunction(IEditorPart)
-	 */
-	public String parseCurrentFunction(IEditorPart editor) throws CoreException {
-
-		// Check for correct editor type		
-		if (!(editor instanceof AbstractTextEditor))
-			return "";
-
-		// Get the editor, test selection and input.
-		AbstractTextEditor a_editor = (AbstractTextEditor) editor;
-		ITextSelection selection = (ITextSelection) (a_editor)
-				.getSelectionProvider().getSelection();
-		IEditorInput input = a_editor.getEditorInput();
-
-		// Parse it and return the function.
-		return parseCurrentFunction(input, selection.getOffset());
-	}
-
-	
 }
