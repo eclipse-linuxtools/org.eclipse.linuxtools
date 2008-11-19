941e132ad997536f4bd677f80e3160dfd591601c
Kent Sebastian <ksebasti@redhat.com>
2008-11-19 20:40:29 +0000

    Creates a treeviewer, populates data model with click from an action (only used for debugging atm).

--- oprofile/org.eclipse.linuxtools.oprofile.ui/src/org/eclipse/linuxtools/oprofile/ui/view/OprofileView.java
+++ oprofile/org.eclipse.linuxtools.oprofile.ui/src/org/eclipse/linuxtools/oprofile/ui/view/OprofileView.java
@@ -12,30 +12,51 @@ package org.eclipse.linuxtools.oprofile.ui.view;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.linuxtools.oprofile.core.model.OpModelRoot;
 import org.eclipse.linuxtools.oprofile.ui.OprofileUiPlugin;
+import org.eclipse.linuxtools.oprofile.ui.model.UiModelRoot;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.part.ViewPart;
 
 public class OprofileView extends ViewPart {
+	private TreeViewer _viewer;
 
 	@Override
 	public void createPartControl(Composite parent) {
-		
+		_createTreeViewer(parent);
 		_createActionMenu();
 
 		OprofileUiPlugin.getDefault().setOprofileView(this);
+	}
+	
+	private void _createTreeViewer(Composite parent) {
+		_viewer = new TreeViewer(parent);
+		_viewer.setContentProvider(new OprofileViewContentProvider());
+		_viewer.setLabelProvider(new OprofileViewLabelProvider());
 	}
 
 	private void _createActionMenu() {
 		IMenuManager manager = getViewSite().getActionBars().getMenuManager();
 		manager.add(new Action("Refresh Model"){
 			public void run() {
-				OpModelRoot r = OpModelRoot.getDefault();
-				r.refreshModel();
-				System.out.println(r);
+				Display.getCurrent().asyncExec(new Runnable() {
+					public void run() {
+						OpModelRoot r = OpModelRoot.getDefault();
+						r.refreshModel();
+						System.out.println(r);
+	
+						UiModelRoot r2 = UiModelRoot.getDefault();
+						_viewer.setInput(r2);
+					}					
+				});
 			}
 		});
+	}
+	
+	public void refreshTree() {
+		
 	}
 
 	@Override
