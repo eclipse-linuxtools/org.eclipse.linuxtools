2bd34317fa10bf8434328389cf3ad56cdfa7df8c
Kent Sebastian <ksebasti@redhat.com>
2008-11-19 20:38:27 +0000

    Updated to parse data model.

--- oprofile/org.eclipse.linuxtools.oprofile.ui/src/org/eclipse/linuxtools/oprofile/ui/model/UiModelEvent.java
+++ oprofile/org.eclipse.linuxtools.oprofile.ui/src/org/eclipse/linuxtools/oprofile/ui/model/UiModelEvent.java
@@ -10,6 +10,66 @@
  *******************************************************************************/ 
 package org.eclipse.linuxtools.oprofile.ui.model;
 
-public class UiModelEvent {
+import java.util.ArrayList;
+
+import org.eclipse.linuxtools.oprofile.core.model.OpModelEvent;
+import org.eclipse.swt.graphics.Image;
+
+public class UiModelEvent implements IUiModelElement {
+	private OpModelEvent _event;
+	private UiModelSession _sessions[];
+	
+	public UiModelEvent(OpModelEvent event) {
+		_event = event;
+		refreshModel();
+	}
+
+	/**
+	 * Create the ui sessions from the data model.
+	 */
+	private void refreshModel() {
+		ArrayList<UiModelSession> sessionList = new ArrayList<UiModelSession>();
+
+		//TODO the stuff here that parses them
+		
+		_sessions = new UiModelSession[sessionList.size()];
+		sessionList.toArray(_sessions);
+	}
+	
+	@Override
+	public IUiModelElement[] getChildren() {
+		return _sessions;
+	}
+
+	@Override
+	public Image getLabelImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLabelText() {
+		return toString();
+	}
+
+	/**
+	 * Parent is the ui model root, which isn't displayed 
+	 * in the tree viewer.
+	 */
+	@Override
+	public IUiModelElement getParent() {
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren() {
+		return (_sessions.length == 0 ? false : true);
+	}
+	
+	@Override
+	public String toString() {
+		return _event.getName();
+	}
+
 
 }
