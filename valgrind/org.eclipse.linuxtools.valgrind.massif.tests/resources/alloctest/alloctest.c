d6774829d9d49321f5c066dbae1a6b6b643cfa7d
Elliott Baron <ebaron@fedoraproject.org>
2008-12-15 21:57:33 +0000

    Initial import.

--- valgrind/org.eclipse.linuxtools.valgrind.massif.tests/resources/alloctest/alloctest.c
+++ valgrind/org.eclipse.linuxtools.valgrind.massif.tests/resources/alloctest/alloctest.c
@@ -7,43 +7,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define SIZE sizeof(int) * 10
+
 int main(int argc, char **argv) {
-	long long bytes = 40;
-	long long times = 1;
-	if (argc > 1) {
-		bytes = atoll(argv[1]);
-		if (argc > 2) {
-			times = atoll(argv[2]);
-		}
-	}
-	else {
-		bytes = 40;
-	}
-	
-	while (times-- > 0) {
-		int *foo();
-		void bar(int *);
-		int *ptr1 = (int *)malloc(bytes);
-		int *ptr2 = foo();
-		int *ptr3 = (int *)malloc(bytes);
-		int *ptr4 = foo();
-		int *ptr5 = (int *)malloc(bytes);
-		int *ptr6 = foo();
-	
-		free(ptr1);
-		bar(ptr2);
-		free(ptr3);
-		bar(ptr4);
-		free(ptr5);
-		bar(ptr6);
-	}
+	int *foo();
+	void bar(int *);
+	int *ptr1 = (int *)malloc(SIZE);
+	int *ptr2 = foo();
+	int *ptr3 = (int *)malloc(SIZE);
+	int *ptr4 = foo();
+	int *ptr5 = (int *)malloc(SIZE);
+	int *ptr6 = foo();
+
+	free(ptr1);
+	bar(ptr2);
+	free(ptr3);
+	bar(ptr4);
+	free(ptr5);
+	bar(ptr6);
 	return 0;
 }
 
-int *foo(long long bytes) {
-	return (int *)malloc(bytes);
+int *foo() {
+	return (int *)malloc(SIZE);
 }
 
 void bar(int *ptr) {
-	free(ptr);
+	//free(ptr);
 }
--- valgrind/org.eclipse.linuxtools.valgrind.massif.tests/src/org/eclipse/linuxtools/valgrind/massif/tests/BasicMassifTest.java
+++ valgrind/org.eclipse.linuxtools.valgrind.massif.tests/src/org/eclipse/linuxtools/valgrind/massif/tests/BasicMassifTest.java
@@ -3,6 +3,8 @@ package org.eclipse.linuxtools.valgrind.massif.tests;
 import org.eclipse.cdt.core.model.IBinary;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.linuxtools.valgrind.massif.MassifViewPart;
+import org.eclipse.linuxtools.valgrind.ui.ValgrindUIPlugin;
 
 public class BasicMassifTest extends AbstractMassifTest {
 	
@@ -20,5 +22,8 @@ public class BasicMassifTest extends AbstractMassifTest {
 		IBinary bin = proj.getBinaryContainer().getBinaries()[0];
 		ILaunchConfiguration config = createConfiguration(bin);
 		config.launch(ILaunchManager.PROFILE_MODE, null, true);
+				
+		MassifViewPart view = (MassifViewPart) ValgrindUIPlugin.getDefault().getView().getDynamicView();
+		assertEquals(11, view.getSnapshots().length);
 	}
 }
