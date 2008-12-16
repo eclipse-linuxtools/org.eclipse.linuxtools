917c8ff07d43d873d3bb9d0a050a5fedc4357738
Elliott Baron <ebaron@fedoraproject.org>
2008-12-16 21:39:11 +0000

    Added more massif tests, changed Plugin provider to Eclipse.org

--- valgrind/org.eclipse.linuxtools.valgrind.massif.tests/src/org/eclipse/linuxtools/valgrind/massif/tests/TreeTest.java
+++ valgrind/org.eclipse.linuxtools.valgrind.massif.tests/src/org/eclipse/linuxtools/valgrind/massif/tests/TreeTest.java
@@ -1,5 +1,59 @@
 package org.eclipse.linuxtools.valgrind.massif.tests;
 
-public class TreeTest extends AbstractMassifTest {
+import org.eclipse.cdt.core.model.IBinary;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.linuxtools.valgrind.massif.MassifHeapTreeNode;
+import org.eclipse.linuxtools.valgrind.massif.MassifSnapshot;
+import org.eclipse.linuxtools.valgrind.massif.MassifToolPage;
+import org.eclipse.linuxtools.valgrind.massif.MassifViewPart;
+import org.eclipse.linuxtools.valgrind.ui.ValgrindUIPlugin;
 
+public class TreeTest extends AbstractMassifTest {
+	@Override
+	protected void setUp() throws Exception {
+		proj = createProject("alloctest"); //$NON-NLS-1$
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		deleteProject(proj);
+	}
+	
+	public void testTreeNodes() throws Exception {
+		IBinary bin = proj.getBinaryContainer().getBinaries()[0];
+		ILaunchConfiguration config = createConfiguration(bin);
+		config.launch(ILaunchManager.PROFILE_MODE, null, true);
+				
+		MassifViewPart view = (MassifViewPart) ValgrindUIPlugin.getDefault().getView().getDynamicView();
+		TreeViewer treeViewer = view.getTreeViewer();
+		
+		MassifSnapshot[] snapshots = view.getSnapshots();
+		MassifHeapTreeNode[] nodes = (MassifHeapTreeNode[]) treeViewer.getInput();
+		for (int i = 0; i < nodes.length; i++) {
+			// every odd snapshot should be detailed with --detailed-freq=2
+			// and thus in the tree
+			assertEquals(snapshots[2 * i + 1].getRoot(), nodes[i]);
+		}
+	}
+	
+	public void testNoDetailed() throws Exception {
+		IBinary bin = proj.getBinaryContainer().getBinaries()[0];
+		ILaunchConfiguration config = createConfiguration(bin);
+		ILaunchConfigurationWorkingCopy wc = config.getWorkingCopy();
+		wc.setAttribute(MassifToolPage.ATTR_MASSIF_DETAILEDFREQ, 12); // > #snapshots
+		config = wc.doSave();
+		config.launch(ILaunchManager.PROFILE_MODE, null, true);
+				
+		MassifViewPart view = (MassifViewPart) ValgrindUIPlugin.getDefault().getView().getDynamicView();
+		TreeViewer treeViewer = view.getTreeViewer();
+		
+		MassifHeapTreeNode[] nodes = (MassifHeapTreeNode[]) treeViewer.getInput();
+		
+		assertNotNull(nodes);
+		assertEquals(1, nodes.length); // should always contain peak
+	}
+	
 }
