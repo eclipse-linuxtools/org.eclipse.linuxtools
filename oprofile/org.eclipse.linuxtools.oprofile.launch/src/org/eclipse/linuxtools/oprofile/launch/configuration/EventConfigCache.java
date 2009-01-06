499d706ea08673067fff19337840f0201b3502d7
Kent Sebastian <ksebasti@redhat.com>
2009-01-06 17:01:26 +0000

    added comments

--- oprofile/org.eclipse.linuxtools.oprofile.launch/src/org/eclipse/linuxtools/oprofile/launch/configuration/EventConfigCache.java
+++ oprofile/org.eclipse.linuxtools.oprofile.launch/src/org/eclipse/linuxtools/oprofile/launch/configuration/EventConfigCache.java
@@ -14,7 +14,15 @@ package org.eclipse.linuxtools.oprofile.launch.configuration;
 import java.util.HashMap;
 import org.eclipse.linuxtools.oprofile.core.Oprofile;
 
+/**
+ * Cache class for event configuration. Currently only used to cache the results
+ * of an `opxml check-event ..` run, since the isValid() method in the EventConfigTab
+ * needlessly spawns opxml dozens of times with the same values. Note that multiple
+ * checks of the same value will necessarily return the same result (for a given 
+ * processor) so there is no worry of an invalid cache entry.
+ */
 public class EventConfigCache {
+	//a cache entry for an event check, used as the hashmap key 
 	class CheckEventEntry {
 		public Integer counterNum, eventNum, maskValue;
 		
@@ -45,11 +53,23 @@ public class EventConfigCache {
 	}
 	
 	HashMap<CheckEventEntry, Boolean> validEventCache;
-	
+
+	/**
+	 * Default constructor, creates an empty cache.
+	 */
 	public EventConfigCache() {
 		validEventCache = new HashMap<CheckEventEntry, Boolean>();
 	}
-	
+
+	/**
+	 * Method to be used to check the validity of an event. Will check
+	 * the cache for the result of a previous check of the same values,
+	 * or otherwise will spawn opxml to check properly.
+	 * @param counter counter number
+	 * @param event event number
+	 * @param mask unit mask value
+	 * @return true or false, depending if the event config is valid
+	 */
 	public boolean checkEvent(int counter, int event, int mask) {
 		CheckEventEntry e = new CheckEventEntry(counter, event, mask);
 		Boolean result = null;
