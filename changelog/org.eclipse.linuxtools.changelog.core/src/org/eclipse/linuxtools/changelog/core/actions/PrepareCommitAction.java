fc6c6e112994a8b09b4e3b0f1650ae8ac069c37f
Alexander Kurtakov <akurtako@redhat.com>
2009-01-13 12:38:02 +0000

    2009-01-13  Alexander Kurtakov  <akurtakov@gmail.com>
    	* src/org/eclipse/linuxtools/changelog/core/actions/PrepareCommitAction.java 
    	(loadClipboard): Fix NPE when project is not shared (#260719).

--- changelog/org.eclipse.linuxtools.changelog.core/src/org/eclipse/linuxtools/changelog/core/actions/PrepareCommitAction.java
+++ changelog/org.eclipse.linuxtools.changelog.core/src/org/eclipse/linuxtools/changelog/core/actions/PrepareCommitAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2007 Red Hat Inc. and others.
+ * Copyright (c) 2006-2009 Red Hat Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -159,6 +159,9 @@ public class PrepareCommitAction extends ChangeLogAction {
 		}
 		
 		RepositoryProvider r = RepositoryProvider.getProvider(project);
+		if (r == null) {
+			return; //There is no repository provider for this project, i.e it's not shared.
+		}
 		SyncInfoSet set = new SyncInfoSet();
 		Subscriber s = r.getSubscriber();
 		s.collectOutOfSync(resources, IResource.DEPTH_ZERO, set, monitor);
