f9250a2c382d26e67ddffa20b419dff0dd856bc9
Alexander Kurtakov <akurtako@redhat.com>
2009-01-14 13:47:53 +0000

    2009-01-14  Alexander Kurtakov  <akurtakov@gmail.com>
    	* src/org/eclipse/linuxtools/rpm/ui/editor/SpecfileDocumentProvider.java 
    	(getDocument): Create new partitioner and connect it only if we are going to set it to the document.
    	Should fix #260868.

--- rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/rpm/ui/editor/SpecfileDocumentProvider.java
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/rpm/ui/editor/SpecfileDocumentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 Red Hat, Inc.
+ * Copyright (c) 2007-2009 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,12 +23,13 @@ public class SpecfileDocumentProvider extends TextFileDocumentProvider {
 	public IDocument getDocument(Object element) {
 		document = super.getDocument(element);
 		if (document != null) {
-			SpecfilePartitioner partitioner = new SpecfilePartitioner(
-					new SpecfilePartitionScanner(),
-					SpecfilePartitionScanner.SPEC_PARTITION_TYPES);
-			partitioner.connect(document, false);
-			if (document.getDocumentPartitioner() == null)
+			if (document.getDocumentPartitioner() == null){
+				SpecfilePartitioner partitioner = new SpecfilePartitioner(
+						new SpecfilePartitionScanner(),
+						SpecfilePartitionScanner.SPEC_PARTITION_TYPES);
+				partitioner.connect(document, false);
 				document.setDocumentPartitioner(partitioner);
+			}
 		}
 		return document;
 	}
