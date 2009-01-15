35750f4318719dba42446a3c0e57314d4482148e
Alexander Kurtakov <akurtako@redhat.com>
2009-01-15 11:29:43 +0000

    2009-01-15  Alexander Kurtakov  <akurtako@redhat.com>
    	* .settings/org.eclipse.jdt.core.prefs: Enable javadoc validity warnings.
    	* src/org/eclipse/linuxtools/rpmstubby/StubbyLog.java 
    	(logInfo): Fix javadoc.
    	(logError): Likewise.
    	(log): Likewise.
    	(createStatus): Likewise.

--- rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/rpmstubby/StubbyLog.java
+++ rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/rpmstubby/StubbyLog.java
@@ -22,7 +22,7 @@ public class StubbyLog {
    /**
     * Log the specified information.
     * 
-    * @param message, a human-readable message, localized to the
+    * @param message A human-readable message, localized to the
     *           current locale.
     */
    public static void logInfo(String message) {
@@ -32,7 +32,7 @@ public class StubbyLog {
    /**
     * Log the specified error.
     * 
-    * @param exception, a low-level exception.
+    * @param exception A low-level exception.
     */
    public static void logError(Throwable exception) {
       logError("Unexpected Exception", exception);
@@ -41,9 +41,9 @@ public class StubbyLog {
    /**
     * Log the specified error.
     * 
-    * @param message, a human-readable message, localized to the
+    * @param message A human-readable message, localized to the
     *           current locale.
-    * @param exception, a low-level exception, or <code>null</code>
+    * @param exception A low-level exception, or <code>null</code>
     *           if not applicable.
     */
    public static void logError(String message, Throwable exception) {
@@ -53,18 +53,16 @@ public class StubbyLog {
    /**
     * Log the specified information.
     * 
-    * @param severity, the severity; one of the following:
+    * @param severity The severity; one of the following:
     *           <code>IStatus.OK</code>,
     *           <code>IStatus.ERROR</code>,
     *           <code>IStatus.INFO</code>, or
     *           <code>IStatus.WARNING</code>.
-    * @param pluginId. the unique identifier of the relevant
-    *           plug-in.
-    * @param code, the plug-in-specific status code, or
+    * @param code The plug-in-specific status code, or
     *           <code>OK</code>.
-    * @param message, a human-readable message, localized to the
+    * @param message A human-readable message, localized to the
     *           current locale.
-    * @param exception, a low-level exception, or <code>null</code>
+    * @param exception A low-level exception, or <code>null</code>
     *           if not applicable.
     */
    public static void log(int severity, int code, String message,
@@ -76,18 +74,16 @@ public class StubbyLog {
    /**
     * Create a status object representing the specified information.
     * 
-    * @param severity, the severity; one of the following:
+    * @param severity The severity; one of the following:
     *           <code>IStatus.OK</code>,
     *           <code>IStatus.ERROR</code>,
     *           <code>IStatus.INFO</code>, or
     *           <code>IStatus.WARNING</code>.
-    * @param pluginId, the unique identifier of the relevant
-    *           plug-in.
-    * @param code, the plug-in-specific status code, or
+    * @param code The plug-in-specific status code, or
     *           <code>OK</code>.
-    * @param message, a human-readable message, localized to the
+    * @param message A human-readable message, localized to the
     *           current locale.
-    * @param exception, a low-level exception, or <code>null</code>
+    * @param exception A low-level exception, or <code>null</code>
     *           if not applicable.
     * @return, the status object (not <code>null</code>).
     */
@@ -101,7 +97,7 @@ public class StubbyLog {
    /**
     * Log the given status.
     * 
-    * @param status, the status to log.
+    * @param status The status to log.
     */
    public static void log(IStatus status) {
 	   StubbyPlugin.getDefault().getLog().log(status);
