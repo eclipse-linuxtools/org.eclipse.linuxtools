ac82f265116ecc63d324c8c20c183cecabf9c619
Kent Sebastian <ksebasti@redhat.com>
2009-01-21 16:46:00 +0000

    style changes + some removing some unnecessary code

--- oprofile/org.eclipse.linuxtools.oprofile.core/src/org/eclipse/linuxtools/oprofile/core/opxml/checkevent/CheckEventsProcessor.java
+++ oprofile/org.eclipse.linuxtools.oprofile.core/src/org/eclipse/linuxtools/oprofile/core/opxml/checkevent/CheckEventsProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004 Red Hat, Inc.
+ * Copyright (c) 2004, 2009 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Keith Seitz <keiths@redhat.com> - initial API and implementation
+ *    Kent Sebastian <ksebasti@redhat.com> 
  *******************************************************************************/ 
 package org.eclipse.linuxtools.oprofile.core.opxml.checkevent;
 
@@ -17,41 +18,26 @@ import org.eclipse.linuxtools.oprofile.core.opxml.XMLProcessor;
  * @see org.eclipse.linuxtools.oprofile.core.opxml.OpxmlRunner
  */
 public class CheckEventsProcessor extends XMLProcessor {
-	// A flag of all the possible problems with the 
-	private int _result;
-	
-	/**
-	 * The checked event is valid.
-	 */
-	public static final int EVENT_OK = 0;
-	
-	/**
-	 * The checked event has an invalid event.
-	 */
-	public static final int INVALID_EVENT = 1;
+	public static final int INVALID_UNKNOWN = 0;	//unexpected error
+	public static final int EVENT_OK = 1;			//valid
+	public static final int INVALID_EVENT = 2;		//invalid event number
+	public static final int INVALID_UMASK = 3;		//invalid unit mask value
+	public static final int INVALID_COUNTER = 4;	//invalid event for given counter number
 
-	/**
-	 * The checked event has an invalid unit mask.
-	 */
-	public static final int INVALID_UMASK = 2;
-	
-	/**
-	 * The checked event is for an invalid counter.
-	 */
-	public static final int INVALID_COUNTER = 4;
-	
 	private static final String _RESULT_TAG = "result"; //$NON-NLS-1$
 	private static final String _CHECK_EVENTS_TAG ="check-events"; //$NON-NLS-1$
 
 	private static final String _INVALID_EVENT = "invalid-event"; //$NON-NLS-1$
 	private static final String _INVALID_UMASK = "invalid-um"; //$NON-NLS-1$
 	private static final String _INVALID_COUNTER = "invalid-counter"; //$NON-NLS-1$
-	
+
+	private int _result;
+
 	/**
 	 * @see org.eclipse.linuxtools.oprofile.core.XMLProcessor#reset()
 	 */
 	public void reset(Object callData) {
-		_result = EVENT_OK;
+		_result = INVALID_UNKNOWN;
 	}
 	
 	/**
@@ -59,16 +45,13 @@ public class CheckEventsProcessor extends XMLProcessor {
 	 */
 	public void endElement(String name, Object callData) {
 		if (name.equals(_RESULT_TAG)) {
-			int flag = 0;
 			if (_characters.equals(_INVALID_EVENT)) {
-				flag = INVALID_EVENT;
+				_result = INVALID_EVENT;
 			} else if (_characters.equals(_INVALID_UMASK)) {
-				flag = INVALID_UMASK;
+				_result = INVALID_UMASK;
 			} else if (_characters.equals(_INVALID_COUNTER)) {
-				flag = INVALID_COUNTER;
+				_result = INVALID_COUNTER;
 			}
-			
-			_result |= flag;
 		} else if (name.equals(_CHECK_EVENTS_TAG)) {
 			int[] result = (int[]) callData;
 			result[0] = _result;
