681d2983745fc5eea1b70af4ad008d198f1dee3c
Alexander Kurtakov <akurtako@redhat.com>
2009-02-19 20:10:25 +0000

    Add Wind River copyright.

--- rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/rpm/ui/editor/preferences/MacroProposalsPreferencePage.java
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/rpm/ui/editor/preferences/MacroProposalsPreferencePage.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2007 Alphonse Van Assche.
+ * Copyright (c) 2008, 2009 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +8,7 @@
  *
  * Contributors:
  *    Alphonse Van Assche - initial API and implementation
+ *    Wind River Systems, Inc - compile fixes
  *******************************************************************************/
 
 package org.eclipse.linuxtools.rpm.ui.editor.preferences;
