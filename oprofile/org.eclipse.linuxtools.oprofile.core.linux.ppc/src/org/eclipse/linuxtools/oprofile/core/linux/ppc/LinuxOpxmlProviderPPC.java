8fac2d6177885df2d4fbd02cde5fae9f54fe4c86
Kent Sebastian <ksebasti@redhat.com>
2009-03-17 15:57:40 +0000

    Initial import.

---
+++ oprofile/org.eclipse.linuxtools.oprofile.core.linux.ppc/src/org/eclipse/linuxtools/oprofile/core/linux/ppc/LinuxOpxmlProviderPPC.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Kent Sebastian <ksebasti@redhat.com> - initial API and implementation
+ *******************************************************************************/ 
+package org.eclipse.linuxtools.oprofile.core.linux.ppc;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.linuxtools.oprofile.core.OpxmlException;
+import org.eclipse.linuxtools.oprofile.core.linux.LinuxOpxmlProvider;
+
+public class LinuxOpxmlProviderPPC extends LinuxOpxmlProvider {
+	private static final String PLUGIN_ID = "org.eclipse.linuxtools.oprofile.core.linux.ppc"; //$NON-NLS-1$
+	private static final String OPXML_BINARY_NAME = "opxml"; //$NON-NLS-1$
+	private static final String OPXML_BINARY_PATH = "$os$"; //$NON-NLS-1$
+
+	public LinuxOpxmlProviderPPC() throws OpxmlException {
+		super();
+	}
+
+	@Override
+	public String _getOpxmlPath() {
+		String opxmlPath = null;
+		URL opxmlBinaryUrl = FileLocator.find(Platform.getBundle(PLUGIN_ID), new Path(OPXML_BINARY_PATH + Path.SEPARATOR + OPXML_BINARY_NAME), null); 
+		
+		if (opxmlBinaryUrl != null) {
+			try {
+				opxmlPath = FileLocator.toFileURL(opxmlBinaryUrl).getPath();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return opxmlPath;
+	}
+}
