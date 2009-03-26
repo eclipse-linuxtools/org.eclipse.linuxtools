5f642cfa66c317af67a088ee409c94f292b15f78
Alexander Kurtakov <akurtako@redhat.com>
2009-03-26 13:26:28 +0000

    2009-03-26  Alexander Kurtakov  <akurtako@redhat.com>
    	* .settings/org.eclipse.jdt.ui.prefs: New file.
    	* src/org/eclipse/linuxtools/rpm/ui/editor/tests/actions/ActionsAllTests.java (suite): Add one more test.
    	* src/org/eclipse/linuxtools/rpm/ui/editor/tests/actions/SpecfileEditorShowOutlineActionDelegateTest.java: New file.

--- rpm/org.eclipse.linuxtools.rpm.ui.editor.tests/src/org/eclipse/linuxtools/rpm/ui/editor/tests/actions/ActionsAllTests.java
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor.tests/src/org/eclipse/linuxtools/rpm/ui/editor/tests/actions/ActionsAllTests.java
@@ -18,6 +18,7 @@ public class ActionsAllTests {
 				"Test for org.eclipse.linuxtools.rpm.ui.editor.tests.actions");
 		suite.addTestSuite(SpecfileChangelogFormatterTest.class);
 		suite.addTestSuite(SpecfileChangelogParserTest.class);
+		suite.addTestSuite(SpecfileEditorShowOutlineActionDelegateTest.class);
 		return suite;
 	}
 }
