170627ae92ece5d6e8b6f3b917d8a95367744ab4
Alexander Kurtakov <akurtako@redhat.com>
2009-06-25 11:42:02 +0000

    2009-06-25  Alexander Kurtakov  <akurtako@redhat.com>
    	* plugin.properties: Removed.
    	* build.properties: Fix copyright header.
    	* plugin.xml: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/internal/Messages.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/internal/rpm_strings.properties: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/internal/RPMConfiguration.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/internal/RPMProject.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/internal/SourceRPM.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/IRPMConfiguration.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/IRPMConstants.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/IRPMProject.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/ISourceRPM.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/RPMCorePlugin.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/RPMProjectFactory.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/RPMProjectNature.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/utils/Diff.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/utils/internal/Command.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/utils/internal/ShellScript.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/utils/internal/StreamReaderThread.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/utils/RPM.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/utils/RPMBuild.java: Likewise.
    	* src/org/eclipse/linuxtools/rpm/core/utils/RPMQuery.java: Likewise.

--- rpm/org.eclipse.linuxtools.rpm.core/src/org/eclipse/linuxtools/rpm/core/internal/SourceRPM.java
+++ rpm/org.eclipse.linuxtools.rpm.core/src/org/eclipse/linuxtools/rpm/core/internal/SourceRPM.java
@@ -1,9 +1,13 @@
-/*
- * (c) 2005 Red Hat, Inc.
+/*******************************************************************************
+ * Copyright (c) 2005, 2009 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * This program is open source software licensed under the 
- * Eclipse Public License ver. 1
- */
+ * Contributors:
+ *     Red Hat - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.linuxtools.rpm.core.internal;
 
 import org.eclipse.core.resources.IFile;
