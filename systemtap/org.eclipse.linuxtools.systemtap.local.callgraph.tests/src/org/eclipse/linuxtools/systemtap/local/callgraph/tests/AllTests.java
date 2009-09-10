5a828a4715d447b723c3279365e4c2fc7514b499
Charley Wang <chwang@redhat.com>
2009-09-10 16:14:41 +0000

    Added AllTests suite for testing purposes. Still need to double check which non-java files from .tests need to be migrated over to make all tests run properly.

--- systemtap/org.eclipse.linuxtools.systemtap.local.callgraph.tests/src/org/eclipse/linuxtools/systemtap/local/callgraph/tests/AllTests.java
+++ systemtap/org.eclipse.linuxtools.systemtap.local.callgraph.tests/src/org/eclipse/linuxtools/systemtap/local/callgraph/tests/AllTests.java
@@ -1,17 +1,4 @@
-/*******************************************************************************
- * Copyright (c) 2009 Red Hat, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     Red Hat - initial API and implementation
- *******************************************************************************/
-
 package org.eclipse.linuxtools.systemtap.local.callgraph.tests;
-
-import org.eclipse.linuxtools.systemtap.local.core.SystemTapUIErrorMessages;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -19,13 +6,12 @@ import junit.framework.TestSuite;
 public class AllTests {
 
 	public static Test suite() {
-		SystemTapUIErrorMessages.setActive(false);
 		TestSuite suite = new TestSuite(
 				"Test for org.eclipse.linuxtools.systemtap.local.callgraph.tests");
 		//$JUnit-BEGIN$
-		suite.addTestSuite(StapGraphParserTest.class);
-		suite.addTestSuite(SystemTapGraphViewTest.class);
 		suite.addTestSuite(SystemTapGraphTest.class);
+		suite.addTestSuite(SystemTapGraphViewTest.class);
+		suite.addTestSuite(StapGraphParserTest.class);
 		//$JUnit-END$
 		return suite;
 	}
