05d557cf7af8f08fbfd7cf8c4aba232312e09829
Charley Wang <chwang@redhat.com>
2009-09-17 19:19:08 +0000

    Removed unused imports in LaunchStapGraph

--- systemtap/org.eclipse.linuxtools.systemtap.local.launch/src/org/eclipse/linuxtools/systemtap/local/launch/LaunchStapGraph.java
+++ systemtap/org.eclipse.linuxtools.systemtap.local.launch/src/org/eclipse/linuxtools/systemtap/local/launch/LaunchStapGraph.java
@@ -12,10 +12,8 @@
 package org.eclipse.linuxtools.systemtap.local.launch;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
