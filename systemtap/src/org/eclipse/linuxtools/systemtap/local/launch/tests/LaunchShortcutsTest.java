a7b0f08d6aee47cdc6c85192477d1c8e3c6f6707
Charley Wang <chwang@redhat.com>
2009-09-21 17:54:22 +0000

    Added print hooks for LaunchShortcutsTest, reorganized MANIFEST -- see if this works on rgrunbers' version

--- systemtap/src/org/eclipse/linuxtools/systemtap/local/launch/tests/LaunchShortcutsTest.java
+++ systemtap/src/org/eclipse/linuxtools/systemtap/local/launch/tests/LaunchShortcutsTest.java
@@ -49,24 +49,27 @@ public class LaunchShortcutsTest extends AbstractStapTest{
 	
 	public void testLaunchCallGraph() {
 		try {
+			System.out.println("Launching LaunchShortcutsTest");
 			LaunchStapGraph launch = new LaunchStapGraph();
 			
 			//Need to set funcs, scriptPath, projectName, partialScriptPath
 			launch.setProjectName(testName);
 			launch.writeFunctionListToScript(null);
+			System.out.println("Writing function list");
 				
 			String scriptPath = getPathToFiles(testName).toOSString() + testName + "Script.gen-stp";
 			launch.setScriptPath(scriptPath);
 			System.out.println(scriptPath);
 			launch.setPartialScriptPath(PluginConstants.PLUGIN_LOCATION + "parse_function_partial.stp");
 			launch.generateScript();
+			System.out.println("Generated Script");
 			
 			checkScript(launch);
 			
 			ILaunchConfiguration config = createConfiguration(proj.getProject());
 			
 			
-			
+			System.out.println("Launching");
 			doLaunch(config, "testLaunchCallGraph"); //$NON-NLS-1$
 			
 			String outputPath =config.getAttribute(LaunchConfigurationConstants.OUTPUT_PATH, "COULDN'T FIND"); 
