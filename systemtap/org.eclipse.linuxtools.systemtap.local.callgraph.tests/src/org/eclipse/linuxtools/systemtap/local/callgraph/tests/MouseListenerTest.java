51492e82353679abfe025dc3fa824b1813e8407b
Charley Wang <chwang@redhat.com>
2009-09-23 13:21:51 +0000

    Added quick checks in MouseListenerTest for mouseUp and mouseDown events -- since there is no way to verify that anything 'really worked,' we're essentially just testing for a lack of failure. Modified Manifest to include cdt.core.model, because we need it.

--- systemtap/org.eclipse.linuxtools.systemtap.local.callgraph.tests/src/org/eclipse/linuxtools/systemtap/local/callgraph/tests/MouseListenerTest.java
+++ systemtap/org.eclipse.linuxtools.systemtap.local.callgraph.tests/src/org/eclipse/linuxtools/systemtap/local/callgraph/tests/MouseListenerTest.java
@@ -10,10 +10,8 @@ import org.eclipse.linuxtools.systemtap.local.callgraph.StapGraphParser;
 import org.eclipse.linuxtools.systemtap.local.callgraph.graphlisteners.StapGraphKeyListener;
 import org.eclipse.linuxtools.systemtap.local.callgraph.graphlisteners.StapGraphMouseListener;
 import org.eclipse.linuxtools.systemtap.local.callgraph.graphlisteners.StapGraphMouseWheelListener;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.zest.core.widgets.GraphItem;
+
 
 public class MouseListenerTest extends TestCase{
 
@@ -33,13 +31,18 @@ public class MouseListenerTest extends TestCase{
 		StapGraphMouseWheelListener mwListener = CallgraphView.getGraph().getMouseWheelListener();
 		
 		StapGraph g = (StapGraph) CallgraphView.getGraph();
-		
+		g.setProject(parse.project);
 		
 		
 		GraphItem[] nodes = {g.getNode(g.getFirstUsefulNode())}; 
 		g.setSelection(nodes);
 		
+		
 		System.out.println(mListener.controlDoubleClick());
+		mListener.mouseDownEvent(0, 0);
+		g.draw(StapGraph.CONSTANT_DRAWMODE_TREE, StapGraph.CONSTANT_ANIMATION_FASTEST,
+				g.getFirstUsefulNode());
+		mListener.mouseUpEvent();
 		
 		System.out.println("TEST");
 		
