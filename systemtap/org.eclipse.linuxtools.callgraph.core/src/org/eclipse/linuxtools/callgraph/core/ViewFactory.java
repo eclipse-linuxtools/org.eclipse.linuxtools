4485adf714f6ef178f631a265d0366d074e7edf7
Roland Grunberg <rgrunber@redhat.com>
2009-10-07 19:34:57 +0000

    Create : ViewFactory class for easier launching/manipulation of eclipse views.

---
+++ systemtap/org.eclipse.linuxtools.callgraph.core/src/org/eclipse/linuxtools/callgraph/core/ViewFactory.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Red Hat - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.callgraph.core;
+
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+
+/**
+ * Class to create / manipulate Views
+ */
+public class ViewFactory {
+	
+	/**
+	 * Create a view of type designated by the viewID argument
+	 * @param viewID : A string corresonding to a type of View
+	 * @return : The view object that corresponds to the viewID
+	 */
+	public static IViewPart createView(String viewID) {
+		try {
+			IViewPart view = PlatformUI.getWorkbench()
+					.getActiveWorkbenchWindow().getActivePage()
+					.showView(viewID);
+			return view;
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
