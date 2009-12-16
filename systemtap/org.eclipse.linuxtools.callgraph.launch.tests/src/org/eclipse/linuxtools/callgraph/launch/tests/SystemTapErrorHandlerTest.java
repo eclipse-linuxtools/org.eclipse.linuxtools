ac0f4153e2cd89b6a1d8ef68f28434cec0710154
Roland Grunberg <rgrunber@redhat.com>
2009-12-16 20:08:18 +0000

    2009-12-16  rgrunber  <rgrunber@redhat.com>
    	* src/org/eclipse/linuxtools/callgraph/launch/tests/SystemTapErrorHandlerTest.java:
    		New File. Basic class to test the error handling.

---
+++ systemtap/org.eclipse.linuxtools.callgraph.launch.tests/src/org/eclipse/linuxtools/callgraph/launch/tests/SystemTapErrorHandlerTest.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.callgraph.launch.tests;
+
+import junit.framework.TestCase;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.linuxtools.callgraph.core.SystemTapErrorHandler;
+
+public class SystemTapErrorHandlerTest extends TestCase {
+
+	SystemTapErrorHandler errHandler;
+	String errorString;
+
+	
+	public void testErrorNotRecognized(){
+
+		errorString = "This error will not be caught \n" +
+				"Not even this one \n" +
+				"Unrecognized \n" +
+				"Not found \n" +
+				"Error";
+		
+		errHandler = new SystemTapErrorHandler();
+		errHandler.handle(new NullProgressMonitor(), errorString);
+		
+		assertTrue(!errHandler.isErrorRecognized());
+	}
+	
+		
+	public void testErrorRecognized(){
+
+		errorString = "As long as the word stapdev or stapusr is here, error is recognized";
+		
+		errHandler = new SystemTapErrorHandler();
+		errHandler.handle(new NullProgressMonitor(), errorString);
+		
+		assertTrue(errHandler.isErrorRecognized());
+	}
+	
+	
+	public void testUserGroupError(){
+		
+		errorString = "ERROR: You are trying to run systemtap as a normal user.\n" +
+			"You should either be root, or be part of either " +
+			"group \"stapdev\" or group \"stapusr.\n";
+		
+		errHandler = new SystemTapErrorHandler();
+		errHandler.handle(new NullProgressMonitor(), errorString);
+		
+		assertTrue(errHandler.isErrorRecognized());
+		assertTrue(errHandler.getErrorMessage().contains("Please add yourself to the 'stapdev' or 'stapusr' group in order to run stap."));
+	}
+	
+	
+	public void testDebugInfoError(){
+		
+		errorString = "missing [architecture] kernel/module debuginfo under '[kernel-build-tree]'";
+		
+		errHandler = new SystemTapErrorHandler();
+		errHandler.handle(new NullProgressMonitor(), errorString);
+		
+		assertTrue(errHandler.isErrorRecognized());
+		assertTrue(errHandler.getErrorMessage().contains("No debuginfo could be found. Make sure you have yum-utils installed, and run debuginfo-install kernel as root."));
+	}
+	
+	
+	public void testUprobesError(){
+		
+		errorString = "SystemTap's version of uprobes is out of date. As root, run or a member of the stapdev group run \"make -C /usr/local/share/systemtap/runtime/uprobes\".";
+		
+		errHandler = new SystemTapErrorHandler();
+		errHandler.handle(new NullProgressMonitor(), errorString);
+		
+		assertTrue(errHandler.isErrorRecognized());
+		assertTrue(errHandler.getErrorMessage().contains("Probes were not compiled on your system. Try 'make -C /usr/local/share/systemtap/runtime/uprobes' as root"));
+		
+	}
+
+}
