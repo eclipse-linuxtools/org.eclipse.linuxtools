5d5f314aca3a1c199837c169fa23ba78c3a3b2a7
Roland Grunberg <rgrunber@redhat.com>
2009-12-22 15:55:10 +0000

    Make necessary changes for switching from JUnit 3 to JUnit 4.

---
+++ oprofile/org.eclipse.linuxtools.oprofile.launch.tests/src/org/eclipse/linuxtools/oprofile/launch/tests/AllLaunchTests.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Kent Sebastian <ksebasti@redhat.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.oprofile.launch.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllLaunchTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.eclipse.linuxtools.oprofile.launch.tests"); //$NON-NLS-1$
+		
+		if (LaunchTestsPlugin.RUN_OPROFILE) {
+			suite.addTestSuite(TestLaunching.class);
+			suite.addTestSuite(TestManualLaunching.class);
+			suite.addTestSuite(TestSetup.class);
+		}
+		
+		suite.addTestSuite(TestDummy.class);
+		
+		return suite;
+	}
+
+}
