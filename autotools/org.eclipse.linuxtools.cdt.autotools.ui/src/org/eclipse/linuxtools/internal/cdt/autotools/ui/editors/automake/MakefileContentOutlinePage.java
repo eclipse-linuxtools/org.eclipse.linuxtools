f9dc802152296861b7003779cffd99b37b719094
Jeff Johnston <jjohnstn@redhat.com>
2010-07-19 18:29:56 +0000

    2010-07-19  Jeff Johnston  <jjohnstn@redhat.com>
    	Bug #314334
    	* src/org/eclipse/linuxtools/internal/cdt/autotools/ui/editors/automake/AddBuildTargetAction.java: Removed.
    	* src/org/eclipse/linuxtools/internal/cdt/autotools/ui/editors/automake/MakefileContentOutlinePage.java (MakefileContentOutlinePage):
    	Remove reference to AddBuildTargetAction which isn't used. 
    	(contextMenuAboutToShow): Ditto.

--- autotools/org.eclipse.linuxtools.cdt.autotools.ui/src/org/eclipse/linuxtools/internal/cdt/autotools/ui/editors/automake/MakefileContentOutlinePage.java
+++ autotools/org.eclipse.linuxtools.cdt.autotools.ui/src/org/eclipse/linuxtools/internal/cdt/autotools/ui/editors/automake/MakefileContentOutlinePage.java
@@ -208,13 +208,11 @@ public class MakefileContentOutlinePage extends ContentOutlinePage implements IC
 
 	protected MakefileEditor fEditor;
 	protected Object fInput;
-	protected AddBuildTargetAction fAddBuildTargetAction;
 	protected OpenIncludeAction fOpenIncludeAction;
 
 	public MakefileContentOutlinePage(MakefileEditor editor) {
 		super();
 		fEditor = editor;
-		fAddBuildTargetAction = new AddBuildTargetAction(this);
 		fOpenIncludeAction = new OpenIncludeAction(this);
 	}
 
@@ -265,9 +263,6 @@ public class MakefileContentOutlinePage extends ContentOutlinePage implements IC
 	protected void contextMenuAboutToShow(IMenuManager menu) {
 		if (OpenIncludeAction.canActionBeAdded(getSelection())) {
 			menu.add(fOpenIncludeAction);
-		}
-		if (fAddBuildTargetAction.canActionBeAdded(getSelection())) {
-			menu.add(fAddBuildTargetAction);
 		}
 		menu.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 		menu.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS+"-end"));//$NON-NLS-1$
