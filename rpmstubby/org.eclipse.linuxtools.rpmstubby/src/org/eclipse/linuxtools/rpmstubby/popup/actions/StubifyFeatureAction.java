29b26c05c594be4255ede9fa9be6ed19bb249a30
Alexander Kurtakov <akurtako@redhat.com>
2010-08-31 11:26:48 +0000

    2010-08-31  Alexander Kurtakov  <akurtako@redhat.com>
    	* META-INF/MANIFEST.MF: Move to using commands framework.
    	* plugin.xml: Likewise.
    	* src/org/eclipse/linuxtools/rpmstubby/popup/actions/StubifyAction.java: Make actions command handlers.
    	* src/org/eclipse/linuxtools/rpmstubby/popup/actions/StubifyFeatureAction.java (execute): Likewise.
    	* src/org/eclipse/linuxtools/rpmstubby/popup/actions/StubifyPomAction.java (execute): Likewise.

--- rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/rpmstubby/popup/actions/StubifyFeatureAction.java
+++ rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/rpmstubby/popup/actions/StubifyFeatureAction.java
@@ -2,16 +2,21 @@ package org.eclipse.linuxtools.rpmstubby.popup.actions;
 
 import java.util.Iterator;
 
+import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.linuxtools.rpmstubby.SpecfileWriter;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Action handling stybifying RPM spec file from a Eclipse feature.xml file.
- *
+ * 
  */
 public class StubifyFeatureAction extends StubifyAction {
 
@@ -33,5 +38,42 @@ public class StubifyFeatureAction extends StubifyAction {
 		}
 		SpecfileWriter specfileWriter = new SpecfileWriter();
 		specfileWriter.write(featureFile);
+	}
+
+	public Object execute(ExecutionEvent event) {
+
+		IFile featureFile = null;
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		if (selection instanceof IStructuredSelection) {
+			for (Object element : ((IStructuredSelection) selection).toList()) {
+				if (element instanceof IFile) {
+					featureFile = (IFile) element;
+				} else if (element instanceof IAdaptable) {
+					featureFile = (IFile) ((IAdaptable) element)
+							.getAdapter(IFile.class);
+				}
+				if (featureFile != null) {
+					SpecfileWriter specfileWriter = new SpecfileWriter();
+					specfileWriter.write(featureFile);
+				}
+			}
+			// StructuredSelection structuredSelection = (StructuredSelection)
+			// selection;
+			// for (Iterator<?> selectionIter = structuredSelection.iterator();
+			// selectionIter
+			// .hasNext();) {
+			// Object selected = selectionIter.next();
+			// if (selected instanceof IProject) {
+			// featureFile = ((IProject) selected).getFile(new Path(
+			// "/feature.xml"));
+			// } else if (selected instanceof IFile) {
+			// featureFile = (IFile) selected;
+			// } else {
+			// // FIXME: error
+			// }
+			// }
+
+		}
+		return null;
 	}
 }
