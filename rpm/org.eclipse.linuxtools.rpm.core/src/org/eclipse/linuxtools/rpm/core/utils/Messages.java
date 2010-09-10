f611cb489dde040715e8264d39f2dbf7c21be57a
Alexander Kurtakov <akurtako@redhat.com>
2010-09-10 11:21:30 +0000

    2010-09-10  Alexander Kurtakov  <akurtako@redhat.com>     Add a utility job for downloading file. 	* src/org/eclipse/linuxtools/rpm/core/utils/DownloadJob.java: New file. 	* src/org/eclipse/linuxtools/rpm/core/utils/Messages.java: New file. 	* src/org/eclipse/linuxtools/rpm/core/utils/messages.properties: New file.

---
+++ rpm/org.eclipse.linuxtools.rpm.core/src/org/eclipse/linuxtools/rpm/core/utils/Messages.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Kurtakov - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.rpm.core.utils;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Messages for this package.
+ */
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.linuxtools.rpm.core.utils.messages"; //$NON-NLS-1$
+	/** Download + name  message	 */
+	public static String DownloadJob_0;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
