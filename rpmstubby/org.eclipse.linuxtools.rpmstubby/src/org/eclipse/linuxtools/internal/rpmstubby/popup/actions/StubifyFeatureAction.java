1a45eb1d805ad282005bc7d8c85fc72a184be9f6
Alexander Kurtakov <akurtako@redhat.com>
2011-08-09 09:47:53 +0300

    Remove forgotten things from the old action based implementation. They are no longer needed now that we're using the command api.

--- rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/internal/rpmstubby/popup/actions/StubifyFeatureAction.java
+++ rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/internal/rpmstubby/popup/actions/StubifyFeatureAction.java
@@ -1,16 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Kurtakov - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.linuxtools.internal.rpmstubby.popup.actions;
 
-import java.util.Iterator;
-
+import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.linuxtools.internal.rpmstubby.SpecfileWriter;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -18,27 +24,7 @@ import org.eclipse.ui.handlers.HandlerUtil;
  * Action handling stybifying RPM spec file from a Eclipse feature.xml file.
  * 
  */
-public class StubifyFeatureAction extends StubifyAction {
-
-	@Override
-	public void run(IAction action) {
-		IFile featureFile = null;
-		StructuredSelection structuredSelection = (StructuredSelection) selection;
-		for (Iterator<?> selectionIter = structuredSelection.iterator(); selectionIter
-				.hasNext();) {
-			Object selected = selectionIter.next();
-			if (selected instanceof IProject) {
-				featureFile = ((IProject) selected).getFile(new Path(
-						"/feature.xml"));
-			} else if (selected instanceof IFile) {
-				featureFile = (IFile) selected;
-			} else {
-				// FIXME: error
-			}
-		}
-		SpecfileWriter specfileWriter = new SpecfileWriter();
-		specfileWriter.write(featureFile);
-	}
+public class StubifyFeatureAction extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) {
 
@@ -57,22 +43,6 @@ public class StubifyFeatureAction extends StubifyAction {
 					specfileWriter.write(featureFile);
 				}
 			}
-			// StructuredSelection structuredSelection = (StructuredSelection)
-			// selection;
-			// for (Iterator<?> selectionIter = structuredSelection.iterator();
-			// selectionIter
-			// .hasNext();) {
-			// Object selected = selectionIter.next();
-			// if (selected instanceof IProject) {
-			// featureFile = ((IProject) selected).getFile(new Path(
-			// "/feature.xml"));
-			// } else if (selected instanceof IFile) {
-			// featureFile = (IFile) selected;
-			// } else {
-			// // FIXME: error
-			// }
-			// }
-
 		}
 		return null;
 	}
--- rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/internal/rpmstubby/popup/actions/StubifyPomAction.java
+++ rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/internal/rpmstubby/popup/actions/StubifyPomAction.java
@@ -9,16 +9,22 @@
  *     Red Hat Incorporated - initial API and implementation
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.rpmstubby.popup.actions;
-
-import java.util.Iterator;
-
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Kurtakov - initial API and implementation
+ *******************************************************************************/
+import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.linuxtools.internal.rpmstubby.SpecfilePomWriter;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -26,57 +32,24 @@ import org.eclipse.ui.handlers.HandlerUtil;
  * Action handling stybifying RPM spec file from a Maven pom.xml file.
  * 
  */
-public class StubifyPomAction extends StubifyAction {
-
-	@Override
-	public void run(IAction action) {
-		IFile pomFile = null;
-		StructuredSelection structuredSelection = (StructuredSelection) selection;
-		for (Iterator<?> selectionIter = structuredSelection.iterator(); selectionIter
-				.hasNext();) {
-			Object selected = selectionIter.next();
-			if (selected instanceof IFile) {
-				pomFile = (IFile) selected;
-			} else {
-				// FIXME: error
-			}
-		}
-		SpecfilePomWriter specfileWriter = new SpecfilePomWriter();
-		specfileWriter.write(pomFile);
-	}
+public class StubifyPomAction extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) {
-		IFile featureFile = null;
+		IFile pomFile = null;
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
 		if (selection instanceof IStructuredSelection) {
 			for (Object element : ((IStructuredSelection) selection).toList()) {
 				if (element instanceof IFile) {
-					featureFile = (IFile) element;
+					pomFile = (IFile) element;
 				} else if (element instanceof IAdaptable) {
-					featureFile = (IFile) ((IAdaptable) element)
+					pomFile = (IFile) ((IAdaptable) element)
 							.getAdapter(IFile.class);
 				}
-				if (featureFile != null) {
+				if (pomFile != null) {
 					SpecfilePomWriter specfileWriter = new SpecfilePomWriter();
-					specfileWriter.write(featureFile);
+					specfileWriter.write(pomFile);
 				}
 			}
-			// StructuredSelection structuredSelection = (StructuredSelection)
-			// selection;
-			// for (Iterator<?> selectionIter = structuredSelection.iterator();
-			// selectionIter
-			// .hasNext();) {
-			// Object selected = selectionIter.next();
-			// if (selected instanceof IProject) {
-			// featureFile = ((IProject) selected).getFile(new Path(
-			// "/feature.xml"));
-			// } else if (selected instanceof IFile) {
-			// featureFile = (IFile) selected;
-			// } else {
-			// // FIXME: error
-			// }
-			// }
-
 		}
 		return null;
 	}
