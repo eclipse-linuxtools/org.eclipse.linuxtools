9c21f751ae0b6a1347011d263f2fc231dbed84b5
Francois Chouinard <fchouinard@gmail.com>
2011-08-26 11:38:28 -0400

    Fix for Bug340341

--- lttng/org.eclipse.linuxtools.lttng.ui/src/org/eclipse/linuxtools/lttng/ui/views/project/dialogs/Messages.java
+++ lttng/org.eclipse.linuxtools.lttng.ui/src/org/eclipse/linuxtools/lttng/ui/views/project/dialogs/Messages.java
@@ -22,6 +22,7 @@ public class Messages extends NLS {
 	public static String TraceLibraryPathWizardPage_TraceLoaderLibrary_notExists;
 	public static String TraceLibraryPathWizardPage_Title;
 	public static String TraceLibraryPathWizardPage_Description;
+	public static String SelectTrace_ErrorTitle;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
