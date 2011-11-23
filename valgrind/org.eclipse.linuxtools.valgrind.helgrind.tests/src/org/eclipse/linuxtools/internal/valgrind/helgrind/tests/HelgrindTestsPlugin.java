cca821e50f99a47d400ab5d03d0d349d6f71126a
Daniel Henrique Barboza <danielhb@br.ibm.com>
2011-11-23 11:24:11 -0200

    Helgrind.tests plug-in cleanup.

--- valgrind/org.eclipse.linuxtools.valgrind.helgrind.tests/src/org/eclipse/linuxtools/internal/valgrind/helgrind/tests/HelgrindTestsPlugin.java
+++ valgrind/org.eclipse.linuxtools.valgrind.helgrind.tests/src/org/eclipse/linuxtools/internal/valgrind/helgrind/tests/HelgrindTestsPlugin.java
@@ -19,7 +19,7 @@ import org.osgi.framework.BundleContext;
 public class HelgrindTestsPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.linuxtools.valgrind.cachegrind.tests"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.linuxtools.valgrind.helgrind.tests"; //$NON-NLS-1$
 
 	// The shared instance
 	private static HelgrindTestsPlugin plugin;
