96adfbeee26202ce3a743f665c129eb4d230dd68
Roland Grunberg <rgrunber@redhat.com>
2012-03-14 14:51:28 -0400

    Implement data rendering and begin making charting look similar to BIRT.

--- profiling/org.eclipse.linuxtools.dataviewers.charts/src/org/eclipse/linuxtools/dataviewers/charts/provider/ChartFactory.java
+++ profiling/org.eclipse.linuxtools.dataviewers.charts/src/org/eclipse/linuxtools/dataviewers/charts/provider/ChartFactory.java
@@ -24,6 +24,7 @@ import org.eclipse.ui.PlatformUI;
 import org.swtchart.Chart;
 import org.swtchart.IAxis;
 import org.swtchart.IBarSeries;
+import org.swtchart.ISeries.SeriesType;
 import org.swtchart.ITitle;
 import org.swtchart.LineStyle;
 
@@ -131,25 +132,24 @@ public class ChartFactory {
 			
 			final Color WHITE = new Color(Display.getDefault(), 255, 255, 255);
 			final Color BLACK = new Color(Display.getDefault(), 0, 0, 0);
-			final Color RED = new Color(Display.getDefault(), 255, 0, 0);
-			final Color ORANGE = new Color(Display.getDefault(), 255, 165, 0);
-			final Color GREEN = new Color(Display.getDefault(), 0, 255, 0);
 			final Color GRAD = new Color(Display.getDefault(), 225, 225, 225);
 			
 			view = (ChartView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(ChartView.VIEW_ID, ""+(ChartView.getSecId()), IWorkbenchPage.VIEW_ACTIVATE);
 			Chart chart = new Chart(view.getParent(), SWT.NONE);
-			
+
 			chart.setBackground(WHITE);
 			chart.setBackgroundInPlotArea(GRAD);
-			
+
 			chart.getTitle().setText(title);
-			
+			chart.getTitle().setForeground(BLACK);
+
+			// this is correct (refers to orientation of x-axis, not bars)
 			if (horizontal){
-				chart.setOrientation(SWT.HORIZONTAL);
-			}else{
 				chart.setOrientation(SWT.VERTICAL);
+			}else{
+				chart.setOrientation(SWT.HORIZONTAL);
 			}
-			
+
 			chart.getLegend().setPosition(SWT.RIGHT);
 			
 			String [] textLabels = new String [objects.length];
@@ -157,6 +157,7 @@ public class ChartFactory {
 				textLabels[i] = nameField.getValue(objects[i]);
 			}
 			
+			// x-axis
 			IAxis xAxis = chart.getAxisSet().getXAxis(0);
 			xAxis.getGrid().setStyle(LineStyle.NONE);
 			xAxis.getTick().setForeground(BLACK);
@@ -164,15 +165,35 @@ public class ChartFactory {
 			xTitle.setForeground(BLACK);
 			xTitle.setText(nameField.getColumnHeaderText());
 			xAxis.setCategorySeries(textLabels);
-			
+			xAxis.enableCategory(true);
+
+			// y-axis
 			IAxis yAxis = chart.getAxisSet().getYAxis(0);
-			yAxis.getGrid().setStyle(LineStyle.SOLID);
+			yAxis.getGrid().setStyle(LineStyle.NONE);
 			yAxis.getTick().setForeground(BLACK);
-			ITitle yTitle = yAxis.getTitle();
-//			yTitle.setText(heapChart.yUnits);
-			yTitle.setForeground(BLACK);
-			
-			
+			yAxis.getTitle().setVisible(false);
+
+			double max = -1D;
+
+			// data
+			for (IChartField field : valFields) {
+				final IBarSeries bs = (IBarSeries) chart.getSeriesSet().
+						createSeries(SeriesType.BAR, field.getColumnHeaderText());
+				bs.setBarColor(new Color(Display.getDefault(), getRC(), getRC(), getRC()));
+				double [] doubleValues = new double [objects.length];
+
+				for (int i = 0; i < objects.length; i++) {
+					Number num = field.getNumber(objects[i]);
+					double longVal = num.doubleValue();
+					max = longVal > max ? longVal : max;
+					doubleValues[i] = longVal;
+				}
+
+				bs.setYSeries(doubleValues);
+			}
+
+			chart.getAxisSet().adjustRange();
+
 			return chart;
 		} catch (PartInitException e) {
 			e.printStackTrace();
@@ -262,6 +283,10 @@ public class ChartFactory {
 		sdX.getSeries( ).add( seCategory );
 
 		return cwaBar;*/
+	}
+
+	private static int getRC () {
+		return (int) (Math.random() * 255);
 	}
 	
 	/**
