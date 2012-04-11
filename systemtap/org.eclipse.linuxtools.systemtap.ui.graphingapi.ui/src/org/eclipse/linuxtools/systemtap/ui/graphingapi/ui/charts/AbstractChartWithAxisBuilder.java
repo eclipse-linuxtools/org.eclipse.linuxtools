7da8f5d3c8c5918b1cc369acaacb210cf520f215
Otavio Pontes <obusatto@linux.vnet.ibm.com>
2012-04-11 16:42:05 -0300

    Systemtap: Adding label to XAxis and cleaning YAxis label.

--- systemtap/org.eclipse.linuxtools.systemtap.ui.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/ui/graphingapi/ui/charts/AbstractChartWithAxisBuilder.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/ui/graphingapi/ui/charts/AbstractChartWithAxisBuilder.java
@@ -1,9 +1,12 @@
-
 package org.eclipse.linuxtools.systemtap.ui.graphingapi.ui.charts;
 
 import org.eclipse.swt.widgets.Composite;
 
 import org.eclipse.linuxtools.systemtap.ui.graphingapi.nonui.adapters.IAdapter;
+
+import org.swtchart.IAxis;
+import org.swtchart.ITitle;
+import org.swtchart.LineStyle;
 
 /**
  * Builds the chart with axis.
@@ -41,5 +44,33 @@ public abstract class AbstractChartWithAxisBuilder extends AbstractChartBuilder 
     
     public AbstractChartWithAxisBuilder(IAdapter adapter, Composite parent, int style, String title) {
     	 super(adapter, parent, style, title);
+	}
+
+	/**
+	 * Builds X axis.
+	 */
+	protected void buildXAxis() {
+		String labels[] = adapter.getLabels();
+		IAxis xAxis = this.chart.getAxisSet().getXAxis(0);
+		xAxis.getGrid().setStyle(LineStyle.NONE);
+		xAxis.getTick().setForeground(BLACK);
+		ITitle xTitle = xAxis.getTitle();
+		xTitle.setForeground(BLACK);
+
+		if (labels.length > 0)
+			xTitle.setText(labels[0]);
+		else
+			xTitle.setText("");
+	}
+
+	/**
+	 * Builds Y axis.
+	 */
+	protected void buildYAxis() {
+		String labels[] = adapter.getLabels();
+		IAxis yAxis = this.chart.getAxisSet().getYAxis(0);
+		yAxis.getTitle().setText("");
+		yAxis.getGrid().setStyle(LineStyle.SOLID);
+		yAxis.getTick().setForeground(BLACK);
 	}
 }
