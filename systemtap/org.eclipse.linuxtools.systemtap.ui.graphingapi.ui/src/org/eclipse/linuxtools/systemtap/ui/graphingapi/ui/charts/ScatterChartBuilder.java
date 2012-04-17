18b5a7875b33c32181a30333b6af96bc151d6600
Otavio Pontes <obusatto@linux.vnet.ibm.com>
2012-04-16 21:07:45 -0300

    Systemtap: Implementing line chart

--- systemtap/org.eclipse.linuxtools.systemtap.ui.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/ui/graphingapi/ui/charts/ScatterChartBuilder.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/ui/graphingapi/ui/charts/ScatterChartBuilder.java
@@ -13,18 +13,12 @@
  */
 package org.eclipse.linuxtools.systemtap.ui.graphingapi.ui.charts;
 
-import org.eclipse.jface.preference.IPreferenceStore;
-
 import org.eclipse.swt.widgets.Composite;
 
-import org.eclipse.linuxtools.internal.systemtap.ui.graphingapi.ui.GraphingAPIUIPlugin;
-import org.eclipse.linuxtools.systemtap.ui.graphingapi.nonui.adapters.BlockAdapter;
 import org.eclipse.linuxtools.systemtap.ui.graphingapi.nonui.adapters.IAdapter;
-import org.eclipse.linuxtools.systemtap.ui.graphingapi.ui.preferences.GraphingAPIPreferenceConstants;
 
 import org.swtchart.ILineSeries;
 import org.swtchart.ISeries;
-import org.swtchart.ISeries.SeriesType;
 import org.swtchart.LineStyle;
 
 /**
@@ -33,49 +27,15 @@ import org.swtchart.LineStyle;
  * @author Qi Liang
  */
 
-public class ScatterChartBuilder extends AbstractChartWithAxisBuilder {
-
-    protected AbstractChartBuilder builder = null;
-
-	private boolean fullUpdate;
-	protected int xSeriesTicks;
-	protected static int ySeriesTicks;
-	protected static int maxItems;
-	protected static int viewableItems;
-
+public class ScatterChartBuilder extends LineChartBuilder {
 	public static final String ID = "org.eclipse.linuxtools.systemtap.ui.graphingapi.ui.charts.scatterchartbuilder";
 
     public ScatterChartBuilder(Composite parent, int style, String title,IAdapter adapter) {
-		super(adapter, parent, style, title);
-		fullUpdate = (adapter instanceof BlockAdapter) ? true : false;
+		super(parent, style, title, adapter);
     }
     
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.ibm.examples.chart.widget.AbstractChartBuilder#buildChart()
-     */
-    protected void createChart() {
-		super.createChart();
-    	IPreferenceStore store = GraphingAPIUIPlugin.getDefault().getPreferenceStore();
-		xSeriesTicks = store.getInt(GraphingAPIPreferenceConstants.P_X_SERIES_TICKS);
-		ySeriesTicks = store.getInt(GraphingAPIPreferenceConstants.P_Y_SERIES_TICKS);
-		maxItems = store.getInt(GraphingAPIPreferenceConstants.P_MAX_DATA_ITEMS);
-		viewableItems = store.getInt(GraphingAPIPreferenceConstants.P_VIEWABLE_DATA_ITEMS);
-    }
-
-	public void setScale(double scale) {
-		xSeriesTicks = (int) (((Integer)xSeriesTicks).doubleValue() * scale);
-		handleUpdateEvent();
-	}
-
-	public void updateDataSet() {
-		buildXSeries();
-	}
-
 	protected ISeries createChartISeries(int i) {
-		ILineSeries series = (ILineSeries) chart.getSeriesSet().
-			createSeries(SeriesType.LINE, adapter.getLabels()[i+1]);
+		ILineSeries series = (ILineSeries)super.createChartISeries(i);
 		series.setSymbolColor(COLORS[i % COLORS.length]);
 		series.setLineStyle(LineStyle.NONE);
 		return series;
