4a894644d49b2d14b26bfc98364fc9826cef2459
Daniel Henrique Barboza <danielhb@br.ibm.com>
2012-06-21 16:09:47 -0300

    danielhb: Fixing Helgrind unit tests

--- valgrind/org.eclipse.linuxtools.valgrind.helgrind.tests/resources/basicTest/test.c
+++ valgrind/org.eclipse.linuxtools.valgrind.helgrind.tests/resources/basicTest/test.c
@@ -1,19 +1,41 @@
-/*******************************************************************************
- * Copyright (c) 2008 Red Hat, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Elliott Baron <ebaron@redhat.com> - initial API and implementation
- *******************************************************************************/
-#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
-#define SIZE 10
-int main() {
-	char *waste = (char *)malloc(sizeof(char) * SIZE);
-	waste[0] = waste[SIZE];
-	waste[SIZE] = 0;
-	return 0;
+pthread_t thread;
+
+void* thread3 (void* d)
+{
+  int count3 = 0;
+
+  while(count3 < 100){
+    printf("Thread 3: %d\n", count3++);
+  }
+  return NULL;
+}
+
+void* thread2 (void* d)
+{
+  int count2 = 0;
+
+  while(count2 < 1000){
+	  //
+    printf("Thread 2: %d\n", count2++);
+  }
+  return NULL;
+}
+
+int main (){
+
+  pthread_create (&thread, NULL, thread2, NULL);
+  pthread_create (&thread, NULL, thread3, NULL);
+
+  //Thread 1
+  int count1 = 0;
+
+  while(count1 < 10){
+    printf("Thread 1: %d\n", count1++);
+  }
+
+  pthread_join(thread,NULL);
+  return 0;
 }
