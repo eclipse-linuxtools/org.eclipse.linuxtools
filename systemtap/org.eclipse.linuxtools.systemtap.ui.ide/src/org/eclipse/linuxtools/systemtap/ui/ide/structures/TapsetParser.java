e2d16377568760995448111f0e27e9476cdb8d60
Sami Wagiaalla <swagiaal@redhat.com>
2012-08-14 14:58:34 -0400

    Fix infinite loop in runPass2ProbeSet()
    When low and high are only one index apart the list
    is one element long and cannot be split into 2. I
    added a check for this so that this function does
    not go into an infinite loopl.
    
    Change-Id: I747c93bc0bdfba8cdc3953d689178dfd635476c8
    Reviewed-on: https://git.eclipse.org/r/7234
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    IP-Clean: Jeff Johnston <jjohnstn@redhat.com>
    Tested-by: Jeff Johnston <jjohnstn@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/systemtap/ui/ide/structures/TapsetParser.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/systemtap/ui/ide/structures/TapsetParser.java
@@ -424,11 +424,14 @@ public class TapsetParser implements Runnable {
 	}
 	
 	/**
-	 * Runs stap -up2 on the selected probe group, using high and low
-	 * to determin which subelements to select.
+	 * Does a depth first search for valid probes: Runs stap -up2 on the
+	 * selected probe group, using high and low to determine which
+	 * subelements to select. If an error is encountered in a group this
+	 * function divides the group into a top and bottom half and makes a
+	 * recursive call on each subgroup to isolate the failing probes.
 	 * @param probe The top level probe group to probe.
 	 * @param low The lower bound of child elements of probe to include
-	 * @param high The upper bound of child elements of probe to inclue
+	 * @param high The upper bound of child elements of probe to include
 	 */
 	private void runPass2ProbeSet(TreeNode probe, int low, int high) {
 		if(low == high)
@@ -448,8 +451,8 @@ public class TapsetParser implements Runnable {
 		result = runStap(new String[] {"-u"}, probeStr.toString(), 2);
 		
 		if(0 < result.trim().length()) {
-			boolean success = parsePass2Probes(result, probe);
-			if(!success) {
+			boolean success = parsePass2Probes(result,probe);
+			if(!success && low+1 != high) {
 				runPass2ProbeSet(probe, low, low+((high-low)>>1));
 				runPass2ProbeSet(probe, low+((high-low)>>1), high);
 			}
