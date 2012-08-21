ec9bc33bab1618ee2d14c892eb20e7306b38b094
Alexander Kurtakov <akurtako@redhat.com>
2012-08-21 23:10:25 +0300

    Add javadoc and remove unused stuff.
    Change-Id: I858255a8dfa8683365d0e6d709fda7e5eecd80e2
    Reviewed-on: https://git.eclipse.org/r/7336
    Reviewed-by: Sami Wagiaalla <swagiaal@redhat.com>
    IP-Clean: Sami Wagiaalla <swagiaal@redhat.com>
    Tested-by: Sami Wagiaalla <swagiaal@redhat.com>

--- rpm/org.eclipse.linuxtools.rpm.rpmlint/src/org/eclipse/linuxtools/internal/rpm/rpmlint/parser/RpmlintItem.java
+++ rpm/org.eclipse.linuxtools.rpm.rpmlint/src/org/eclipse/linuxtools/internal/rpm/rpmlint/parser/RpmlintItem.java
@@ -12,6 +12,9 @@ package org.eclipse.linuxtools.internal.rpm.rpmlint.parser;
 
 import org.eclipse.linuxtools.rpm.ui.editor.parser.SpecfileParser;
 
+/**
+ * Single warning/error parsed from rpmlint output.
+ */
 public class RpmlintItem {
 
 	private static final String[] sections = SpecfileParser.simpleSections;
@@ -24,71 +27,114 @@ public class RpmlintItem {
 
 	private String refferedContent;
 
-	private String refferedSection;
-
 	private String message;
 	
 	private String fileName;
 
+	/**
+	 * Returns the name of the file rpmlint gives warning for.
+	 * 
+	 * @return The file name.
+	 */
 	public String getFileName() {
 		return fileName;
 	}
 
+	/**
+	 * Sets the name of the file rpmlint gives warning for.
+	 * @param file The File name.
+	 */
 	public void setFileName(String file) {
 		this.fileName = file;
 	}
 
+	/**
+	 * Returns the line number where the warning appears if rpmlint gives it. 
+	 * @return The line number.
+	 */
 	public int getLineNbr() {
 		return lineNbr;
 	}
 
+	/**
+	 * Sets the line number where the warning appears if rpmlint gives it. 
+	 * @param lineNbr The line number.
+	 */
 	public void setLineNbr(int lineNbr) {
 		this.lineNbr = lineNbr;
 	}
 
+	/**
+	 * The id of the warning.
+	 * @return The id.
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the id of the warning.
+	 * @param id The id of the warning.
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Returns the message as rpmlint gives it.
+	 * @return The message.
+	 */
 	public String getMessage() {
 		return message;
 	}
 
+	/**
+	 * Sets the message as rpmlint gives it.
+	 * @param message The raw message.
+	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
+	/**
+	 * Returns the referred content - section, tag, etc.
+	 * @return The referred content.
+	 */
 	public String getRefferedContent() {
 		return refferedContent;
 	}
 
+	/**
+	 * Sets the referred content.
+	 * @param refferedContent The referred content.
+	 */
 	public void setRefferedContent(String refferedContent) {
 		for (int i = 0; i < sections.length; i++) {
 			if (refferedContent.startsWith(sections[i])) {
 				this.refferedContent = refferedContent.trim();
 				if (this.refferedContent.equals("")) //$NON-NLS-1$
 					this.refferedContent = sections[i];
-				this.refferedSection = sections[i];
 				i = sections.length;
 			} else {
 				this.refferedContent = refferedContent;
-				this.refferedSection = ""; //$NON-NLS-1$
 			}
 		}
 	}
 
-	public String getrefferedSection() {
-		return refferedSection;
-	}
-
+	/**
+	 * Returns the severity of the rpmlint item.
+	 * 
+	 * @return The severity.
+	 */
 	public int getSeverity() {
 		return severity;
 	}
 
+	/**
+	 * Returns the severity of the rpmlint item.
+	 * 
+	 * @param severity The severity of the rpmlint item.
+	 */
 	public void setSeverity(String severity) {
 		severity = severity.replaceAll(":", "").trim(); //$NON-NLS-1$ //$NON-NLS-2$
 		switch (severity.charAt(0)) {
@@ -107,6 +153,9 @@ public class RpmlintItem {
 		}
 	}
 
+	/**
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder();
