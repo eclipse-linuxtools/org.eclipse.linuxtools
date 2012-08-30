eca98f248f908e7d9d6c4824113fd8f82ef54c51
Alexander Kurtakov <akurtako@redhat.com>
2012-08-30 11:19:58 +0300

    Replace instanceof with null check.
    An item in IBinary[] is always IBinary so instance of makes no sense.
    The only case where IBinary[0] instanceof IBinary can fail is if it's
    null.
    
    Change-Id: Iea0c06d5307cedd0b016dd0bc9e791dd75148423
    Reviewed-on: https://git.eclipse.org/r/7506
    Tested-by: Hudson CI
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

--- systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapOptionsTab.java
+++ systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapOptionsTab.java
@@ -894,7 +894,7 @@ public class SystemTapOptionsTab extends CLaunchConfigurationTab{
 			ICProject project =  CDebugUtils.verifyCProject(config);
 			IBinary[] binaries = project.getBinaryContainer().getBinaries();
 			if (binaries != null && binaries.length > 0) {
-				if (binaries.length == 1 && binaries[0] instanceof IBinary) {
+				if (binaries.length == 1 && binaries[0] != null) {
 					return binaries[0];
 				} else
 					return chooseBinary(binaries);
