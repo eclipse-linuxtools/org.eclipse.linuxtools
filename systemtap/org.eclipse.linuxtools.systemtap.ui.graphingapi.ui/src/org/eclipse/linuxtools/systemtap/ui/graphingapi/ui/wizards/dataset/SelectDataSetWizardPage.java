72acc1dd34705e9a2f1e976e069a012b67106f3c
Alexander Kurtakov <akurtako@redhat.com>
2012-09-04 11:04:22 +0300

    Display DataSet descriptions by default.
    Selecting a dataset type just to see what it presents is wrong. 
    Labels are displayed by default so whenever one selects something, he is
    aware of what is selected. 
    
    Change-Id: I888e4c43513b6c0fa75a7130e49400a6841c6f1e
    Reviewed-on: https://git.eclipse.org/r/7578
    Tested-by: Hudson CI
    Reviewed-by: Sami Wagiaalla <swagiaal@redhat.com>
    IP-Clean: Sami Wagiaalla <swagiaal@redhat.com>
    Tested-by: Sami Wagiaalla <swagiaal@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.ui.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/ui/graphingapi/ui/wizards/dataset/SelectDataSetWizardPage.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/ui/graphingapi/ui/wizards/dataset/SelectDataSetWizardPage.java
@@ -15,23 +15,22 @@ import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.linuxtools.internal.systemtap.ui.graphingapi.ui.Localization;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.ui.forms.widgets.ColumnLayout;
 
 
 
 public class SelectDataSetWizardPage extends WizardPage {
 	public SelectDataSetWizardPage() {
-		super("selectDataSetType");
-		setTitle(Localization.getString("SelectDataSetWizardPage.SelectDataSetType"));
-		dataSetID = "";
+		super("selectDataSetType"); //$NON-NLS-1$
+		setTitle(Localization.getString("SelectDataSetWizardPage.SelectDataSetType")); //$NON-NLS-1$
+		dataSetID = ""; //$NON-NLS-1$
 		btnDataSets = null;
 	}
 	
@@ -39,43 +38,34 @@ public class SelectDataSetWizardPage extends WizardPage {
 		wizard = (DataSetWizard)super.getWizard();
 		
 		Composite comp = new Composite(parent, SWT.NULL);
-		comp.setLayout(new FormLayout());
-		FormData data1 = new FormData();
-		data1.left = new FormAttachment(0, 0);
-		data1.top = new FormAttachment(0, 0);
-		data1.right = new FormAttachment(40, 0);
-		data1.bottom = new FormAttachment(100, 0);
+		GridData layoutData = new GridData(GridData.FILL_BOTH);
+		layoutData.grabExcessHorizontalSpace = true;
+		layoutData.horizontalAlignment = SWT.FILL;
+		GridLayout gridLayout = new GridLayout(2, false);
+		comp.setLayout(gridLayout);
 
-		Composite cmpFilterOpts = new Composite(comp, SWT.NONE);
-		cmpFilterOpts.setLayoutData(data1);
-		ColumnLayout colLayout = new ColumnLayout();
-		colLayout.maxNumColumns = 1;
-		cmpFilterOpts.setLayout(colLayout);
-		
 		String[] ids = DataSetFactory.getIDs();
 		btnDataSets = new Button[ids.length];
+		Label lblDesc;
 		for(int i=0; i<btnDataSets.length; i++) {
-			btnDataSets[i] = new Button(cmpFilterOpts, SWT.NONE);
+			btnDataSets[i] = new Button(comp, SWT.RADIO);
 			btnDataSets[i].setText(DataSetFactory.getName(ids[i]));
 			btnDataSets[i].addSelectionListener(buttonListener);
 			btnDataSets[i].setData(ids[i]);
+			lblDesc = new Label(comp, SWT.WRAP);
+			lblDesc.setText(DataSetFactory.getDescription(btnDataSets[i].getData().toString()));
+			Label separator = new Label(comp, SWT.HORIZONTAL | SWT.SEPARATOR);
+			GridData separatorData = new GridData(GridData.FILL_HORIZONTAL);
+			separatorData.horizontalSpan=2;
+		    separator.setLayoutData(separatorData);
 		}
-		
-		FormData data2 = new FormData();
-		data2.left = new FormAttachment(cmpFilterOpts);
-		data2.top = new FormAttachment(0, 0);
-		data2.right = new FormAttachment(100, 0);
-		data2.bottom = new FormAttachment(100, 0);
-		
-		lblDesc = new Label(comp, SWT.WRAP);
-		lblDesc.setLayoutData(data2);
 		
 		setControl(comp);
 	}
 	
 	@Override
 	public boolean canFlipToNextPage() {
-		return (dataSetID.length() > 0);
+		return !dataSetID.isEmpty();
 	}
 	
 	@Override
@@ -93,12 +83,10 @@ public class SelectDataSetWizardPage extends WizardPage {
 				btnDataSets[i] = null;
 			}
 		btnDataSets = null;
-		lblDesc = null;
 	}
 
-	private SelectionListener buttonListener = new SelectionListener() {
-		public void widgetDefaultSelected(SelectionEvent e) {}
-		
+	private SelectionListener buttonListener = new SelectionAdapter() {
+		@Override
 		public void widgetSelected(SelectionEvent e) {
 			if(e.widget instanceof Button) {
 				Button target = (Button)e.widget;
@@ -106,8 +94,6 @@ public class SelectDataSetWizardPage extends WizardPage {
 				for(int i=0; i<btnDataSets.length; i++) {
 					if(target == btnDataSets[i]) {
 						dataSetID = btnDataSets[i].getData().toString();
-						lblDesc.setText(DataSetFactory.getName(dataSetID) + "\n\n" +
-										DataSetFactory.getDescription(dataSetID));
 						wizard.getContainer().updateButtons();
 					}
 				}
@@ -116,7 +102,6 @@ public class SelectDataSetWizardPage extends WizardPage {
 	};
 	
 	private Button[] btnDataSets;
-	private Label lblDesc;
 	private String dataSetID;
 	private DataSetWizard wizard;
 }
