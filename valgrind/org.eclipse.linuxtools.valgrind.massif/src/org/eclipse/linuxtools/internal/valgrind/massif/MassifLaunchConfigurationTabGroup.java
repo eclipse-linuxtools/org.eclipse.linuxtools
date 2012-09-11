b5358375fbcc5b2f44a4eeb4bb7ca7ed1e67a434
Jeff Johnston <jjohnstn@redhat.com>
2012-09-11 16:43:58 -0400

    Add massif memory profiling support
    - Add massif launch shortcut
    - Add massif launch configuration tab group
    - Update tool page to set Valgrind tool to massif
    
    Change-Id: I096362fb00d9e92fd1946d21bde3e15f274773cf
    Reviewed-on: https://git.eclipse.org/r/7720
    Tested-by: Hudson CI
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>
    IP-Clean: Roland Grunberg <rgrunber@redhat.com>
    Tested-by: Roland Grunberg <rgrunber@redhat.com>

---
+++ valgrind/org.eclipse.linuxtools.valgrind.massif/src/org/eclipse/linuxtools/internal/valgrind/massif/MassifLaunchConfigurationTabGroup.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Red Hat Inc. - initial API and implementation
+ *******************************************************************************/ 
+package org.eclipse.linuxtools.internal.valgrind.massif;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.linuxtools.internal.valgrind.launch.ValgrindSingleToolOptionsTab;
+import org.eclipse.linuxtools.profiling.launch.ProfileLaunchConfigurationTabGroup;
+
+public class MassifLaunchConfigurationTabGroup extends
+		ProfileLaunchConfigurationTabGroup {
+
+	@Override
+	public AbstractLaunchConfigurationTab[] getProfileTabs() {
+		return new AbstractLaunchConfigurationTab[] {
+			new ValgrindSingleToolOptionsTab("org.eclipse.linuxtools.valgrind.launch.massif"), //$NON-NLS-1$
+		};
+	}	
+	
+}
--- valgrind/org.eclipse.linuxtools.valgrind.massif/src/org/eclipse/linuxtools/internal/valgrind/massif/MassifToolPage.java
+++ valgrind/org.eclipse.linuxtools.valgrind.massif/src/org/eclipse/linuxtools/internal/valgrind/massif/MassifToolPage.java
@@ -19,6 +19,7 @@ import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.window.Window;
+import org.eclipse.linuxtools.internal.valgrind.core.LaunchConfigurationConstants;
 import org.eclipse.linuxtools.valgrind.launch.IValgrindToolPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
@@ -431,6 +432,7 @@ public class MassifToolPage extends AbstractLaunchConfigurationTab
 	}
 	
 	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+		configuration.setAttribute(LaunchConfigurationConstants.ATTR_TOOL, MassifPlugin.TOOL_ID);
 		configuration.setAttribute(MassifLaunchConstants.ATTR_MASSIF_HEAP, MassifLaunchConstants.DEFAULT_MASSIF_HEAP);
 		configuration.setAttribute(MassifLaunchConstants.ATTR_MASSIF_HEAPADMIN, MassifLaunchConstants.DEFAULT_MASSIF_HEAPADMIN);
 		configuration.setAttribute(MassifLaunchConstants.ATTR_MASSIF_STACKS, MassifLaunchConstants.DEFAULT_MASSIF_STACKS);
