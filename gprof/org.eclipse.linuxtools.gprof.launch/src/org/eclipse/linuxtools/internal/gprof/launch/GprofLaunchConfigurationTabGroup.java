4342f0b66890bec0ccad83250a895ef19129ccb1
Jeff Johnston <jjohnstn@redhat.com>
2012-10-03 16:36:47 -0400

    Bug 390874: Add gprof to Profile Timing category https://bugs.eclipse.org/bugs/show_bug.cgi?id=390874
    - Add o.e.l.gprof.launch plug-in
    
    Change-Id: Ifb8c47eb4007544fc8a4b393af57eb1017f0fbb1
    Reviewed-on: https://git.eclipse.org/r/8013
    Tested-by: Hudson CI
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

---
+++ gprof/org.eclipse.linuxtools.gprof.launch/src/org/eclipse/linuxtools/internal/gprof/launch/GprofLaunchConfigurationTabGroup.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Red Hat Inc. - initial API and implementation
+ *******************************************************************************/ 
+package org.eclipse.linuxtools.internal.gprof.launch;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.linuxtools.profiling.launch.ProfileLaunchConfigurationTabGroup;
+
+public class GprofLaunchConfigurationTabGroup extends
+		ProfileLaunchConfigurationTabGroup {
+
+	@Override
+	public AbstractLaunchConfigurationTab[] getProfileTabs() {
+		return new AbstractLaunchConfigurationTab[0];
+	}	
+	
+}
