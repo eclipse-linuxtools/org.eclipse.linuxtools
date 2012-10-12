6221eda20afbc9439e6afb02716ba2c806c1a7d6
Jeff Johnston <jjohnstn@redhat.com>
2012-10-12 19:00:47 -0400

    Add test for Massif launch short-cut.
    Change-Id: Ifc994b5ce940204e42dbb106fd831cd1102d8d5e
    Reviewed-on: https://git.eclipse.org/r/8189
    Tested-by: Hudson CI
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

--- valgrind/org.eclipse.linuxtools.valgrind.massif.tests/resources/basicTest/test.c
+++ valgrind/org.eclipse.linuxtools.valgrind.massif.tests/resources/basicTest/test.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 Red Hat, Inc.
+ * Copyright (c) 2008 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,15 +8,12 @@
  * Contributors:
  *    Elliott Baron <ebaron@redhat.com> - initial API and implementation
  *******************************************************************************/
-package org.eclipse.linuxtools.internal.valgrind.massif.tests;
+#include <stdlib.h>
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ DoubleClickTest.class, ChartTests.class, TreeTest.class,
-		BasicMassifTest.class, LaunchConfigTabTest.class,
-		ExportWizardTest.class, MultiProcessTest.class,
-		ExpandCollapseTest.class, SortTest.class, ChartExportTest.class, ShortcutTest.class })
-public class AllTests {
+#define SIZE 10
+int main() {
+	char *waste = (char *)malloc(sizeof(char) * SIZE);
+	waste[0] = waste[SIZE];
+	waste[SIZE] = 0;
+	return 0;
 }
--- valgrind/org.eclipse.linuxtools.valgrind.massif.tests/src/org/eclipse/linuxtools/internal/valgrind/massif/tests/ValgrindTestMassifLaunchShortcut.java
+++ valgrind/org.eclipse.linuxtools.valgrind.massif.tests/src/org/eclipse/linuxtools/internal/valgrind/massif/tests/ValgrindTestMassifLaunchShortcut.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 Red Hat, Inc.
+ * Copyright (c) 2009, 2012 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,16 +7,24 @@
  *
  * Contributors:
  *    Elliott Baron <ebaron@redhat.com> - initial API and implementation
+ *    Red Hat Inc. - modified to test Massif short-cut
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.valgrind.massif.tests;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.eclipse.cdt.core.model.IBinary;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.linuxtools.internal.valgrind.massif.MassifLaunchShortcut;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ DoubleClickTest.class, ChartTests.class, TreeTest.class,
-		BasicMassifTest.class, LaunchConfigTabTest.class,
-		ExportWizardTest.class, MultiProcessTest.class,
-		ExpandCollapseTest.class, SortTest.class, ChartExportTest.class, ShortcutTest.class })
-public class AllTests {
+public class ValgrindTestMassifLaunchShortcut extends MassifLaunchShortcut {
+
+	private ILaunchConfiguration config;
+
+	@Override
+	public void launch(IBinary bin, String mode) {
+		config = findLaunchConfiguration(bin, mode);
+	}
+	
+	public ILaunchConfiguration getConfig() {
+		return config;
+	}
 }
