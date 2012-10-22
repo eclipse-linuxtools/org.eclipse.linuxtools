e39b9d442eae2a8ed03952f2130b4a309fe9d91a
Roland Grunberg <rgrunber@redhat.com>
2012-10-22 14:30:58 -0400

    Revert "Move perf test cases that perform a launch into separate test class."
    This reverts commit 7c1c27fbd30a54c13c56701517fd906cdb4629bb.
    Unintentionally pushed directly to master. This should go through the
    code review process.

--- perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/ModelTest.java
+++ perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/ModelTest.java
@@ -18,12 +18,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.core.Launch;
 import org.eclipse.linuxtools.internal.perf.PerfCore;
 import org.eclipse.linuxtools.internal.perf.PerfPlugin;
 import org.eclipse.linuxtools.internal.perf.launch.PerfEventsTab;
+import org.eclipse.linuxtools.internal.perf.launch.PerfLaunchConfigDelegate;
 import org.eclipse.linuxtools.internal.perf.launch.PerfOptionsTab;
 import org.eclipse.linuxtools.internal.perf.model.PMCommand;
 import org.eclipse.linuxtools.internal.perf.model.PMDso;
@@ -36,6 +41,9 @@ import org.osgi.framework.FrameworkUtil;
 
 public class ModelTest extends AbstractTest {
 	protected ILaunchConfiguration config;
+	protected PerfLaunchConfigDelegate delegate;
+	protected ILaunch launch;
+	protected ILaunchConfigurationWorkingCopy wc;
 	protected Stack<Class<?>> stack;
 
 	@Override
@@ -43,6 +51,11 @@ public class ModelTest extends AbstractTest {
 		super.setUp();
 		proj = createProjectAndBuild(FrameworkUtil.getBundle(this.getClass()), "fibTest"); //$NON-NLS-1$
 		config = createConfiguration(proj.getProject());
+
+		delegate = new PerfLaunchConfigDelegate();
+		launch = new Launch(config, ILaunchManager.PROFILE_MODE, null);
+		wc = config.getWorkingCopy();
+		setProfileAttributes(wc);
 
 		Class<?>[] klassList = new Class<?>[] { PMSymbol.class, PMFile.class,
 				PMDso.class, PMCommand.class, PMEvent.class };
@@ -53,6 +66,7 @@ public class ModelTest extends AbstractTest {
 	@Override
 	protected void tearDown() throws Exception {
 		deleteProject(proj);
+		wc.delete();
 		super.tearDown();
 	}
 
@@ -68,6 +82,26 @@ public class ModelTest extends AbstractTest {
 		wc.setAttribute(PerfPlugin.ATTR_SourceLineNumbers, false);
 		eventsTab.setDefaults(wc);
 		optionsTab.setDefaults(wc);
+	}
+
+	public void testDefaultRun () {
+		try {
+			delegate.launch(wc, ILaunchManager.PROFILE_MODE, launch, null);
+		} catch (CoreException e) {
+			fail();
+		}
+	}
+
+	public void testClockEventRun () {
+		try {
+			ArrayList<String> list = new ArrayList<String>();
+			list.addAll(Arrays.asList(new String [] {"cpu-clock", "task-clock", "cycles"}));
+			wc.setAttribute(PerfPlugin.ATTR_DefaultEvent, false);
+			wc.setAttribute(PerfPlugin.ATTR_SelectedEvents, list);
+			delegate.launch(wc, ILaunchManager.PROFILE_MODE, launch, null);
+		} catch (CoreException e) {
+			fail();
+		}
 	}
 
 	public void testModelDefaultGenericStructure() {
