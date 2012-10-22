7c1c27fbd30a54c13c56701517fd906cdb4629bb
Roland Grunberg <rgrunber@redhat.com>
2012-10-22 14:23:33 -0400

    Move perf test cases that perform a launch into separate test class.
    Test cases that require the perf underlying tool to be present should
    not be executed on the build server, but should run by default.
    
    Change-Id: Ib7cd9a284541eb48cfe67465d5a21cec78760fa9

---
+++ perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/AllPerfTests.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+* Copyright (c) 2012 Red Hat, Inc.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Red Hat, Inc. - initial API and implementation
+*******************************************************************************/
+package org.eclipse.linuxtools.internal.perf.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllPerfTests {
+
+	public static Test suite() {
+		/**
+		 * Java system properties. usage:
+		 * -Dorg.eclipse.linuxtools.oprofile.launch.tests.runOprofile=<yes|no>
+		 * [default: yes] if yes, will run the launch tests no, will skip the
+		 * launch tests (they all require oprofile to be set up)
+		 */
+		String SYSTEM_PROPERTY_RUN_PERF = "org.eclipse.linuxtools.perf.tests.runPerf"; //$NON-NLS-1$
+		boolean RUN_PERF = System
+				.getProperty(SYSTEM_PROPERTY_RUN_PERF, "yes").equals("yes"); //$NON-NLS-1$ //$NON-NLS-2$
+		TestSuite suite = new TestSuite(
+				"Test for org.eclipse.linuxtools.perf.tests"); //$NON-NLS-1$
+
+		suite.addTestSuite(LaunchTabsTest.class);
+		suite.addTestSuite(ModelTest.class);
+
+		if (RUN_PERF) {
+			suite.addTestSuite(LaunchTest.class);
+		}
+
+		return suite;
+	}
+
+}
---
+++ perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/LaunchTest.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+* Copyright (c) 2012 Red Hat, Inc.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Red Hat, Inc. - initial API and implementation
+*******************************************************************************/
+package org.eclipse.linuxtools.internal.perf.tests;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.core.Launch;
+import org.eclipse.linuxtools.internal.perf.PerfPlugin;
+import org.eclipse.linuxtools.internal.perf.launch.PerfEventsTab;
+import org.eclipse.linuxtools.internal.perf.launch.PerfLaunchConfigDelegate;
+import org.eclipse.linuxtools.internal.perf.launch.PerfOptionsTab;
+import org.eclipse.linuxtools.profiling.tests.AbstractTest;
+import org.osgi.framework.FrameworkUtil;
+
+public class LaunchTest extends AbstractTest {
+
+	protected ILaunchConfiguration config;
+	protected PerfLaunchConfigDelegate delegate;
+	protected ILaunch launch;
+	protected ILaunchConfigurationWorkingCopy wc;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		proj = createProjectAndBuild(FrameworkUtil.getBundle(this.getClass()), "fibTest"); //$NON-NLS-1$
+		config = createConfiguration(proj.getProject());
+
+		delegate = new PerfLaunchConfigDelegate();
+		launch = new Launch(config, ILaunchManager.PROFILE_MODE, null);
+		wc = config.getWorkingCopy();
+		setProfileAttributes(wc);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		deleteProject(proj);
+		wc.delete();
+		super.tearDown();
+	}
+
+	@Override
+	protected ILaunchConfigurationType getLaunchConfigType() {
+		return getLaunchManager().getLaunchConfigurationType(PerfPlugin.LAUNCHCONF_ID);
+	}
+
+	@Override
+	protected void setProfileAttributes(ILaunchConfigurationWorkingCopy wc) {
+		PerfEventsTab eventsTab = new PerfEventsTab();
+		PerfOptionsTab optionsTab = new PerfOptionsTab();
+		wc.setAttribute(PerfPlugin.ATTR_SourceLineNumbers, false);
+		eventsTab.setDefaults(wc);
+		optionsTab.setDefaults(wc);
+	}
+
+	public void testDefaultRun () {
+		try {
+			delegate.launch(wc, ILaunchManager.PROFILE_MODE, launch, null);
+		} catch (CoreException e) {
+			fail();
+		}
+	}
+
+	public void testClockEventRun () {
+		try {
+			ArrayList<String> list = new ArrayList<String>();
+			list.addAll(Arrays.asList(new String [] {"cpu-clock", "task-clock", "cycles"}));
+			wc.setAttribute(PerfPlugin.ATTR_DefaultEvent, false);
+			wc.setAttribute(PerfPlugin.ATTR_SelectedEvents, list);
+			delegate.launch(wc, ILaunchManager.PROFILE_MODE, launch, null);
+		} catch (CoreException e) {
+			fail();
+		}
+	}
+
+}
