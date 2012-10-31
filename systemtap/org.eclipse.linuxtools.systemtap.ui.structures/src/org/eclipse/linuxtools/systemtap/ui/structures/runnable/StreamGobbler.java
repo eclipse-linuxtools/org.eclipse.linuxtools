7eb680098ea11f6ca4dd42a974abe541925ed203
Sami Wagiaalla <swagiaal@redhat.com>
2012-10-31 10:07:48 -0400

    Fix deadlock in StreamGobbler
    Also,
    - Remove dead code.
    - Fix spelling and formatting.
    - Use a predictable test input stream in StreamGobblerTest.
    - Rename reader thread from to to reader.
    
    Change-Id: I94b972874a56cbab116ac60faab7e9767621b79f
    Reviewed-on: https://git.eclipse.org/r/8453
    Tested-by: Hudson CI
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>
    IP-Clean: Roland Grunberg <rgrunber@redhat.com>
    Tested-by: Roland Grunberg <rgrunber@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.ui.structures/src/org/eclipse/linuxtools/systemtap/ui/structures/runnable/StreamGobbler.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.structures/src/org/eclipse/linuxtools/systemtap/ui/structures/runnable/StreamGobbler.java
@@ -32,7 +32,6 @@ public class StreamGobbler implements Runnable {
 			line = new StringBuilder();
 			listeners = new ArrayList<IGobblerListener>();
 		}
-		locked = false;
 	}
 	
 	/**
@@ -43,8 +42,8 @@ public class StreamGobbler implements Runnable {
 	 */
 	//Make sure to call this method to start the StreamGobbler
 	public void start() {
-		t = new Thread(this, "StreamGobbler");
-		t.start();
+		reader = new Thread(this, "StreamGobbler"); //$NON-NLS-1$
+		reader.start();
 	}
 	
 	/**
@@ -52,51 +51,47 @@ public class StreamGobbler implements Runnable {
 	 * @return boolean representing whether or not it is sill running
 	 */
 	public boolean isRunning() {
-		return (null != t);
+		return (null != reader);
 	}
 
 	/**
 	 * The main method of this class. It monitors the provided thread to see
 	 * when new data is available and then appends it to its current list of
 	 * data.  When a new line is read it will fire a DataEvent for listeners
-	 * to get ahold of the data.
+	 * to get a hold of the data.
 	 */
 	public void run() {
-		Thread thisThread = Thread.currentThread();
-		try {
-			int val=-1;
+		if (reader == Thread.currentThread())
+			return;
 
-			while(t == thisThread) {
-				while(0 < is.available()) {
-					if(-1 == (val = is.read()))
-						this.stop();
-					else
-						line.append((char)val);
-					if ('\n' == val)
-						this.fireNewDataEvent();
-					
-				}
-				try {
-					Thread.sleep(10);
-				} catch(InterruptedException ie) {}
+		try {
+			int val = is.read();
+			while(val != -1) {
+				line.append((char)val);
+
+				if ('\n' == val)
+					this.fireNewDataEvent();
+
+				val = is.read();
 			}
-		} catch (IOException ioe) {}	//If stream closed before thread shuts down
+		} catch (IOException ioe) {}	// If stream closed before thread shuts down
 	}
 
 	/**
-	 * Stops the gobbler from monitering the stream, and fires one last data event
+	 * Stops the gobbler from monitoring the stream, and fires one last data event
 	 * to make sure that listeners have the entire contents of what was read in
 	 * from the stream.
 	 */
 	public synchronized void stop() {
-		try {	//Make sure we don't stop while there is still data in the stream
-			while(0 != is.available()) {
-				Thread.sleep(10);
-			}
-		} catch(Exception e) {}
-		t = null;
+		try {
+			// Wait for the reader thread to finish reading the stream.
+			reader.join();
+		} catch (InterruptedException e) {
+			// The thread was interrupted; nothing to do; finish stopping.
+		}
+		reader = null;
 		notify();
-		//Fire one last time to ensure listeners have gotten everything.
+		// Fire one last time to ensure listeners have gotten everything.
 		this.fireNewDataEvent();
 	}
 	
@@ -116,50 +111,33 @@ public class StreamGobbler implements Runnable {
 		if(isRunning())
 			stop();
 		line = null;
-		t = null;
+		reader = null;
 		is = null;
 	}
 	
 	/**
-	 * Fires new events to everything that is monitering this stream. Then clears
+	 * Fires new events to everything that is monitoring this stream. Then clears
 	 * the current line of data.
 	 */
 	private void fireNewDataEvent() {
-		//Implement our own lock since using synchronized causes a deadlock here
-	/*	while(locked) {
-			try {
-				wait(10);
-			} catch(Exception e) {}
-		}
-		locked = true;*/
 		for(int i = 0; i < listeners.size(); i++)
 		{
 		
 			listeners.get(i).handleDataEvent(line.toString());
 		}
 		line.delete(0, line.length());
-		locked = false;
 	}
 	
 	public void fireNewDataEvent(String l) {
-		//Implement our own lock since using synchronized causes a deadlock here
-	/*	while(locked) {
-			try {
-				wait(10);
-			} catch(Exception e) {}
-		}
-		locked = true;*/
 		for(int i = 0; i < listeners.size(); i++)
 		{
 			listeners.get(i).handleDataEvent(l);
 		}
-//		line.delete(0, line.length());
-		locked = false;
 	}
 	
 	/**
 	 * Registers the provided listener to get data events.
-	 * @param l A listener that needs to moniter the stream.
+	 * @param l A listener that needs to monitor the stream.
 	 */
 	public void addDataListener(IGobblerListener l) {
 		
@@ -173,8 +151,8 @@ public class StreamGobbler implements Runnable {
 	}
 
 	/**
-	 * Unregisters the provied listener from getting new data events.
-	 * @param l A listener that is monitering the stream and should be removed
+	 * Unregisters the provided listener from getting new data events.
+	 * @param l A listener that is monitoring the stream and should be removed
 	 */
 	public void removeDataListener(IGobblerListener l) {
 		
@@ -184,7 +162,7 @@ public class StreamGobbler implements Runnable {
 
 	/**
 	 * Returns a list of all of the <code>IGobblerListeners</code> that
-	 * are lstening for data events.
+	 * are listening for data events.
 	 * @return ArrayList of all of the listeners registered.
 	 */
 	public ArrayList<IGobblerListener> getDataListeners() {
@@ -193,9 +171,6 @@ public class StreamGobbler implements Runnable {
 	
 	private ArrayList<IGobblerListener> listeners;
 	private StringBuilder line;
-	private Thread t;
-	private InputStream is;
-	
-	@SuppressWarnings("unused")
-	private boolean locked;
+	private Thread reader;
+	private InputStream is;	
 }
