aff456c6acebcfacc7b38ae6157adb99ab91f986
Sami Wagiaalla <swagiaal@redhat.com>
2012-11-22 14:49:32 -0500

    Change 'Run' button to run script locally as current user
    Change-Id: I50e6bafe8bcba415a183b18bfe54ec17005da780
    Reviewed-on: https://git.eclipse.org/r/8871
    Tested-by: Hudson CI
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

---
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/systemtap/ui/ide/actions/RunScriptLocallyAction.java
@@ -0,0 +1,8 @@
+package org.eclipse.linuxtools.systemtap.ui.ide.actions;
+
+public class RunScriptLocallyAction extends RunScriptAction {
+
+	public RunScriptLocallyAction() {
+		this.setLocalScript(true);
+	}
+}
