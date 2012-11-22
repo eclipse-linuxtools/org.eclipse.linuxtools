f5e426542eb2e75d0426a95020b906339d0ddb74
Sami Wagiaalla <swagiaal@redhat.com>
2012-11-22 13:59:05 -0500

    Do not remove console after the script is stopped
    Change-Id: I513815d286dcdd47a17cc3ed58b1dfb377a688ec
    Reviewed-on: https://git.eclipse.org/r/8870
    Tested-by: Hudson CI
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.ui.consolelog/src/org/eclipse/linuxtools/systemtap/ui/consolelog/structures/ScriptConsole.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.consolelog/src/org/eclipse/linuxtools/systemtap/ui/consolelog/structures/ScriptConsole.java
@@ -254,7 +254,6 @@ public class ScriptConsole extends IOConsole {
               cmd.removeErrorStreamListener(errorDaemon);
               cmd.removeInputStreamListener(consoleDaemon);
               setName(Localization.getString("ScriptConsole.Terminated") + super.getName());
-			ConsolePlugin.getDefault().getConsoleManager().removeConsoles(new IConsole[] {this});
 		}
 	}
 	
