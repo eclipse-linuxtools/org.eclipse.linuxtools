588ae404be57e8fac34b56fdcae1f5c1b41c3699
Alexander Kurtakov <akurtako@redhat.com>
2013-01-04 22:43:37 +0200

    Activators that do nothing don't have to be such.
    Change-Id: I48e8dc69382e112b8d1a71db3c07ef75e40bf43b
    Reviewed-on: https://git.eclipse.org/r/9465
    Tested-by: Hudson CI
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

--- profiling/org.eclipse.linuxtools.tools.launch.core/src/org/eclipse/linuxtools/tools/launch/core/Activator.java
+++ profiling/org.eclipse.linuxtools.tools.launch.core/src/org/eclipse/linuxtools/tools/launch/core/Activator.java
@@ -11,35 +11,8 @@
 
 package org.eclipse.linuxtools.tools.launch.core;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-
-public class Activator implements BundleActivator {
+public class Activator {
 	public static final String PLUGIN_ID = "org.eclipse.linuxtools.tools.launch.core"; //$NON-NLS-1$
 	public static final String UI_PLUGIN_ID = "org.eclipse.linuxtools.tools.launch.ui"; //$NON-NLS-1$
-
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void start(BundleContext bundleContext) {
-		Activator.context = bundleContext;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void stop(BundleContext bundleContext) {
-		Activator.context = null;
-	}
 
 }
