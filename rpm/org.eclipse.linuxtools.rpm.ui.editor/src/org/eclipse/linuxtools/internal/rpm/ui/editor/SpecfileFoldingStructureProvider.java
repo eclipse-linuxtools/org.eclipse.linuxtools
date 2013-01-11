18580202fc6c3c63817c2a4240b015dfef720e29
Alexander Kurtakov <akurtako@redhat.com>
2013-01-11 13:24:04 +0200

    No idea why Exception is caught here.
    But it shouldn't be catched if there is some positioning problem let's
    see it and fix it.
    
    Change-Id: If2e9273b35ba0dc3d427fba125456fecbeb84b50
    Reviewed-on: https://git.eclipse.org/r/9611
    Tested-by: Hudson CI
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

--- rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/internal/rpm/ui/editor/SpecfileFoldingStructureProvider.java
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/internal/rpm/ui/editor/SpecfileFoldingStructureProvider.java
@@ -110,15 +110,10 @@ public class SpecfileFoldingStructureProvider {
 	}
 
 	private void addFoldingRegions(Set<Position> regions, Object[] elements) {
-		Position position;
 		// add folding on the preamble section
-		try {
-			SpecfileElement element = (SpecfileElement) elements[0];
-			position = new Position(0, element.getLineStartPosition() - 1);
-			regions.add(position);
-		} catch (Exception exception){
-			//pass
-		}
+		SpecfileElement element = (SpecfileElement) elements[0];
+		Position position = new Position(0, element.getLineStartPosition() - 1);
+		regions.add(position);
 
 		for (int i = 0; i < elements.length; i++) {
 			SpecfileElement startElement = (SpecfileElement) elements[i];
