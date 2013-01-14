d510bf107f406fba154f885b2841708a4b64b133
Alexander Kurtakov <akurtako@redhat.com>
2013-01-14 14:31:43 +0200

    Unneeded activator which even had empty implementation of IStartup.
    Change-Id: I1aef47b4efbb5312358f2d0dba7a367c72c97036
    Reviewed-on: https://git.eclipse.org/r/9649
    Tested-by: Hudson CI
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.ui.graphicalrun/src/org/eclipse/linuxtools/internal/systemtap/ui/graphicalrun/GraphicalRunPlugin.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.graphicalrun/src/org/eclipse/linuxtools/internal/systemtap/ui/graphicalrun/GraphicalRunPlugin.java
@@ -11,60 +11,10 @@
 
 package org.eclipse.linuxtools.internal.systemtap.ui.graphicalrun;
 
-import org.eclipse.ui.IStartup;
-import org.eclipse.ui.plugin.*;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.osgi.framework.BundleContext;
 
 /**
  * The main plugin class to be used in the desktop.
  */
-public class GraphicalRunPlugin extends AbstractUIPlugin implements IStartup {
-	//The shared instance.
-	private static GraphicalRunPlugin plugin;
+public class GraphicalRunPlugin {
 	public static final String PLUGIN_ID = "org.eclipse.linuxtools.systemtap.ui.graphicalrun"; //$NON-NLS-1$
-	
-	/**
-	 * The constructor.
-	 */
-	public GraphicalRunPlugin() {
-		plugin = this;
-	}
-
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		plugin = null;
-	}
-
-	/**
-	 * Returns the shared instance.
-	 */
-	public static GraphicalRunPlugin getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
-	
-	public void earlyStartup() {}
 }
