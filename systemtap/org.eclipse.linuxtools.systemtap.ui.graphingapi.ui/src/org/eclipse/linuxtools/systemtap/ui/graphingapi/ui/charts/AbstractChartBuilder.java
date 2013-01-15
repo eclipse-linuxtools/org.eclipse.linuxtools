6fb6b2d7a3f116648290d9ee7e277e718ed977cd
Wainer S. Moschetta <wainersm@linux.vnet.ibm.com>
2013-01-15 11:08:04 -0200

    Fix erroneous license headers for systemtap.
    Some files were contributed properly but copyright statement are erroneous.
    
    Change-Id: I8f0a9e2216e1e030ff7226d86e894456f447cbdf
    Signed-off-by: Wainer S. Moschetta <wainersm@linux.vnet.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/9680
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.ui.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/ui/graphingapi/ui/charts/AbstractChartBuilder.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/ui/graphingapi/ui/charts/AbstractChartBuilder.java
@@ -1,13 +1,12 @@
 /****************************************************************
- * Licensed Material - Property of IBM
- *
- * ****-***
- *
- * (c) Copyright IBM Corp. 2006.  All rights reserved.
- *
- * US Government Users Restricted Rights - Use, duplication or
- * disclosure restricted by GSA ADP Schedule Contract with
- * IBM Corp.
+ * Copyright (c) 2006-2013 IBM Corp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM - initial API and implementation
  *
  ****************************************************************
  */
