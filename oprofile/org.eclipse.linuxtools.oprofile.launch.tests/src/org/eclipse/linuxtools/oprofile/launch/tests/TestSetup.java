964f7901fe1c1c259e8988b3d7aebe5321ff0156
Camilo Bernal <cabernal@redhat.com>
2013-01-21 16:56:59 -0500

    Separate opcontrol and ui functionality in oprofile's event config tab.
    Created abstract class to contain all ui code, leaving opcontrol
    dependent functionality to be provided in a separate class and
    allowing for better mocking during testing.
    
    Change-Id: I0ac108793d5843db59c85598b5702111144b4fde
    Reviewed-on: https://git.eclipse.org/r/9719
    Tested-by: Hudson CI
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>
    IP-Clean: Roland Grunberg <rgrunber@redhat.com>
    Tested-by: Roland Grunberg <rgrunber@redhat.com>

--- oprofile/org.eclipse.linuxtools.oprofile.launch.tests/src/org/eclipse/linuxtools/oprofile/launch/tests/TestSetup.java
+++ oprofile/org.eclipse.linuxtools.oprofile.launch.tests/src/org/eclipse/linuxtools/oprofile/launch/tests/TestSetup.java
@@ -125,9 +125,9 @@ public class TestSetup extends AbstractTest {
 	
 	private static class OprofileTestingEventConfigTab extends OprofileEventConfigTab {
 		@Override
-		protected boolean getTimerMode() { return false; }
+		protected boolean getOprofileTimerMode() { return false; }
 		@Override
-		protected int getNumberOfCounters() { return 1; }
+		protected int getNumberOfOprofileCounters() { return 1; }
 		@Override
 		protected boolean checkEventSetupValidity(int counter, String name, int maskValue) { return true; }
 		@Override
