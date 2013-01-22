aa176b863cd4d8b89ef87f28ae4ff5ce601e9721
Sami Wagiaalla <swagiaal@redhat.com>
2013-01-22 16:01:08 -0500

    Make kernel sources auto-discovered. Fixes #388525
    Change-Id: Ib982f01273fc0eca53268744d210dfc497d6ef17
    Reviewed-on: https://git.eclipse.org/r/9849
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/preferences/PreferenceInitializer.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/preferences/PreferenceInitializer.java
@@ -10,17 +10,25 @@
 
 package org.eclipse.linuxtools.internal.systemtap.ui.ide.preferences;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferenceConverter;
 import org.eclipse.linuxtools.internal.systemtap.ui.ide.IDEPlugin;
 import org.eclipse.linuxtools.internal.systemtap.ui.ide.editors.stp.STPColorConstants;
+import org.eclipse.linuxtools.tools.launch.core.factory.RuntimeProcessFactory;
 import org.eclipse.swt.graphics.RGB;
 
 
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
+	private static final String[] KERNEL_SOURCE_PATH = {"/usr/src/kernels/{kernel_version}", //$NON-NLS-1$
+		"/usr/src/linux", //$NON-NLS-1$
+		"/usr/src/linux-{kernel_version}" }; //$NON-NLS-1$
+
 	@Override
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = IDEPlugin.getDefault().getPreferenceStore();
@@ -30,20 +38,19 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(IDEPreferenceConstants.P_REMOTE_PROBES, false);
 
 		//ide.path
-//		store.setDefault(PreferenceConstants.P_DEFAULT_TAPSET, "/usr/share/systemtap/tapset");
-		store.setDefault(IDEPreferenceConstants.P_KERNEL_SOURCE, "");
+		store.setDefault(IDEPreferenceConstants.P_KERNEL_SOURCE, getKernelSourceLocation());
 		store.setDefault(IDEPreferenceConstants.P_EXCLUDED_KERNEL_SOURCE,
-				"CVS/" + File.pathSeparator +
-				".svn/" + File.pathSeparator +
-				"{arch}/" + File.pathSeparator +
-				".arch-ids/" + File.pathSeparator +
-				".bzr/" + File.pathSeparator +
-				"debian/" + File.pathSeparator +
-				".git/");
+				"CVS/" + File.pathSeparator + //$NON-NLS-1$
+				".svn/" + File.pathSeparator + //$NON-NLS-1$
+				"{arch}/" + File.pathSeparator + //$NON-NLS-1$
+				".arch-ids/" + File.pathSeparator + //$NON-NLS-1$
+				".bzr/" + File.pathSeparator + //$NON-NLS-1$
+				"debian/" + File.pathSeparator + //$NON-NLS-1$
+				".git/"); //$NON-NLS-1$
 		store.setDefault(IDEPreferenceConstants.P_REMOTE_LOCAL_KERNEL_SOURCE, PathPreferencePage.LOCAL);
 
 		//ide.stap.tapsets
-		store.setDefault(IDEPreferenceConstants.P_TAPSETS, "");
+		store.setDefault(IDEPreferenceConstants.P_TAPSETS, ""); //$NON-NLS-1$
 
 		//ide.editor
 		PreferenceConverter.setDefault(store, IDEPreferenceConstants.P_EDITOR_BACKGROUND, new RGB(255,255,255));
@@ -53,14 +60,14 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(IDEPreferenceConstants.P_USE_CODE_ASSIST, true);
 		store.setDefault(IDEPreferenceConstants.P_COMPLETION, IDEPreferenceConstants.P_COMPLETION_INSERT);
 		store.setDefault(IDEPreferenceConstants.P_ACTIVATION_DELAY, 200);
-		store.setDefault(IDEPreferenceConstants.P_ACTIVATION_TRIGGER, ".");
+		store.setDefault(IDEPreferenceConstants.P_ACTIVATION_TRIGGER, "."); //$NON-NLS-1$
 
 		//ide.editor.conditionalfilters
 		store.setDefault(IDEPreferenceConstants.P_CONDITIONAL_FILTERS,
-				"if(pid=currentpid)" + File.pathSeparator +
-				"if(execname=cmdname)" + File.pathSeparator +
-				"if(cpu=0)" + File.pathSeparator +
-				"if(caller=functionname)");
+				"if(pid=currentpid)" + File.pathSeparator + //$NON-NLS-1$
+				"if(execname=cmdname)" + File.pathSeparator + //$NON-NLS-1$
+				"if(cpu=0)" + File.pathSeparator + //$NON-NLS-1$
+				"if(caller=functionname)"); //$NON-NLS-1$
 
 
 		//ide.stap.stapoptions
@@ -69,7 +76,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		}
 
 		for(int i=0; i<IDEPreferenceConstants.P_STAP_OPTS.length; i++) {
-			store.setDefault(IDEPreferenceConstants.P_STAP_OPTS[i], "");
+			store.setDefault(IDEPreferenceConstants.P_STAP_OPTS[i], ""); //$NON-NLS-1$
 		}
 
 		//ide.editor.syntaxcoloring
@@ -82,4 +89,28 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		PreferenceConverter.setDefault(store, IDEPreferenceConstants.P_STP_STRING_COLOR, STPColorConstants.STP_STRING);
 
 	}
+
+	private String getKernelSourceLocation(){
+		// Find out the version of the currently running kernel.
+		String version = ""; //$NON-NLS-1$
+		try {
+			Process process = RuntimeProcessFactory.getFactory().exec("uname -r", null, null);//$NON-NLS-1$
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			version = reader.readLine();
+		} catch (IOException e) {
+			// Could not run uname use an empty String
+		}
+
+		// Go over the search path looking for the file System.map
+		for (String path : KERNEL_SOURCE_PATH) {
+			path = path.replace("{kernel_version}", version);  //$NON-NLS-1$
+			File file = new File(path+ "/System.map"); //$NON-NLS-1$
+			if (file.exists()){
+				return path;
+			}
+		}
+
+		return ""; //$NON-NLS-1$
+	}
+
 }
