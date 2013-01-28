37245926348b8437138f6e1e3ed21876a098516b
Alexander Kurtakov <akurtako@redhat.com>
2013-01-28 20:05:10 +0200

    StubbyLog can be final.
    Change-Id: Iae4dcbb71621d61f1b2270ea05da290351b81cb9
    Reviewed-on: https://git.eclipse.org/r/9965
    Tested-by: Hudson CI
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

--- rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/internal/rpmstubby/StubbyLog.java
+++ rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/internal/rpmstubby/StubbyLog.java
@@ -18,15 +18,15 @@ import org.eclipse.core.runtime.Status;
 /**
  * The logger of convenience for the Specfile Plug-In.
  */
-public class StubbyLog {
-	
+public final class StubbyLog {
+
 	private StubbyLog() {
 		//don't allow instantiation
 	}
 
    /**
     * Log the specified information.
-    * 
+    *
     * @param message A human-readable message, localized to the
     *           current locale.
     */
@@ -36,7 +36,7 @@ public class StubbyLog {
 
    /**
     * Log the specified error.
-    * 
+    *
     * @param exception A low-level exception.
     */
    public static void logError(Throwable exception) {
@@ -45,7 +45,7 @@ public class StubbyLog {
 
    /**
     * Log the specified error.
-    * 
+    *
     * @param message A human-readable message, localized to the
     *           current locale.
     * @param exception A low-level exception, or <code>null</code>
@@ -57,7 +57,7 @@ public class StubbyLog {
 
    /**
     * Log the specified information.
-    * 
+    *
     * @param severity The severity; one of the following:
     *           <code>IStatus.OK</code>,
     *           <code>IStatus.ERROR</code>,
@@ -78,7 +78,7 @@ public class StubbyLog {
 
    /**
     * Create a status object representing the specified information.
-    * 
+    *
     * @param severity The severity; one of the following:
     *           <code>IStatus.OK</code>,
     *           <code>IStatus.ERROR</code>,
@@ -101,7 +101,7 @@ public class StubbyLog {
 
    /**
     * Log the given status.
-    * 
+    *
     * @param status The status to log.
     */
    public static void log(IStatus status) {
