1d1fd23ee6e83d16a06a98941848935af3351492
Alexander Kurtakov <akurtako@redhat.com>
2013-02-11 18:34:05 +0200

    Remove unused imports in StapNewWizard.
    Change-Id: I9c4b621ea23259fc97a4071bdd2aa181a98b8d64
    Reviewed-on: https://git.eclipse.org/r/10281
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/systemtap/ui/ide/wizards/StapNewWizard.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/systemtap/ui/ide/wizards/StapNewWizard.java
@@ -11,23 +11,15 @@
 package org.eclipse.linuxtools.systemtap.ui.ide.wizards;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ResourceBundle;
 
-import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
