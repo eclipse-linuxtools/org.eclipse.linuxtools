e03826a09c6335683da13b15e0696c40c47a48c0
Alexander Kurtakov <akurtako@redhat.com>
2013-02-12 01:37:37 +0200

    Activator unneeded.
    Change-Id: I86cdea8dbb50d3e61652106b123289c891ced4e5
    Reviewed-on: https://git.eclipse.org/r/10294
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.ui.structures/src/org/eclipse/linuxtools/internal/systemtap/ui/structures/StructuresPlugin.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.structures/src/org/eclipse/linuxtools/internal/systemtap/ui/structures/StructuresPlugin.java
@@ -11,58 +11,13 @@
 
 package org.eclipse.linuxtools.internal.systemtap.ui.structures;
 
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
-
 /**
  * The main plugin class to be used in the desktop.
+ *
+ * @since 2.0
  */
-public class StructuresPlugin extends AbstractUIPlugin {
+public interface StructuresPlugin {
 
-	public static final String PLUGIN_ID = "org.eclipse.linuxtools.systemtap.ui.structures"; //$NON-NLS-1$
-	//The shared instance.
-	private static StructuresPlugin plugin;
-	
-	/**
-	 * The constructor.
-	 */
-	public StructuresPlugin() {
-		plugin = this;
-	}
+	String PLUGIN_ID = "org.eclipse.linuxtools.systemtap.ui.structures"; //$NON-NLS-1$
 
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		plugin = null;
-	}
-
-	/**
-	 * Returns the shared instance.
-	 */
-	public static StructuresPlugin getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
 }
