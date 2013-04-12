81d00a780cfc4397b921d3eda4f99a5b0054cc21
Alexander Kurtakov <akurtako@redhat.com>
2013-04-12 23:08:50 +0300

    valgrind: o.e.l.valgrind.tests simplified.
    * No need to be singleton.
    * No need for activator.
    
    Change-Id: I3782780224fdb7dd7995fa66ded8c8018ed64f56
    Reviewed-on: https://git.eclipse.org/r/11872
    Tested-by: Hudson CI
    Reviewed-by: Camilo Bernal <cabernal@redhat.com>
    IP-Clean: Camilo Bernal <cabernal@redhat.com>
    Tested-by: Camilo Bernal <cabernal@redhat.com>

--- valgrind/org.eclipse.linuxtools.valgrind.tests/src/org/eclipse/linuxtools/internal/valgrind/tests/ValgrindTestsPlugin.java
+++ valgrind/org.eclipse.linuxtools.valgrind.tests/src/org/eclipse/linuxtools/internal/valgrind/tests/ValgrindTestsPlugin.java
@@ -10,15 +10,13 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.valgrind.tests;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
 
-public class ValgrindTestsPlugin extends AbstractUIPlugin {
+public class ValgrindTestsPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.linuxtools.valgrind.tests"; //$NON-NLS-1$
-	
-	// Java Runtime System Properties	
+
+	// Java Runtime System Properties
 	/**
 	 *  usage: -Declipse.valgrind.tests.generateFiles=<yes|no> [default: no]
 	 *  if yes, will run Valgrind and store its output files for each test under
@@ -27,7 +25,7 @@ public class ValgrindTestsPlugin extends AbstractUIPlugin {
 	 */
 	public static final String SYSTEM_PROPERTY_GENERATE_FILES = "eclipse.valgrind.tests.generateFiles"; //$NON-NLS-1$
 	public static final boolean GENERATE_FILES = System.getProperty(SYSTEM_PROPERTY_GENERATE_FILES, "no").equals("yes"); //$NON-NLS-1$ //$NON-NLS-2$
-	
+
 	/**
 	 *  usage: -Declipse.valgrind.tests.runValgrind=<yes|no> [default: yes]
 	 *  if yes, will run Valgrind as in a normal launch
@@ -36,45 +34,8 @@ public class ValgrindTestsPlugin extends AbstractUIPlugin {
 	public static final String SYSTEM_PROPERTY_RUN_VALGRIND = "eclipse.valgrind.tests.runValgrind"; //$NON-NLS-1$
 	// generateFiles implies runValgrind
 	public static final boolean RUN_VALGRIND = GENERATE_FILES || System.getProperty(SYSTEM_PROPERTY_RUN_VALGRIND, "yes").equals("yes"); //$NON-NLS-1$ //$NON-NLS-2$
-	
+
 	// Launch config attribute to mock valgrind's exit code
 	public static final String ATTR_MOCK_EXIT_CODE = PLUGIN_ID + ".MOCK_EXIT_CODE"; //$NON-NLS-1$
-	
-	// The shared instance
-	private static ValgrindTestsPlugin plugin;
 
-	/**
-	 * The constructor
-	 */
-	public ValgrindTestsPlugin() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static ValgrindTestsPlugin getDefault() {
-		return plugin;
-	}
 }
