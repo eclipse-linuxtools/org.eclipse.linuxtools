aae102f60cf1d64f5071a9bbea5ddb4fa9af8276
Camilo Bernal <cabernal@redhat.com>
2013-04-29 17:16:34 -0400

    Perf: Remove o.e.l.perf.tests.runPerf maven flag from parent pom.xml
    This flag is only used in a single test, which already checks if the perf
    tool is installed before running.
    
    Change-Id: I980af50a90a165ee3f0ea3092d12292bfd11e9d9
    Reviewed-on: https://git.eclipse.org/r/12366
    Tested-by: Hudson CI
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

--- perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/AllPerfTests.java
+++ perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/AllPerfTests.java
@@ -16,27 +16,14 @@ import junit.framework.TestSuite;
 public class AllPerfTests {
 
 	public static Test suite() {
-		/**
-		 * Java system properties. usage:
-		 * -Dorg.eclipse.linuxtools.perf.tests.runPerf=<yes|no>
-		 * [default: yes] if yes, will run the launch tests no, will skip the
-		 * launch tests (they all require perf to be installed)
-		 */
-		String SYSTEM_PROPERTY_RUN_PERF = "org.eclipse.linuxtools.perf.tests.runPerf"; //$NON-NLS-1$
-		boolean RUN_PERF = System
-				.getProperty(SYSTEM_PROPERTY_RUN_PERF, "yes").equals("yes"); //$NON-NLS-1$ //$NON-NLS-2$
-		TestSuite suite = new TestSuite(
-				"Test for org.eclipse.linuxtools.perf.tests"); //$NON-NLS-1$
+		TestSuite suite = new TestSuite("Test for org.eclipse.linuxtools.perf.tests"); //$NON-NLS-1$
 
 		suite.addTestSuite(LaunchTabsTest.class);
 		suite.addTestSuite(ModelTest.class);
 		suite.addTestSuite(DataManipulatorTest.class);
 		suite.addTestSuite(SaveSessionTest.class);
 		suite.addTestSuite(StatsComparisonTest.class);
-
-		if (RUN_PERF) {
-			suite.addTestSuite(LaunchTest.class);
-		}
+		suite.addTestSuite(LaunchTest.class);
 
 		return suite;
 	}
