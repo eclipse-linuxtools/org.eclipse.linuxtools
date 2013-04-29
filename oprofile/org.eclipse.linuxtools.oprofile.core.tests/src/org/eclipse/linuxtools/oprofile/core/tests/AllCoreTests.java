e252684d28aa69772e6e25af850c561375652ed7
Camilo Bernal <cabernal@redhat.com>
2013-04-29 15:53:03 -0400

    Oprofile: Remove o.e.l.oprofile.*.runOprofile flags from pom.xml
    Tests are properly mocked and do not require functionality from the
    oprofile tools or kerndel module.
    
    Change-Id: I4835692f022ed0a4bb93eb4855e2bd22f6c01566
    Reviewed-on: https://git.eclipse.org/r/12356
    Tested-by: Hudson CI
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

--- oprofile/org.eclipse.linuxtools.oprofile.core.tests/src/org/eclipse/linuxtools/oprofile/core/tests/AllCoreTests.java
+++ oprofile/org.eclipse.linuxtools.oprofile.core.tests/src/org/eclipse/linuxtools/oprofile/core/tests/AllCoreTests.java
@@ -16,26 +16,13 @@ import junit.framework.TestSuite;
 public class AllCoreTests {
 
 	public static Test suite() {
-		
-		/**
-		 *  Java system properties.
-		 *  usage: -Dorg.eclipse.linuxtools.oprofile.tests.runOprofile=<yes|no> [default: yes]
-		 *  if yes, will run the core tests
-		 *     no, will skip the core tests (they all require oOProfile to be set up)
-		 */
-		String SYSTEM_PROPERTY_RUN_OPROFILE = "org.eclipse.linuxtools.oprofile.tests.runOprofile"; //$NON-NLS-1$
-		boolean RUN_OPROFILE = System.getProperty(SYSTEM_PROPERTY_RUN_OPROFILE, "yes").equals("yes"); //$NON-NLS-1$ //$NON-NLS-2$
-		
 		TestSuite suite = new TestSuite("Test for org.eclipse.linuxtools.oprofile.core.tests"); //$NON-NLS-1$
-		
-		if (RUN_OPROFILE) {
+
 		suite.addTestSuite(TestModelDataParse.class);
 		suite.addTestSuite(TestSessionsParse.class);
 		suite.addTestSuite(TestCheckEventsParse.class);
 		suite.addTestSuite(TestInfoParse.class);
 		suite.addTestSuite(TestDataModel.class);
-		}
-		
 		suite.addTestSuite(TestDummy.class);
 		
 		return suite;
--- oprofile/org.eclipse.linuxtools.oprofile.launch.tests/src/org/eclipse/linuxtools/oprofile/launch/tests/AllLaunchTests.java
+++ oprofile/org.eclipse.linuxtools.oprofile.launch.tests/src/org/eclipse/linuxtools/oprofile/launch/tests/AllLaunchTests.java
@@ -14,30 +14,16 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public class AllLaunchTests {
-	
-
 
 	public static Test suite() {
-		/**
-		 *  Java system properties.
-		 *  usage: -Dorg.eclipse.linuxtools.oprofile.tests.runOprofile=<yes|no> [default: yes]
-		 *  if yes, will run the launch tests
-		 *     no, will skip the launch tests (they all require oprofile to be set up)
-		 */
-		String SYSTEM_PROPERTY_RUN_OPROFILE = "org.eclipse.linuxtools.oprofile.tests.runOprofile"; //$NON-NLS-1$
-		boolean RUN_OPROFILE = System.getProperty(SYSTEM_PROPERTY_RUN_OPROFILE, "yes").equals("yes"); //$NON-NLS-1$ //$NON-NLS-2$
 		TestSuite suite = new TestSuite("Test for org.eclipse.linuxtools.oprofile.launch.tests"); //$NON-NLS-1$
-		
-		if (RUN_OPROFILE) {
-			suite.addTestSuite(TestLaunching.class);
-			suite.addTestSuite(TestManualLaunching.class);
-			suite.addTestSuite(TestSetup.class);
-			suite.addTestSuite(TestLaunchingExternalProject.class);
-		}
-		
+
+		suite.addTestSuite(TestLaunching.class);
+		suite.addTestSuite(TestManualLaunching.class);
+		suite.addTestSuite(TestSetup.class);
+		suite.addTestSuite(TestLaunchingExternalProject.class);
 		suite.addTestSuite(TestDummy.class);
-		
+
 		return suite;
 	}
-
 }
