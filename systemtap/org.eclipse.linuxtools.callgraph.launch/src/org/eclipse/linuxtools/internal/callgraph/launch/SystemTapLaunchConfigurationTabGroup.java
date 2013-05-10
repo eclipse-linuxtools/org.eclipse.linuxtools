124f73a79321d989dea4a7fe36be8fcad1508be8
Alexander Kurtakov <akurtako@redhat.com>
2013-05-10 17:22:51 +0300

    callgraph: launch tab hierarchy simplified
    Callgraph launch configuration tab group had useless abstract parent.
    Just remove it.
    
    Change-Id: I7c2f2ed0a2b52ddc3872c39415489a770c752bc2
    Reviewed-on: https://git.eclipse.org/r/12710
    Tested-by: Hudson CI
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>
    IP-Clean: Roland Grunberg <rgrunber@redhat.com>
    Tested-by: Roland Grunberg <rgrunber@redhat.com>

--- systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapLaunchConfigurationTabGroup.java
+++ systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapLaunchConfigurationTabGroup.java
@@ -4,22 +4,27 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Red Hat - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.linuxtools.internal.callgraph.launch;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.linuxtools.profiling.launch.ProfileLaunchConfigurationTabGroup;
 
 /**
  * Expansion of the SystemTapLCTG, which was a stripped down version of the ProfileLaunchConfigurationTabGroup
- * 
+ *
  * @author chwang
  *
  */
-public class SystemTapLaunchConfigurationTabGroup extends SystemTapLCTG{
+public class SystemTapLaunchConfigurationTabGroup extends ProfileLaunchConfigurationTabGroup {
 
 	@Override
 	public AbstractLaunchConfigurationTab[] getProfileTabs() {
@@ -28,4 +33,13 @@ public class SystemTapLaunchConfigurationTabGroup extends SystemTapLCTG{
 			};
 	}
 
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		ArrayList<AbstractLaunchConfigurationTab> tabs = new ArrayList<AbstractLaunchConfigurationTab>();
+
+		tabs.addAll(Arrays.asList(getProfileTabs()));
+
+
+		setTabs(tabs.toArray(new AbstractLaunchConfigurationTab[tabs.size()]));
+	}
 }
