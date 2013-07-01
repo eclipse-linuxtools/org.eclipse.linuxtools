47eeed4670e96cd506b2ba1860758d7c23bbd025
Rafael Peria de Sene <rpsene@linux.vnet.ibm.com>
2013-07-01 14:25:33 -0300

    [linuxtools settings | bug 412003] - This commit includes the capability to change the ComboFieldEditor according the selected path
    I have read and agree to abide term by the terms of the Eclipse Foundation Commiter Guidelines
    
    Change-Id: Ia09fffaec2e9fda5f3b152caf0ecb876e047793f
    Signed-off-by: Rafael Peria de Sene <rpsene@linux.vnet.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/14171
    Tested-by: Hudson CI
    Reviewed-by: Wainer dos Santos Moschetta <wainersm@linux.vnet.ibm.com>
    Reviewed-by: Rafael Medeiros Teixeira <rafaelmt@linux.vnet.ibm.com>
    IP-Clean: Rafael Medeiros Teixeira <rafaelmt@linux.vnet.ibm.com>
    Tested-by: Rafael Medeiros Teixeira <rafaelmt@linux.vnet.ibm.com>

--- profiling/org.eclipse.linuxtools.tools.launch.ui/src/org/eclipse/linuxtools/tools/launch/ui/properties/LinuxtoolsPathPropertyPage.java
+++ profiling/org.eclipse.linuxtools.tools.launch.ui/src/org/eclipse/linuxtools/tools/launch/ui/properties/LinuxtoolsPathPropertyPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation
+ * Copyright (c) 2011,2013 IBM Corporation
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Otavio Busatto Pontes <obusatto@br.ibm.com> - initial API and implementation
+ *    Rafael Peria de Sene <rpsene@br.ibm.com>
  *******************************************************************************/
 
 package org.eclipse.linuxtools.tools.launch.ui.properties;
@@ -21,7 +22,6 @@ import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
@@ -63,7 +63,7 @@ public class LinuxtoolsPathPropertyPage extends PropertyPage {
 				{"Custom", ""}, //$NON-NLS-1$  //$NON-NLS-2$
 	};
 	private StringFieldEditor linuxtoolsPath;
-	private ComboFieldEditor linuxtoolsPathCombo;
+	private CustomComboFieldEditor linuxtoolsPathCombo;
 	private IAdaptable element = null;
 	private Composite result;
 	private Button systemEnvButton, customButton;
@@ -107,6 +107,7 @@ public class LinuxtoolsPathPropertyPage extends PropertyPage {
 		layoutRadios.marginWidth= 0;
 		layoutRadios.numColumns= 1;
 		radios.setLayout(layoutRadios);
+
 		Composite space = new Composite(result, SWT.NONE);
 
 		boolean systemPathSelected = getPreferenceStore().getBoolean(LaunchCoreConstants.LINUXTOOLS_PATH_SYSTEM_NAME);
@@ -126,7 +127,7 @@ public class LinuxtoolsPathPropertyPage extends PropertyPage {
 		customButton.setSelection(!systemPathSelected);
 
 		//Add combo box
-		linuxtoolsPathCombo = new ComboFieldEditor(
+		linuxtoolsPathCombo = new CustomComboFieldEditor(
 									LINUXTOOLS_PATH_COMBO_NAME,
 									Messages.LINUXTOOLS_PATH_COMBO,
 									paths,
@@ -138,8 +139,9 @@ public class LinuxtoolsPathPropertyPage extends PropertyPage {
 			@Override
 			public void propertyChange(PropertyChangeEvent event) {
 				customSelected = event.getNewValue().toString().equals(""); //$NON-NLS-1$
-				if (!customSelected)
+				if (!customSelected){
 					linuxtoolsPath.setStringValue(event.getNewValue().toString());
+				}
 				updateOptionsEnable();
 			}
 		});
@@ -157,7 +159,7 @@ public class LinuxtoolsPathPropertyPage extends PropertyPage {
 		customSelected = selected.equals(""); //$NON-NLS-1$
 		getPreferenceStore().setDefault(LaunchCoreConstants.LINUXTOOLS_PATH_NAME, LinuxtoolsPathProperty.getInstance().getLinuxtoolsPathDefault());
 		linuxtoolsPath.load();
-
+		linuxtoolsPathCombo.setSelectedValue(linuxtoolsPath.getStringValue());
 		Dialog.applyDialogFont(result);
 		updateOptionsEnable();
 		return result;
