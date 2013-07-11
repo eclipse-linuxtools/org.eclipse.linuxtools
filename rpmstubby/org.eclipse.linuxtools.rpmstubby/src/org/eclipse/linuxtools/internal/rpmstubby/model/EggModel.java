73e5e28fc1f81ee33925f096d512a9da6058c7ab
Neil Guzman <nguzman@redhat.com>
2013-07-11 14:17:48 -0400

    RPMStubby: Python Egg template B#350065
    Simple parser to parse a setup.py for its
    setup properties to be put into a python specfile
    template.
    
    Todo:
        o support triple quotations
    
    Amend:
        o added javadoc
        o fixed case of setup(...) function being 1 line
        o handles empty files
        o handles incorrect setup(...) function
        o added StubbyLog.logError in catch statements
        o added as popup action when right-clicking setup.py files
        o ignore function calls, clean up returned value
    
    Change-Id: Ie740144d8dca55ebde547d4677daa3418347deea
    Signed-off-by: Neil Guzman <nguzman@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/14594
    Tested-by: Hudson CI
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

---
+++ rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/internal/rpmstubby/model/EggModel.java
@@ -0,0 +1,226 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Neil Guzman - python implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.internal.rpmstubby.model;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.linuxtools.internal.rpmstubby.StubbyLog;
+import org.eclipse.linuxtools.internal.rpmstubby.parser.CommonMetaData;
+import org.eclipse.linuxtools.internal.rpmstubby.parser.PythonEggParser;
+import org.eclipse.linuxtools.internal.rpmstubby.parser.ValidLicenses;
+
+/**
+ * Gives easy access to data from the python setup.py file.
+ *
+ */
+public class EggModel {
+
+	private static final String[] validLicenses = {
+		ValidLicenses.GPL,
+		ValidLicenses.ARTISTIC,
+		ValidLicenses.MIT,
+		ValidLicenses.APACHE,
+		ValidLicenses.PUBLIC_DOMAIN,
+		ValidLicenses.ZLIB,
+		ValidLicenses.RICOH_SOURCE_CODE,
+		ValidLicenses.VOVIDA_SOFTWARE,
+		ValidLicenses.INTEL_OPEN_SOURCE,
+		ValidLicenses.LGPL,
+		ValidLicenses.BSD,
+		ValidLicenses.QPL,
+		ValidLicenses.IBM_PUBLIC,
+		ValidLicenses.PHP,
+		ValidLicenses.MODIFIED_CNRI_OPEN_SOURCE,
+		ValidLicenses.CVW,
+		ValidLicenses.PYTHON,
+		ValidLicenses.SUN_INTERNET_STANDARDS_SOURCE,
+		ValidLicenses.JABBER_OPEN_SOURCE
+	};
+
+	private static final String LONG_DESCRIPTION = "long_description";
+	private static final String CLASSIFIERS = "classifiers";
+	private static final String INSTALL_REQUIRES = "install_requires";
+
+	private static final String FIX_ME = "#FIXME";
+
+	private PythonEggParser pyEggParser;
+
+	/**
+	 * Python egg setup.py file
+	 *
+	 * @param file The Python setup.py file
+	 */
+	public EggModel(IFile file) {
+		try {
+			pyEggParser = new PythonEggParser(file);
+		} catch (IOException e) {
+			StubbyLog.logError(e);
+		} catch (CoreException e) {
+			StubbyLog.logError(e);
+		}
+	}
+
+	/**
+	 * Get the value from one of the setup options.
+	 * If the value is empty, it will return #FIXME
+	 * It will also return #FIX_ME if it looks like a function
+	 *
+	 * @param option The option from the setup(...) function to get value of
+	 * @return The value of the option
+	 */
+	private String getValue(String option) {
+		String str = pyEggParser.getValue(option);
+
+		if (str.isEmpty() || pyEggParser.checkFunction(str)) {
+			str = FIX_ME;
+		}
+
+		return str;
+	}
+
+	/**
+	 * Get the list of values from one of the setup options
+	 * that stores a list of strings
+	 *
+	 * @param option The option from the setup(...) function to get value of
+	 * @return The list of values of the option
+	 */
+	private List<String> getValueList(String option) {
+		return pyEggParser.getValueList(option);
+	}
+
+	/**
+	 * Get the values from the classifiers option and
+	 * check to see if the keyword is in one of them
+	 *
+	 * @param keyword What to check for within the list of values in classifiers
+	 * @return The value within classifiers that contains the keyword
+	 */
+	private String getClassifiersList(String keyword) {
+		String rc = "";
+		List<String> list = getValueList(CLASSIFIERS);
+
+		for (String str : list) {
+			if (str.toLowerCase().contains(keyword)) {
+				rc = str;
+			}
+		}
+
+		return rc;
+	}
+
+	/**
+	 * Get the values from the install_requires option
+	 *
+	 * @return The values within install_requires
+	 */
+	public List<String> getInstallRequiresList() {
+		return getValueList(INSTALL_REQUIRES);
+	}
+
+	/**
+	 * Returns the package name.
+	 *
+	 * @return The package name.
+	 */
+	public String getSimplePackageName() {
+		return getValue(CommonMetaData.NAME);
+	}
+
+	/**
+	 * The simple package name with "eclipse-" prepended to make better RPM package name.
+	 *
+	 * @return The package
+	 */
+	public String getPackageName() {
+		return "eclipse-"+getSimplePackageName();
+	}
+
+	/**
+	 * Returns the version as retrieved by /feature/@version xpath with .qualifier removed if any.
+	 *
+	 * @return The version of the feature.
+	 */
+	public String getVersion() {
+		String version = getValue(CommonMetaData.VERSION);
+
+		if (!hasDigits(version)) {
+			version = "1 " + FIX_ME;
+		}
+
+		return version;
+	}
+
+	/**
+	 * Utitlity method to try and see if a string contains
+	 * digits within it
+	 *
+	 * @param str The string to check if it has digits
+	 * @return True if string contains digits
+	 */
+	public boolean hasDigits(String str) {
+		return str.matches(".*\\d.*");
+	}
+
+	/**
+	 * Returns the summary
+	 *
+	 * @return The package summary
+	 */
+	public String getSummary() {
+		return getValue(LONG_DESCRIPTION);
+	}
+
+	/**
+	 * Returns the license
+	 *
+	 * @return The license
+	 */
+	public String getLicense() {
+		String rawLicense = getClassifiersList(CommonMetaData.LICENSE).toLowerCase();
+		String license = "";
+
+		for (String valid : validLicenses) {
+			if (rawLicense.contains(valid.toLowerCase())) {
+					license += valid + ", ";
+			}
+		}
+
+		if (!license.isEmpty()) {
+			license = license.substring(0, license.length()-2);
+		} else {
+			license = FIX_ME;
+		}
+
+		return license;
+	}
+
+	/**
+	 * Returns the url
+	 *
+	 * @return The url
+	 */
+	public String getURL() {
+		return getValue(CommonMetaData.URL);
+	}
+
+	/**
+	 * Returns the description
+	 *
+	 * @return The description
+	 */
+	public String getDescription() {
+		return getValue(CommonMetaData.DESCRIPTION);
+	}
+}
---
+++ rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/internal/rpmstubby/parser/CommonMetaData.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Neil Guzman - python implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.internal.rpmstubby.parser;
+
+/**
+ * Meta-Data tags common within the files
+ *
+ */
+@SuppressWarnings("javadoc")
+public interface CommonMetaData {
+	String NAME = "name";
+	String DESCRIPTION = "description";
+	String VERSION = "version";
+	String LICENSE = "license";
+	String URL = "url";
+}
