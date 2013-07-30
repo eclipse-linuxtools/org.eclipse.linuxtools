08c26dbba947ff7be06f430d844087ddca039a31
Jeff Johnston <jjohnstn@redhat.com>
2013-07-30 16:44:49 -0400

    Increase libhover devhelp testing coverage
    Change-Id: Icc7c15c2fb235ee7beab18f08827e1efb7b78ca1
    Reviewed-on: https://git.eclipse.org/r/9119
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

--- libhover/org.eclipse.linuxtools.cdt.libhover.devhelp.tests/src/org/eclipse/linuxtools/cdt/libhover/devhelp/test/CheckDevhelp.java
+++ libhover/org.eclipse.linuxtools.cdt.libhover.devhelp.tests/src/org/eclipse/linuxtools/cdt/libhover/devhelp/test/CheckDevhelp.java
@@ -7,6 +7,7 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.cdt.libhover.devhelp.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -19,15 +20,24 @@ import java.util.Map;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.help.ITocContribution;
+import org.eclipse.help.ITopic;
+import org.eclipse.help.IUAElement;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.linuxtools.cdt.libhover.FunctionInfo;
 import org.eclipse.linuxtools.cdt.libhover.LibHoverInfo;
 import org.eclipse.linuxtools.cdt.libhover.LibhoverPlugin;
 import org.eclipse.linuxtools.internal.cdt.libhover.LibHover;
 import org.eclipse.linuxtools.internal.cdt.libhover.LibHoverLibrary;
+import org.eclipse.linuxtools.internal.cdt.libhover.devhelp.DevHelpPlugin;
+import org.eclipse.linuxtools.internal.cdt.libhover.devhelp.DevHelpToc;
+import org.eclipse.linuxtools.internal.cdt.libhover.devhelp.DevHelpTocProvider;
 import org.eclipse.linuxtools.internal.cdt.libhover.devhelp.ParseDevHelp;
+import org.eclipse.linuxtools.internal.cdt.libhover.devhelp.preferences.PreferenceConstants;
 import org.junit.Before;
 import org.junit.Test;
+import org.osgi.framework.FrameworkUtil;
 
 public class CheckDevhelp {
 	
@@ -74,6 +84,46 @@ public class CheckDevhelp {
 				found = true;
 		}
 		assertTrue(found);
+	}
+	@Test
+	public void testTocProvider() {
+		DevHelpTocProvider provider = new DevHelpTocProvider();
+		ITocContribution[] contributions = provider.getTocContributions("en_US"); //$NON-NLS-1$
+		assertTrue(contributions.length > 0);
+		ITocContribution c = contributions[0];
+		assertEquals(c.getId(), "org.eclipse.linuxtools.cdt.libhover.devhelp.toc"); //$NON-NLS-1$
+		assertTrue(c.isPrimary());
+		assertEquals(c.getCategoryId(), null);
+		assertEquals(c.getLocale(), "en_US"); //$NON-NLS-1$
+		String[] docs = c.getExtraDocuments();
+		assertTrue(docs.length == 0);
+		assertEquals(c.getLinkTo(), ""); //$NON-NLS-1$
+		assertEquals(c.getContributorId(), "org.eclipse.linuxtools.cdt.libhover.devhelp"); //$NON-NLS-1$
+	}
+	
+	@Test
+	public void testHelpTopic() {
+		String bundlePath = FrameworkUtil.getBundle(this.getClass()).getLocation();
+		bundlePath = bundlePath.substring(bundlePath.indexOf('/')); // need to chop extraneious chars from front to get actual path
+		// Decision to make this a fragment means we get the host's bundle, not this
+		bundlePath = bundlePath.replaceFirst("libhover.devhelp", "libhover.devhelp.tests"); //$NON-NLS-1$ //$NON-NLS-2$
+		IPath x = new Path(bundlePath);
+		// For some reason the bundle gets the host of this fragment instead of this fragment bundle
+		x = x.append("devhelp/html"); //$NON-NLS-1$
+		IPreferenceStore ps = DevHelpPlugin.getDefault().getPreferenceStore();
+		ps.setValue(PreferenceConstants.DEVHELP_DIRECTORY, x.toOSString());
+		DevHelpToc toc = new DevHelpToc();
+		ITopic[] topics = toc.getTopics();
+		assertTrue(topics.length > 0);
+		ITopic topic = topics[0];
+		assertTrue(topic.getLabel().startsWith("test1")); //$NON-NLS-1$
+		ITopic[] subtopics = topic.getSubtopics();
+		assertTrue(subtopics.length > 3);
+		IUAElement[] elements = topic.getChildren();
+		assertTrue(elements.length > 3);
+		String href = topic.getHref();
+		assertEquals(href, "/org.eclipse.linuxtools.cdt.libhover.devhelp/test1/index.html"); //$NON-NLS-1$
+		assertTrue(topic.isEnabled(null));
 	}
 	
 }
