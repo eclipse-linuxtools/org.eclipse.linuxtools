b17b5f68ff41ba54794bfe5451e1ab4bbb8f2640
Xavier Raynaud <xavier.raynaud@kalray.eu>
2013-08-05 16:04:45 +0200

    fix #414417 (Gcov working directory setting is not recognized)
    Change-Id: Iecb728e5d27b362fb483f9f6a8957c8e4c530b45
    Reviewed-on: https://git.eclipse.org/r/15138
    Tested-by: Hudson CI
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    IP-Clean: Jeff Johnston <jjohnstn@redhat.com>
    Tested-by: Jeff Johnston <jjohnstn@redhat.com>

--- gprof/org.eclipse.linuxtools.gprof.launch/src/org/eclipse/linuxtools/internal/gprof/launch/GprofLaunchConfigurationDelegate.java
+++ gprof/org.eclipse.linuxtools.gprof.launch/src/org/eclipse/linuxtools/internal/gprof/launch/GprofLaunchConfigurationDelegate.java
@@ -15,6 +15,7 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.gprof.launch;
 
+import java.io.File;
 import java.net.URI;
 
 import org.eclipse.cdt.debug.core.CDebugUtils;
@@ -55,14 +56,17 @@ public class GprofLaunchConfigurationDelegate extends ProfileLaunchConfiguration
 		IRemoteCommandLauncher launcher = RemoteProxyManager.getInstance().getLauncher(getProject());
 
 		URI workingDirURI = getProject().getLocationURI();
-		IPath workingDirPath = new Path(workingDirURI.getPath());
+		File workDir = getWorkingDirectory(config);
+		if (workDir == null) {
+			workDir = new File(System.getProperty("user.home", ".")); //$NON-NLS-1$ //$NON-NLS-2$
+		}
 		String arguments[] = getProgramArgumentsArray( config );
 
 		//add a listener for termination of the launch
 		ILaunchManager lmgr = DebugPlugin.getDefault().getLaunchManager();
 		lmgr.addLaunchListener(new LaunchTerminationWatcher(launch, exePath));
 
-		Process process = launcher.execute(exePath, arguments, getEnvironment(config), workingDirPath, monitor);
+		Process process = launcher.execute(exePath, arguments, getEnvironment(config), new Path(workDir.getAbsolutePath()), monitor);
 
 		DebugPlugin.newProcess( launch, process, renderProcessLabel( exePath.toOSString() ) );
 
