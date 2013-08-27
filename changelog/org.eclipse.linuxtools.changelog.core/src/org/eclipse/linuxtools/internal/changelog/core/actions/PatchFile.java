42b029a50df0cc001ae29eb3f1a8b16d507eda07
Alexander Kurtakov <akurtako@redhat.com>
2013-08-27 22:33:33 +0300

    changelog: PatchFile.EmptyStorage can be static.
    It doesn't refer to the enclosing class so can safely be static.
    
    Change-Id: I5a6c03d0d9cc2a083f9bab358fcc985979eb947c
    Reviewed-on: https://git.eclipse.org/r/15911
    Tested-by: Hudson CI
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

--- changelog/org.eclipse.linuxtools.changelog.core/src/org/eclipse/linuxtools/internal/changelog/core/actions/PatchFile.java
+++ changelog/org.eclipse.linuxtools.changelog.core/src/org/eclipse/linuxtools/internal/changelog/core/actions/PatchFile.java
@@ -21,13 +21,13 @@ import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.IPath;
 
 /**
- * 
+ *
  * @author klee
  *
  */
 public class PatchFile {
 
-	private class EmptyStorage implements IStorage {
+	private static class EmptyStorage implements IStorage {
 
 		public EmptyStorage() {
 		}
@@ -52,15 +52,15 @@ public class PatchFile {
 			return true;
 		}
 	}
-	
+
 	private IStorage storage = new EmptyStorage();
 	private ArrayList<PatchRangeElement> pranges = new ArrayList<PatchRangeElement>();
-	
+
 	private boolean newfile = false;
 	private boolean removedfile = false;
 	private IResource resource; // required only if dealing with change
-	
-	
+
+
 	public boolean isNewfile() {
 		return newfile;
 	}
@@ -72,24 +72,24 @@ public class PatchFile {
 	public boolean isRemovedFile() {
 		return removedfile;
 	}
-	
+
 	public void setRemovedFile(boolean removedfile) {
 		this.removedfile = removedfile;
 	}
-	
+
 	public PatchFile(IResource resource) {
 		this.resource = resource;
 	}
-	
+
 	public void addLineRange(int from, int to, boolean localChange) {
-	
+
 		pranges.add(new PatchRangeElement(from, to, localChange));
 	}
-	
+
 	public PatchRangeElement[] getRanges() {
 		Object[] tmpEle = pranges.toArray();
 		PatchRangeElement[] ret = new PatchRangeElement[tmpEle.length];
-		
+
 		for (int i = 0; i < tmpEle.length; i++) {
 			ret[i] = (PatchRangeElement) tmpEle[i];
 		}
@@ -100,7 +100,7 @@ public class PatchFile {
 	public IPath getPath() {
 		return resource.getFullPath();
 	}
-	
+
 	public IStorage getStorage() {
 		return storage;
 	}
@@ -108,36 +108,36 @@ public class PatchFile {
 	public void setStorage(IStorage storage) {
 		this.storage = storage;
 	}
-	
+
 	public IResource getResource() {
 		return resource;
 	}
-	
+
 	public int countRanges() {
 		return pranges.size();
 	}
-	
+
 	@Override
 	public boolean equals(Object o) {
-		
+
 		if (!(o instanceof PatchFile))
 			return false;
-		
+
 		PatchFile that = (PatchFile) o;
 		// check  fpath  +  count
 		if (!this.resource.equals(that.resource) ||
 				this.countRanges() != that.countRanges())
 			return false;
-		
+
 		// check range elements
 		PatchRangeElement[] thatsrange = that.getRanges();
-		
+
 		for(int i=0; i<this.countRanges();i++)
 			if (!thatsrange[i].equals(pranges.get(i)))
 				return false;
 		return true;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		int hash = resource.hashCode();
