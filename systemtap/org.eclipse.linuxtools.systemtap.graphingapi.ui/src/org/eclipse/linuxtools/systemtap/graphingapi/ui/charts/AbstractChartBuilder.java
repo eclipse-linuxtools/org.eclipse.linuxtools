3bdc32d39608349803f647e491f9d68bc3b3b8d0
Alexander Kurtakov <akurtako@redhat.com>
2013-10-02 10:49:45 +0300

    systemtap: Use system colors where possible.
    If one creates new Color it needs to be disposed manually which is not
    an option with these static fields. For now just replace with system
    color which are properly disposed by SWT itself. Not fixing the issue
    entirely but at least minimizes it.
    
    Change-Id: I0f9fe214df1c5d8f14f8e471e4ffc029f0c36583
    Reviewed-on: https://git.eclipse.org/r/16946
    Tested-by: Hudson CI
    Reviewed-by: Alexander Kurtakov <akurtako@redhat.com>
    IP-Clean: Alexander Kurtakov <akurtako@redhat.com>
    Tested-by: Alexander Kurtakov <akurtako@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/graphingapi/ui/charts/AbstractChartBuilder.java
+++ systemtap/org.eclipse.linuxtools.systemtap.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/graphingapi/ui/charts/AbstractChartBuilder.java
@@ -46,18 +46,18 @@ public abstract class AbstractChartBuilder extends Composite implements IUpdateL
 	protected int xseries;
 	protected int[] yseries;
 
-	protected static final Color WHITE = new Color(Display.getDefault(), 255, 255, 255);
-	protected static final Color BLACK = new Color(Display.getDefault(), 0, 0, 0);
-	protected static final Color RED = new Color(Display.getDefault(), 255, 0, 0);
+	protected static final Color WHITE = Display.getDefault().getSystemColor(SWT.COLOR_WHITE);
+	protected static final Color BLACK = Display.getDefault().getSystemColor(SWT.COLOR_BLACK);
+	protected static final Color RED = Display.getDefault().getSystemColor(SWT.COLOR_RED);
 
 	protected static final Color[] COLORS = {
-												new Color(Display.getDefault(), 255, 0, 0),
-												new Color(Display.getDefault(), 0, 255, 0),
-												new Color(Display.getDefault(), 0, 0, 255),
-												new Color(Display.getDefault(), 255, 255, 0),
-												new Color(Display.getDefault(), 255, 0, 255),
-												new Color(Display.getDefault(), 0, 255, 255),
-												new Color(Display.getDefault(), 0, 0, 0),
+												RED,
+												Display.getDefault().getSystemColor(SWT.COLOR_GREEN),
+												Display.getDefault().getSystemColor(SWT.COLOR_BLUE),
+												Display.getDefault().getSystemColor(SWT.COLOR_YELLOW),
+												Display.getDefault().getSystemColor(SWT.COLOR_MAGENTA),
+												Display.getDefault().getSystemColor(SWT.COLOR_CYAN),
+												BLACK,
 												new Color(Display.getDefault(), 64, 128, 128),
 												new Color(Display.getDefault(), 255, 165, 0),
 												new Color(Display.getDefault(), 128, 128, 128),
