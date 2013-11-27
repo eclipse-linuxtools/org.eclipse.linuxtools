9760b1372065624c01bc403c6adfb4cfc9633ec1
Rodrigo Fraxino Araujo <rfaraujo@br.ibm.com>
2013-11-27 06:25:34 -0200

    perf: fix for remote and sync perf after version update
    Change-Id: I1500f859e98e6894188c29efa80e52dff3f3952d
    Reviewed-on: https://git.eclipse.org/r/18944
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>
    IP-Clean: Roland Grunberg <rgrunber@redhat.com>
    Tested-by: Roland Grunberg <rgrunber@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/18997
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    IP-Clean: Jeff Johnston <jjohnstn@redhat.com>
    Tested-by: Jeff Johnston <jjohnstn@redhat.com>

--- perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/LaunchTest.java
+++ perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/LaunchTest.java
@@ -72,14 +72,14 @@ public class LaunchTest extends AbstractTest {
 
 	@Test
 	public void testDefaultRun() throws CoreException {
-		if (PerfCore.checkPerfInPath()) {
+		if (PerfCore.checkPerfInPath(null)) {
 			delegate.launch(wc, ILaunchManager.PROFILE_MODE, launch, null);
 		}
 	}
 
 	@Test
 	public void testClockEventRun() throws CoreException {
-		if (PerfCore.checkPerfInPath()) {
+		if (PerfCore.checkPerfInPath(null)) {
 			ArrayList<String> list = new ArrayList<String>();
 			list.addAll(Arrays.asList(new String[] { "cpu-clock", "task-clock",
 					"cycles" }));
