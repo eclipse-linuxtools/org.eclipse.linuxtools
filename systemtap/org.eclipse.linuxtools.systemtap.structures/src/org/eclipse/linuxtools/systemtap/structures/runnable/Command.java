97d561cdc947ee8a7ff325fb7b99fb2f0ee5d061
Jeff Johnston <jjohnstn@redhat.com>
2013-12-03 14:44:50 -0500

    Add Linux Tools path support for SystemTap
    - Add additional Command constructor that takes a project as input
    - Change RunScriptHandler to calculate the project, if possible
    - Add an additional ScriptConsole.runLocally method which takes
      project as input and uses RuntimeProcessFactory so that Linux
      Tools path will be used
      
    Change-Id: I6aa0cf1ead4dda393fecb2236491aa0b6dd912cd
    Reviewed-on: https://git.eclipse.org/r/19267
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    IP-Clean: Jeff Johnston <jjohnstn@redhat.com>
    Tested-by: Jeff Johnston <jjohnstn@redhat.com>

--- systemtap/org.eclipse.linuxtools.systemtap.structures/src/org/eclipse/linuxtools/systemtap/structures/runnable/Command.java
+++ systemtap/org.eclipse.linuxtools.systemtap.structures/src/org/eclipse/linuxtools/systemtap/structures/runnable/Command.java
@@ -17,13 +17,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.linuxtools.internal.systemtap.structures.StructuresPlugin;
 import org.eclipse.linuxtools.systemtap.structures.LoggingStreamDaemon;
 import org.eclipse.linuxtools.systemtap.structures.listeners.IGobblerListener;
-import org.eclipse.linuxtools.systemtap.structures.process.SystemtapProcessFactory;
+import org.eclipse.linuxtools.tools.launch.core.factory.RuntimeProcessFactory;
 
 
 
@@ -62,7 +63,11 @@ public class Command implements Runnable {
 	private String[] cmd;
 	private String[] envVars;
 	protected Process process;
-	private LoggingStreamDaemon logger;
+	/**
+	 * @since 2.1
+	 */
+	protected IProject project = null;
+	private final LoggingStreamDaemon logger;
 
 	public static final int ERROR_STREAM = 0;
 	public static final int INPUT_STREAM = 1;
@@ -77,6 +82,20 @@ public class Command implements Runnable {
 	 * @since 2.0
 	 */
 	public Command(String[] cmd, String[] envVars) {
+		this(cmd, envVars, null); 
+	}
+	
+	/**
+	 * Spawns the new thread that this class will run in.  From the Runnable
+	 * interface spawning the new thread automatically calls the run() method.
+	 * This must be called by the implementing class in order to start the
+	 * StreamGobbler.
+	 * @param cmd The entire command to run
+	 * @param envVars List of all environment variables to use
+	 * @param project The project this script belongs to or null
+	 * @since 2.1
+	 */
+	public Command(String[] cmd, String[] envVars, IProject project) {
 		if (cmd != null) {
 			this.cmd = Arrays.copyOf(cmd, cmd.length);
 		}
@@ -84,6 +103,7 @@ public class Command implements Runnable {
 		if (envVars != null) {
 			this.envVars = Arrays.copyOf(envVars, envVars.length);
 		}
+		this.project = project;
 		logger = new LoggingStreamDaemon();
 		addInputStreamListener(logger);
 	}
@@ -112,7 +132,7 @@ public class Command implements Runnable {
 	 */
 	protected IStatus init() {
 		try {
-			process = SystemtapProcessFactory.exec(cmd, envVars);
+			process = RuntimeProcessFactory.getFactory().exec(cmd, envVars, project);
 
 			if (process == null){
 				return new Status(IStatus.ERROR, StructuresPlugin.PLUGIN_ID, Messages.Command_failedToRunSystemtap);
