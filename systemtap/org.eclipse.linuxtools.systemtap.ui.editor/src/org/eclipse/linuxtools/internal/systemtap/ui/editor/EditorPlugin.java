6619a6dfdb90b7a9f5a619d3b468242c2624d26a
Alexander Kurtakov <akurtako@redhat.com>
2013-12-19 06:32:40 +0200

    systemtap: Remove unused parts from ui.editor bundle.
    * Deleted unused classes.
    * Bumped major version.
    * No need for activator.
    * Removed unneeded import package.
    
    Change-Id: Ie8abac94bbe832bbc530ddd53ac2994c7f15b611
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/20032
    Tested-by: Hudson CI

--- systemtap/org.eclipse.linuxtools.systemtap.ui.editor/src/org/eclipse/linuxtools/internal/systemtap/ui/editor/EditorPlugin.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.editor/src/org/eclipse/linuxtools/internal/systemtap/ui/editor/EditorPlugin.java
@@ -11,60 +11,10 @@
 
 package org.eclipse.linuxtools.internal.systemtap.ui.editor;
 
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
-
 /**
- * The main plugin class to be used in the desktop.
+ * The constants definitions.
  */
-public class EditorPlugin extends AbstractUIPlugin {
+public interface EditorPlugin {
 
-	public static final String ID = "org.eclipse.linuxtools.systemtap.ui.editor"; //$NON-NLS-1$
-
-	//The shared instance.
-	private static EditorPlugin plugin;
-
-	/**
-	 * The constructor.
-	 */
-	public EditorPlugin() {
-		plugin = this;
-	}
-
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		plugin = null;
-	}
-
-	/**
-	 * Returns the shared instance.
-	 */
-	public static EditorPlugin getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin(ID, path);
-	}
-
+	String ID = "org.eclipse.linuxtools.systemtap.ui.editor"; //$NON-NLS-1$
 }
