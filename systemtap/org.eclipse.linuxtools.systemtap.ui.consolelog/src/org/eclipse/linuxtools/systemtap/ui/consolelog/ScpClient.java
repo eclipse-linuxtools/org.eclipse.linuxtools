a13004a9acfae165b676bf7080f8a8e9c1e6e6bf
Andrew Ferrazzutti <aferrazz@redhat.com>
2014-03-27 11:21:01 -0400

    Systemtap: Remove unused inner class MyUserInfo.
    Change-Id: I28ac4b7df4024b42e1bebbc3728b2b40719342e2
    Signed-off-by: Andrew Ferrazzutti <aferrazz@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/24003
    Tested-by: Hudson CI

--- systemtap/org.eclipse.linuxtools.systemtap.ui.consolelog/src/org/eclipse/linuxtools/systemtap/ui/consolelog/ScpClient.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.consolelog/src/org/eclipse/linuxtools/systemtap/ui/consolelog/ScpClient.java
@@ -11,21 +11,11 @@
 
 package org.eclipse.linuxtools.systemtap.ui.consolelog;
 
-import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 
 import org.eclipse.linuxtools.systemtap.ui.consolelog.structures.RemoteScriptOptions;
 
@@ -34,8 +24,6 @@ import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-import com.jcraft.jsch.UIKeyboardInteractive;
-import com.jcraft.jsch.UserInfo;
 
 public class ScpClient {
 
@@ -142,102 +130,4 @@ public class ScpClient {
 		return b;
 	}
 
-  public static class MyUserInfo implements UserInfo, UIKeyboardInteractive{
-		@Override
-		public String getPassword() {
-			return passwd;
-		}
-
-		@Override
-		public boolean promptYesNo(String str) {
-			Object[] options = { "yes", "no" }; //$NON-NLS-1$ //$NON-NLS-2$
-			int foo = JOptionPane.showOptionDialog(null, str,
-					"Warning", //$NON-NLS-1$
-					JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
-					null, options, options[0]);
-			return foo == 0;
-		}
-
-		String passwd;
-		JTextField passwordField = new JPasswordField(20);
-
-		@Override
-		public String getPassphrase() {
-			return null;
-		}
-
-		@Override
-		public boolean promptPassphrase(String message) {
-			return true;
-		}
-
-		@Override
-		public boolean promptPassword(String message) {
-			Object[] ob = { passwordField };
-			int result = JOptionPane.showConfirmDialog(null, ob, message,
-					JOptionPane.OK_CANCEL_OPTION);
-			if (result == JOptionPane.OK_OPTION) {
-				passwd = passwordField.getText();
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-		@Override
-		public void showMessage(String message) {
-			JOptionPane.showMessageDialog(null, message);
-		}
-
-		final GridBagConstraints gbc = new GridBagConstraints(0, 0, 1, 1, 1, 1,
-				GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
-				new Insets(0, 0, 0, 0), 0, 0);
-		private Container panel;
-
-		@Override
-		public String[] promptKeyboardInteractive(String destination,
-				String name, String instruction, String[] prompt, boolean[] echo) {
-			panel = new JPanel();
-			panel.setLayout(new GridBagLayout());
-
-			gbc.weightx = 1.0;
-			gbc.gridwidth = GridBagConstraints.REMAINDER;
-			gbc.gridx = 0;
-			panel.add(new JLabel(instruction), gbc);
-			gbc.gridy++;
-
-			gbc.gridwidth = GridBagConstraints.RELATIVE;
-
-			JTextField[] texts = new JTextField[prompt.length];
-			for (int i = 0; i < prompt.length; i++) {
-				gbc.fill = GridBagConstraints.NONE;
-				gbc.gridx = 0;
-				gbc.weightx = 1;
-				panel.add(new JLabel(prompt[i]), gbc);
-
-				gbc.gridx = 1;
-				gbc.fill = GridBagConstraints.HORIZONTAL;
-				gbc.weighty = 1;
-				if (echo[i]) {
-					texts[i] = new JTextField(20);
-				} else {
-					texts[i] = new JPasswordField(20);
-				}
-				panel.add(texts[i], gbc);
-				gbc.gridy++;
-			}
-
-			if (JOptionPane.showConfirmDialog(null, panel, destination
-					+ ": " + name, //$NON-NLS-1$
-					JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION) {
-				String[] response = new String[prompt.length];
-				for (int i = 0; i < prompt.length; i++) {
-					response[i] = texts[i].getText();
-				}
-				return response;
-			} else {
-				return null; // cancel
-			}
-		}
-	}
 }
