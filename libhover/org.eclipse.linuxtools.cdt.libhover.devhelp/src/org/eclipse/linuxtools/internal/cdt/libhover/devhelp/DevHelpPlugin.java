6ef7c7ac205c12bd89d91ceb0ce4d1bca3737426
Jeff Johnston <jjohnstn@redhat.com>
2014-07-30 17:03:45 -0400

    Fix NPE occurring occasionally with devhelp plug-in
    - Job started by devhelp may not complete by the time
      plug-ins are slated to be stopped and so may result in
      an NPE trying to access plugin info
    - make Job a private static variable and cancel in stop() method
    - also add canceling() method to Job to cancel the run monitor
    - in the run() method of the Job make checks for the monitor
      being cancelled and stop
    
    Change-Id: I0763631a30f05ae5517c172d26d65748a06207b4
    Reviewed-on: https://git.eclipse.org/r/30763
    Tested-by: Hudson CI
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>
    Tested-by: Roland Grunberg <rgrunber@redhat.com>

--- libhover/org.eclipse.linuxtools.cdt.libhover.devhelp/src/org/eclipse/linuxtools/internal/cdt/libhover/devhelp/DevHelpPlugin.java
+++ libhover/org.eclipse.linuxtools.cdt.libhover.devhelp/src/org/eclipse/linuxtools/internal/cdt/libhover/devhelp/DevHelpPlugin.java
@@ -37,102 +37,126 @@ import org.osgi.framework.BundleContext;
  */
 public class DevHelpPlugin extends AbstractUIPlugin implements IStartup {
 
-    // The plug-in ID
-    public static final String PLUGIN_ID = "org.eclipse.linuxtools.cdt.libhover.devhelp"; //$NON-NLS-1$
-    private static final String REGENERATE_MSG = "Libhover.Devhelp.Regenerate.msg"; //$NON-NLS-1$
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.linuxtools.cdt.libhover.devhelp"; //$NON-NLS-1$
+	private static final String REGENERATE_MSG = "Libhover.Devhelp.Regenerate.msg"; //$NON-NLS-1$
 
-    // The shared instance
-    private static DevHelpPlugin plugin;
+	// The shared instance
+	private static DevHelpPlugin plugin;
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-     * )
-     */
-    @Override
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-        Job k = new DevhelpStartupJob(LibHoverMessages.getString(REGENERATE_MSG)) ;
-        k.schedule();
-    }
+	// Startup job
+	private static Job k;
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-     * )
-     */
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		k = new DevhelpStartupJob(LibHoverMessages.getString(REGENERATE_MSG)) ;
+		k.schedule();
+	}
 
-    /**
-     * Returns the shared instance
-     *
-     * @return the shared instance
-     */
-    public static DevHelpPlugin getDefault() {
-        return plugin;
-    }
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		k.cancel();
+		plugin = null;
+		super.stop(context);
+	}
 
-    @Override
-    public void earlyStartup() {
-        // do nothing...we just want start to get called and reparse the devhelp
-        // directory
-    }
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static DevHelpPlugin getDefault() {
+		return plugin;
+	}
 
-    /**
-     * Job used to load devhelp data on startup.
-     *
-     */
-    private static class DevhelpStartupJob extends Job {
+	@Override
+	public void earlyStartup() {
+		// do nothing...we just want start to get called and reparse the devhelp
+		// directory
+	}
 
-        public DevhelpStartupJob(String name) {
-            super(name);
-        }
+	/**
+	 * Job used to load devhelp data on startup.
+	 *
+	 */
+	private static class DevhelpStartupJob extends Job {
 
-        @Override
-        protected IStatus run(IProgressMonitor monitor) {
-            IPreferenceStore ps = DevHelpPlugin.getDefault()
-                    .getPreferenceStore();
-            ParseDevHelp.DevHelpParser p = new ParseDevHelp.DevHelpParser(
-                    ps.getString(PreferenceConstants.DEVHELP_DIRECTORY));
-            LibHoverInfo hover = p.parse(monitor);
-            // Update the devhelp library info if it is on library list
-            Collection<LibHoverLibrary> libs = LibHover.getLibraries();
-            for (LibHoverLibrary l : libs) {
-                if (l.getName().equals("devhelp")) { //$NON-NLS-1$
-                    l.setHoverinfo(hover);
-                    break;
-                }
-            }
-            try {
-                // Now, output the LibHoverInfo for caching later
-                IPath location = LibhoverPlugin.getDefault()
-                        .getStateLocation().append("C"); //$NON-NLS-1$
-                File ldir = new File(location.toOSString());
-                ldir.mkdir();
-                location = location.append("devhelp.libhover"); //$NON-NLS-1$
-                try (FileOutputStream f = new FileOutputStream(
-                        location.toOSString());
-                        ObjectOutputStream out = new ObjectOutputStream(f)) {
-                    out.writeObject(hover);
-                }
-                monitor.done();
-            } catch (IOException e) {
-                monitor.done();
-                return new Status(IStatus.ERROR, DevHelpPlugin.PLUGIN_ID,
-                        e.getLocalizedMessage(), e);
-            }
+		private IProgressMonitor runMonitor;
 
-            return Status.OK_STATUS;
-        }
+		public DevhelpStartupJob(String name) {
+			super(name);
+		}
+
+		@Override
+		protected void canceling() {
+			if (runMonitor != null)
+				runMonitor.setCanceled(true);
+		};
+
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			runMonitor = monitor;
+			if (monitor.isCanceled())
+				return Status.CANCEL_STATUS;
+			IPreferenceStore ps = DevHelpPlugin.getDefault()
+					.getPreferenceStore();
+			ParseDevHelp.DevHelpParser p = new ParseDevHelp.DevHelpParser(
+					ps.getString(PreferenceConstants.DEVHELP_DIRECTORY));
+			LibHoverInfo hover = p.parse(monitor);
+			if (monitor.isCanceled())
+				return Status.CANCEL_STATUS;
+			// Update the devhelp library info if it is on library list
+			Collection<LibHoverLibrary> libs = LibHover.getLibraries();
+			for (LibHoverLibrary l : libs) {
+				if (monitor.isCanceled())
+					return Status.CANCEL_STATUS;
+				if (l.getName().equals("devhelp")) { //$NON-NLS-1$
+					l.setHoverinfo(hover);
+					break;
+				}
+			}
+			try {
+				if (monitor.isCanceled())
+					return Status.CANCEL_STATUS;
+				// Now, output the LibHoverInfo for caching later
+				IPath location = LibhoverPlugin.getDefault()
+						.getStateLocation().append("C"); //$NON-NLS-1$
+				File ldir = new File(location.toOSString());
+				ldir.mkdir();
+				location = location.append("devhelp.libhover"); //$NON-NLS-1$
+				try (FileOutputStream f = new FileOutputStream(
+						location.toOSString());
+						ObjectOutputStream out = new ObjectOutputStream(f)) {
+					out.writeObject(hover);
+				}
+				monitor.done();
+			} catch (NullPointerException e) {
+				monitor.done();
+				return Status.CANCEL_STATUS;
+			} catch (IOException e) {
+				monitor.done();
+				return new Status(IStatus.ERROR, DevHelpPlugin.PLUGIN_ID,
+						e.getLocalizedMessage(), e);
+			}
+
+			return Status.OK_STATUS;
+		}
 
     };
 
