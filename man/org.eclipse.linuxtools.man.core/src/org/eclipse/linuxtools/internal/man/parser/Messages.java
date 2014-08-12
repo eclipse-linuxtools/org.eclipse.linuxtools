d0fbde21046faa6adb64e74697e296ada607e975
Alexander Kurtakov <akurtako@redhat.com>
2014-08-12 22:21:55 +0300

    No need for javadoc in Messages.
    Change-Id: I415a92de4a5b7fa421e14a7d5e7fbf89aacab9e2
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/31465
    Tested-by: Hudson CI

--- man/org.eclipse.linuxtools.man.core/src/org/eclipse/linuxtools/internal/man/parser/Messages.java
+++ man/org.eclipse.linuxtools.man.core/src/org/eclipse/linuxtools/internal/man/parser/Messages.java
@@ -13,14 +13,15 @@ package org.eclipse.linuxtools.internal.man.parser;
 
 import org.eclipse.osgi.util.NLS;
 
+@SuppressWarnings("javadoc")
 public class Messages extends NLS {
-    private static final String BUNDLE_NAME = "org.eclipse.linuxtools.internal.man.parser.messages"; //$NON-NLS-1$
-    public static String ManParser_RemoteAccessError;
-    static {
-        // initialize resource bundle
-        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-    }
+	private static final String BUNDLE_NAME = "org.eclipse.linuxtools.internal.man.parser.messages"; //$NON-NLS-1$
+	public static String ManParser_RemoteAccessError;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
 
-    private Messages() {
-    }
+	private Messages() {
+	}
 }
