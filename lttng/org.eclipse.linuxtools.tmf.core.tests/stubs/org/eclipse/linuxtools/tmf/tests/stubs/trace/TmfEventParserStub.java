e4a8412cbe9ce3a161101bc368d001e4f30b53bf
Patrick Tasse <patrick.tasse@gmail.com>
2014-10-21 14:17:24 -0400

    tmf: Add unit tests for time offset
    Change-Id: Ib44c6acb6f8bb0e3d23a583e30e6fec7071686b3
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/35271
    Tested-by: Hudson CI
    Reviewed-by: Genevieve Bastien <gbastien+lttng@versatic.net>

--- lttng/org.eclipse.linuxtools.tmf.core.tests/stubs/org/eclipse/linuxtools/tmf/tests/stubs/trace/TmfEventParserStub.java
+++ lttng/org.eclipse.linuxtools.tmf.core.tests/stubs/org/eclipse/linuxtools/tmf/tests/stubs/trace/TmfEventParserStub.java
@@ -22,7 +22,6 @@ import org.eclipse.linuxtools.tmf.core.event.ITmfEventField;
 import org.eclipse.linuxtools.tmf.core.event.TmfEvent;
 import org.eclipse.linuxtools.tmf.core.event.TmfEventField;
 import org.eclipse.linuxtools.tmf.core.event.TmfEventType;
-import org.eclipse.linuxtools.tmf.core.timestamp.TmfTimestamp;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfContext;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfEventParser;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
@@ -112,7 +111,7 @@ public class TmfEventParserStub implements ITmfEventParser {
 
                 final TmfEventField root = new TmfEventField(ITmfEventField.ROOT_FIELD_ID, content.toString(), null);
                 final ITmfEvent event = new TmfEvent(fEventStream,
-                        new TmfTimestamp(ts, -3, 0),     // millisecs
+                        fEventStream.createTimestamp(ts * 1000000L),
                         source, fTypes[typeIndex], root, String.valueOf(reference));
                 return event;
             } catch (final EOFException e) {
