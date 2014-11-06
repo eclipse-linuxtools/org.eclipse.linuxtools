4e97f67b52b94b4ff8d22c26d810f4a9459b32b7
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2014-11-06 18:06:50 -0500

    tmf: Fix superfluous Supplementary files element in trace package import
    When importing a trace package without supplementary files, a superfluous
    supplementary files element was shown in the tree. This is a regression caused
    by a previous patch that changes the way to handle children.
    
    Change-Id: I30b505b623973badb2b1ac94fe39347f24c5a15c
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/36089
    Tested-by: Hudson CI
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/wizards/tracepkg/importexport/ManifestReader.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/wizards/tracepkg/importexport/ManifestReader.java
@@ -125,7 +125,6 @@ public class ManifestReader {
      */
     public static TracePackageElement[] loadElementsFromNode(Element rootElement) {
         List<TracePackageElement> packageElements = new ArrayList<>();
-        TracePackageElement element = null;
         NodeList traceElements = rootElement.getElementsByTagName(ITracePackageConstants.TRACE_ELEMENT);
         for (int i = 0; i < traceElements.getLength(); i++) {
             Node traceNode = traceElements.item(i);
@@ -133,36 +132,29 @@ public class ManifestReader {
                 Element traceElement = (Element) traceNode;
                 String traceName = traceElement.getAttribute(ITracePackageConstants.TRACE_NAME_ATTRIB);
                 String traceType = traceElement.getAttribute(ITracePackageConstants.TRACE_TYPE_ATTRIB);
-                element = new TracePackageTraceElement(null, traceName, traceType);
-
-                List<TracePackageElement> children = new ArrayList<>();
+                TracePackageElement element = new TracePackageTraceElement(null, traceName, traceType);
                 NodeList fileElements = traceElement.getElementsByTagName(ITracePackageConstants.TRACE_FILE_ELEMENT);
                 for (int j = 0; j < fileElements.getLength(); j++) {
                     Node fileNode = fileElements.item(j);
                     if (fileNode.getNodeType() == Node.ELEMENT_NODE) {
                         Element fileElement = (Element) fileNode;
                         String fileName = fileElement.getAttribute(ITracePackageConstants.TRACE_FILE_NAME_ATTRIB);
-                        children.add(new TracePackageFilesElement(element, fileName));
+                        new TracePackageFilesElement(element, fileName);
                     }
                 }
-
-                TracePackageSupplFilesElement supplFilesElement = new TracePackageSupplFilesElement(element);
 
                 // Supplementary files
-                List<TracePackageSupplFileElement> suppFiles = new ArrayList<>();
                 NodeList suppFilesElements = traceElement.getElementsByTagName(ITracePackageConstants.SUPPLEMENTARY_FILE_ELEMENT);
-                for (int j = 0; j < suppFilesElements.getLength(); j++) {
-                    Node suppFileNode = suppFilesElements.item(j);
-                    if (suppFileNode.getNodeType() == Node.ELEMENT_NODE) {
-                        Element suppFileElement = (Element) suppFileNode;
-                        String fileName = suppFileElement.getAttribute(ITracePackageConstants.SUPPLEMENTARY_FILE_NAME_ATTRIB);
-                        TracePackageSupplFileElement supplFile = new TracePackageSupplFileElement(fileName, supplFilesElement);
-                        suppFiles.add(supplFile);
+                if (suppFilesElements.getLength() > 0) {
+                    TracePackageSupplFilesElement supplFilesElement = new TracePackageSupplFilesElement(element);
+                    for (int j = 0; j < suppFilesElements.getLength(); j++) {
+                        Node suppFileNode = suppFilesElements.item(j);
+                        if (suppFileNode.getNodeType() == Node.ELEMENT_NODE) {
+                            Element suppFileElement = (Element) suppFileNode;
+                            String fileName = suppFileElement.getAttribute(ITracePackageConstants.SUPPLEMENTARY_FILE_NAME_ATTRIB);
+                            new TracePackageSupplFileElement(fileName, supplFilesElement);
+                        }
                     }
-                }
-
-                if (!suppFiles.isEmpty()) {
-                    children.add(supplFilesElement);
                 }
 
                 // bookmarks
@@ -188,7 +180,7 @@ public class ManifestReader {
                     }
                 }
                 if (!bookmarkAttribs.isEmpty()) {
-                    children.add(new TracePackageBookmarkElement(element, bookmarkAttribs));
+                    new TracePackageBookmarkElement(element, bookmarkAttribs);
                 }
 
                 packageElements.add(element);
