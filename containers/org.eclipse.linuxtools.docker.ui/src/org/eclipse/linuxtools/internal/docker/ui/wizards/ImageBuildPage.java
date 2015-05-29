96c7f7c5137915641e6a157f79670b3f021e9722
Jeff Johnston <jjohnstn@redhat.com>
2015-05-29 14:21:53 -0400

    Bug 468849 - don't use new shell for Dockerfile Editor dialog
    - fixed to use container's shell from ImageBuildPage
    
    Change-Id: I0c3206dc14bd147ba4547f682b84d710516a15c9
    Reviewed-on: https://git.eclipse.org/r/48995
    Tested-by: Hudson CI
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    Tested-by: Jeff Johnston <jjohnstn@redhat.com>
    (cherry picked from commit ec77bb14af8ec7f152c6920ed2066bf3b29298b4)
    Reviewed-on: https://git.eclipse.org/r/48998

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ImageBuildPage.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ImageBuildPage.java
@@ -30,7 +30,6 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 public class ImageBuildPage extends WizardPage {
@@ -190,7 +189,8 @@ public class ImageBuildPage extends WizardPage {
 				String dir = directoryText.getText();
 				IFileStore fileStore = EFS.getLocalFileSystem().getStore(
 						new Path(dir).append("Dockerfile")); //$NON-NLS-1$
-				DockerfileEditDialog d = new DockerfileEditDialog(new Shell(),
+				DockerfileEditDialog d = new DockerfileEditDialog(
+						container.getShell(),
 						fileStore);
 				d.open();
 				validate();
