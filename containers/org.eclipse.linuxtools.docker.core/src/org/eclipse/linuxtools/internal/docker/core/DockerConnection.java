98bf5b197b0104cd39f0c1612a5ee3847d4c1726
Roland Grunberg <rgrunber@redhat.com>
2015-06-02 17:35:32 -0400

    Remove some print statements and fix typo in property tester.
    Such errors happen when there is access to a stream the underlying
    libraries have closed. We try to avoid this by checking the state of the
    container but this may not be enough.
    
    Change-Id: Ia23d71b8c8124ea289e8b37201629e5a8b983a91
    Reviewed-on: https://git.eclipse.org/r/49262
    Tested-by: Hudson CI
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
@@ -39,6 +39,9 @@ import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
 
+import jnr.unixsocket.UnixSocketAddress;
+import jnr.unixsocket.UnixSocketChannel;
+
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.ListenerList;
@@ -97,9 +100,6 @@ import com.spotify.docker.client.messages.ImageSearchResult;
 import com.spotify.docker.client.messages.Info;
 import com.spotify.docker.client.messages.PortBinding;
 import com.spotify.docker.client.messages.Version;
-
-import jnr.unixsocket.UnixSocketAddress;
-import jnr.unixsocket.UnixSocketChannel;
 
 /**
  * A connection to a Docker daemon. The connection may rely on Unix Socket or TCP connection (using the REST API). 
@@ -1503,7 +1503,6 @@ public class DockerConnection implements IDockerConnection {
 							buff = new byte[1024];
 						}
 					} catch (Exception e) {
-						e.printStackTrace();
 					}
 				}
 			});
@@ -1569,7 +1568,6 @@ public class DockerConnection implements IDockerConnection {
 							}
 						}
 					} catch (Exception e) {
-						e.printStackTrace();
 					}
 				}
 			});
