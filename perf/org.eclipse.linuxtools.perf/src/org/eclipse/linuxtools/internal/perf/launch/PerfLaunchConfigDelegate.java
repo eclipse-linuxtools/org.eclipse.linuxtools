91e798ea61ce8804bf3e0ca9f37b07c46e11994d
Roberto Oliveira <rdutra@linux.vnet.ibm.com>
2015-09-08 15:00:24 -0300

    Bug 476990: Fix when getting the working dir path for local and sync projects using perf
    Change-Id: If87cfad5dfeefde75d7bdf6077456014d4356b06
    Signed-off-by: Roberto Oliveira <rdutra@linux.vnet.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/55569
    Tested-by: Hudson CI
    Reviewed-by: Wainer dos Santos Moschetta <wainersm@linux.vnet.ibm.com>

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/launch/PerfLaunchConfigDelegate.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/launch/PerfLaunchConfigDelegate.java
@@ -58,6 +58,7 @@ import org.eclipse.linuxtools.profiling.launch.ConfigUtils;
 import org.eclipse.linuxtools.profiling.launch.IRemoteFileProxy;
 import org.eclipse.linuxtools.profiling.launch.RemoteConnection;
 import org.eclipse.linuxtools.profiling.launch.RemoteConnectionException;
+import org.eclipse.linuxtools.profiling.launch.RemoteProxyCMainTab;
 import org.eclipse.linuxtools.profiling.launch.RemoteProxyManager;
 import org.eclipse.linuxtools.tools.launch.core.factory.RuntimeProcessFactory;
 import org.eclipse.swt.widgets.Display;
@@ -94,15 +95,17 @@ public class PerfLaunchConfigDelegate extends AbstractCLaunchDelegate {
                 IStatus status = new Status(IStatus.ERROR, PerfPlugin.PLUGIN_ID, "Error: Perf was not found on PATH"); //$NON-NLS-1$
                 throw new CoreException(status);
             }
-            URI binURI = new URI(configUtils.getExecutablePath());
-            binPath=Path.fromPortableString(binURI.getPath().toString());
-            if(binPath==null) {
-                CDebugUtils.verifyProgramPath( config );
-            }
-            if (binPath.removeLastSegments(2).toPortableString().equals(EMPTY_STRING)) {
-                workingDirPath=Path.fromPortableString(getWorkingDirectory(config).toURI().getPath());
+
+            URI workingDirURI = new URI(config.getAttribute(RemoteProxyCMainTab.ATTR_REMOTE_WORKING_DIRECTORY_NAME, EMPTY_STRING));
+            // Local project
+            if (workingDirURI.toString().equals(EMPTY_STRING)) {
+            	workingDirURI = getWorkingDirectory(config).toURI();
+            	workingDirPath = Path.fromPortableString(workingDirURI.getPath());
+            	binPath = CDebugUtils.verifyProgramPath(config);
             } else {
-                workingDirPath=Path.fromPortableString((binPath.removeLastSegments(2).toPortableString()) + IPath.SEPARATOR);
+            	workingDirPath = Path.fromPortableString(workingDirURI.getPath() + IPath.SEPARATOR);
+            	URI binURI = new URI(configUtils.getExecutablePath());
+            	binPath = Path.fromPortableString(binURI.getPath().toString());
             }
 
             PerfPlugin.getDefault().setWorkingDir(workingDirPath);
@@ -112,7 +115,6 @@ public class PerfLaunchConfigDelegate extends AbstractCLaunchDelegate {
             } else {
                 String perfPathString = RuntimeProcessFactory.getFactory().whichCommand(PerfPlugin.PERF_COMMAND, project);
                 IFileStore workingDir;
-                URI workingDirURI = new URI(RemoteProxyManager.getInstance().getRemoteProjectLocation(project));
                 RemoteConnection workingDirRC = new RemoteConnection(workingDirURI);
                 IRemoteFileProxy workingDirRFP = workingDirRC.getRmtFileProxy();
                 workingDir = workingDirRFP.getResource(workingDirURI.getPath());
@@ -199,7 +201,7 @@ public class PerfLaunchConfigDelegate extends AbstractCLaunchDelegate {
 
                 URI perfDataURI = null;
                 IRemoteFileProxy proxy = null;
-                perfDataURI = new URI(RemoteProxyManager.getInstance().getRemoteProjectLocation(project) + IPath.SEPARATOR + PerfPlugin.PERF_DEFAULT_DATA);
+                perfDataURI = new URI(workingDirURI.toString() + IPath.SEPARATOR + PerfPlugin.PERF_DEFAULT_DATA);
                 proxy = RemoteProxyManager.getInstance().getFileProxy(perfDataURI);
                 IFileStore perfDataFileStore = proxy.getResource(perfDataURI.getPath());
                 IFileInfo info = perfDataFileStore.fetchInfo();
