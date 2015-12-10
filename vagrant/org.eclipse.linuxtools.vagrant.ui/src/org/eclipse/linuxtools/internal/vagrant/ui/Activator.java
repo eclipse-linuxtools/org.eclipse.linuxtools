81e0c7d8340718016e376cb2024f2547b8a91fc6
Roland Grunberg <rgrunber@redhat.com>
2015-12-10 11:19:31 -0500

    Issue warning when Polkit Configuration files for libvirt are found.
    The presence of Polkit configuration files for libvirt means that
    prompts for credentials may occur. Java's native command execution is
    not a proper shell so credentials aren't 'remembered', and the prompt
    will occur every time.
    
    Change-Id: I6ba0071be22a2dddb21e0ec9248236b29e892c28
    Reviewed-on: https://git.eclipse.org/r/63119
    Tested-by: Hudson CI
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>

--- vagrant/org.eclipse.linuxtools.vagrant.ui/src/org/eclipse/linuxtools/internal/vagrant/ui/Activator.java
+++ vagrant/org.eclipse.linuxtools.vagrant.ui/src/org/eclipse/linuxtools/internal/vagrant/ui/Activator.java
@@ -10,11 +10,17 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.vagrant.ui;
 
+import java.io.File;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Paths;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -39,6 +45,29 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		final String LAUNCH_ID = "firstLaunch"; //$NON-NLS-1$
+		final boolean firstLaunch = InstanceScope.INSTANCE.getNode(PLUGIN_ID)
+				.getBoolean(LAUNCH_ID, true);
+		if (Platform.OS_LINUX.equals(Platform.getOS()) && firstLaunch) {
+			Thread t = new Thread(() -> {
+				File polkitDir = Paths.get("/", "usr", "share", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+						"polkit-1", "actions").toFile(); //$NON-NLS-1$ //$NON-NLS-2$
+				if (polkitDir.exists()) {
+					File[] libvirtFiles = polkitDir.listFiles((File dir, String name) -> {
+						return name.matches("org\\.libvirt.*\\.policy"); //$NON-NLS-1$
+					});
+					if (libvirtFiles.length > 0) {
+						Display.getDefault()
+						.syncExec(() -> MessageDialog.openWarning(Display.getCurrent()
+								.getActiveShell(),
+										Messages.Activator_additional_configuration_title,
+										Messages.Activator_additional_configuration_msg));
+					}
+				}
+			});
+			t.start();
+			InstanceScope.INSTANCE.getNode(PLUGIN_ID).putBoolean(LAUNCH_ID, false);
+		}
 	}
 
 	@Override
