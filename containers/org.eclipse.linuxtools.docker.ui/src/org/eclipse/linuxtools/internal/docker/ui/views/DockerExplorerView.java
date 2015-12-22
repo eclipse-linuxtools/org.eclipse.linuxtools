01ef493e9da253f8e5cf3a4086c879f4ba17df05
Roland Grunberg <rgrunber@redhat.com>
2015-12-22 14:01:39 -0500

    Bug 479933: Set the Docker Explorer View to the first connection.
    Change-Id: I8d923486179eec11046e1eb4f675d6ff7e27bae0
    Reviewed-on: https://git.eclipse.org/r/63178
    Tested-by: Hudson CI
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerView.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerView.java
@@ -161,6 +161,10 @@ public class DockerExplorerView extends CommonNavigator implements
 		super.createPartControl(container);
 		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL)
 				.grab(true, true).applyTo(getCommonViewer().getControl());
+		if (DockerConnectionManager.getInstance().getConnections().length > 0) {
+			IDockerConnection conn = DockerConnectionManager.getInstance().getConnections()[0];
+			getCommonViewer().setSelection(new StructuredSelection(conn));
+		}
 		return form;
 	}
 
