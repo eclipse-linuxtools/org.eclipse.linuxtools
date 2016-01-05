2bf4b9ee0f15c8b95aa2635c0b7f83baee1fc457
Xavier Coulon <xcoulon@redhat.com>
2016-01-05 15:08:05 +0100

    Bug 483680 - [docker image search] search results ignore repo when searching for repo/imagename
    the searchImages method in the DockerConnection class filters the results
      returned by the client to only retain those that contain the exact term
      given by the user. So if the user input "jboss/wildfly", only this image
      will appear in the result list, but if the user input "wildfly", more
      results (25 at the time of writing this message) will be displayed in
      the list.
    
    Refactored the unit test to avoid code duplication.
    
    Change-Id: Icfdb64814ed6800a10f546e0912ff081e7a84984
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/63552
    Tested-by: Hudson CI
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>

--- containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ImageSearchSWTBotTest.java
+++ containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ImageSearchSWTBotTest.java
@@ -64,50 +64,83 @@ public class ImageSearchSWTBotTest {
 	@Test
 	public void shouldTriggerSearchIfTermWasGiven() {
 		// given
-		final DockerClient client = MockDockerClientFactory
-				.onSearch("foo", MockImageSearchResultFactory.name("foo").build()).build();
-		final DockerConnection dockerConnection = MockDockerConnectionFactory.from("Test", client).get();
-		DockerConnectionManagerUtils.configureConnectionManager(dockerConnection);
-		// expand the 'Images' node
-		SWTUtils.syncExec(() -> dockerExplorerView.getCommonViewer().expandAll());
-		final SWTBotTreeItem imagesTreeItem = SWTUtils.getTreeItem(dockerExplorerViewBot, "Test", "Images");
-
-		// when opening the "Pull..." wizard
-		final SWTBotTree dockerExplorerViewTreeBot = dockerExplorerViewBot.bot().tree();
-		dockerExplorerViewTreeBot.select(imagesTreeItem);
-		dockerExplorerViewTreeBot.contextMenu("Pull...").click();
-
-		// when specifying a term
+		final DockerClient client = MockDockerClientFactory.onSearch("foo", MockImageSearchResultFactory.name("foo").build())
+				.build();
+		// when opening the pull wizard...
+		openPullWizard(client);
+		// ... and specifying a term...
 		bot.textWithLabel(WizardMessages.getString("ImagePull.name.label")).setText("foo");
+		// ... and then opening the search wizard
+		openSearchWizard();
+		// then the search should have been triggered and results should be available
+		assertThat(bot.table().rowCount()).isEqualTo(1);
+	}
 
-		// when clicking on the "Search..." button
-		bot.button(WizardMessages.getString("ImagePull.search.label")).click();
-		// then the search should have been triggered and results should be
+	@Test
+	public void shouldNotTriggerSearchIfNoTermWasGiven() throws InterruptedException {
+		// given
+		final DockerClient client = MockDockerClientFactory.onSearch("foo", MockImageSearchResultFactory.name("foo").build())
+				.build();
+		// when opening the pull wizard...
+		openPullWizard(client);
+		// ... and directly opening the search wizard
+		openSearchWizard();
+		// then the search should have been triggered and results should be available
+		assertThat(bot.table().rowCount()).isEqualTo(0);
+	}
+
+	@Test
+	public void shouldReduceSearchResultsToExactGivenTerm() throws InterruptedException {
+		// given
+		final DockerClient client = MockDockerClientFactory
+				.onSearch("foo/bar", MockImageSearchResultFactory.name("foo/bar").build(),
+						MockImageSearchResultFactory.name("other/bar").build())
+				.build();
+		// when opening the pull wizard...
+		openPullWizard(client);
+		// ... and specifying a term...
+		bot.textWithLabel(WizardMessages.getString("ImagePull.name.label")).setText("foo/bar");
+		// ... and then opening the search wizard
+		openSearchWizard();
+		// then the search should have been triggered and a single result should be
 		// available
 		assertThat(bot.table().rowCount()).isEqualTo(1);
 	}
 
 	@Test
-	public void shouldNotTriggerSearchIfNoTermWasGiven() {
+	public void shouldShowAllSearchResultsForGivenTerm() throws InterruptedException {
 		// given
 		final DockerClient client = MockDockerClientFactory
-				.onSearch("foo", MockImageSearchResultFactory.name("foo").build()).build();
+				.onSearch("bar", MockImageSearchResultFactory.name("foo/bar").build(),
+						MockImageSearchResultFactory.name("other/bar").build())
+				.build();
+		// when opening the pull wizard...
+		openPullWizard(client);
+		// ... and specifying a term...
+		bot.textWithLabel(WizardMessages.getString("ImagePull.name.label")).setText("bar");
+		// ... and then opening the search wizard
+		openSearchWizard();
+		// then the search should have been triggered and both results should be
+		// available
+		assertThat(bot.table().rowCount()).isEqualTo(2);
+	}
+
+	private void openPullWizard(final DockerClient client) {
+		// given 
 		final DockerConnection dockerConnection = MockDockerConnectionFactory.from("Test", client).get();
 		DockerConnectionManagerUtils.configureConnectionManager(dockerConnection);
 		// expand the 'Images' node
 		SWTUtils.syncExec(() -> dockerExplorerView.getCommonViewer().expandAll());
 		final SWTBotTreeItem imagesTreeItem = SWTUtils.getTreeItem(dockerExplorerViewBot, "Test", "Images");
-
 		// when opening the "Pull..." wizard
 		final SWTBotTree dockerExplorerViewTreeBot = dockerExplorerViewBot.bot().tree();
 		dockerExplorerViewTreeBot.select(imagesTreeItem);
 		dockerExplorerViewTreeBot.contextMenu("Pull...").click();
+	}
 
-		// when clicking on the "Search..." button *without specifying a term before*
+	private void openSearchWizard() {	
+		// click on the "Search..." button
 		bot.button(WizardMessages.getString("ImagePull.search.label")).click();
-		// then the search should have been triggered and results should be
-		// available
-		assertThat(bot.table().rowCount()).isEqualTo(0);
 	}
 
 	@Test
