d960ef15289f56c7e16f3c10c814da0ac08f898c
Roland Grunberg <rgrunber@redhat.com>
2016-02-19 13:32:35 -0500

    Update to com.spotify.docker.client 3.4.0.
    - {Build,Logs}Parameter -> {Build,Logs}Param
    - Work around non-public BuildParam constants
    - Guava 18.0 not available so suppress harmless toString() logging error
    
    Change-Id: I273f46bc41e449db238f3def089930a70c345f0b
    Reviewed-on: https://git.eclipse.org/r/66962
    Tested-by: Hudson CI
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
@@ -14,6 +14,7 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
 import java.nio.file.FileSystems;
@@ -72,8 +73,8 @@ import com.spotify.docker.client.ContainerNotFoundException;
 import com.spotify.docker.client.DockerCertificateException;
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.DockerClient.AttachParameter;
-import com.spotify.docker.client.DockerClient.BuildParameter;
-import com.spotify.docker.client.DockerClient.LogsParameter;
+import com.spotify.docker.client.DockerClient.BuildParam;
+import com.spotify.docker.client.DockerClient.LogsParam;
 import com.spotify.docker.client.LogStream;
 import com.spotify.docker.client.messages.Container;
 import com.spotify.docker.client.messages.ContainerConfig;
@@ -482,12 +483,12 @@ public class DockerConnection implements IDockerConnection, Closeable {
 				LogStream stream = null;
 
 				if (timestamps)
-					stream = copyClient.logs(id, LogsParameter.FOLLOW,
-							LogsParameter.STDOUT, LogsParameter.STDERR,
-							LogsParameter.TIMESTAMPS);
+					stream = copyClient.logs(id, LogsParam.follow(),
+							LogsParam.stdout(), LogsParam.stderr(),
+							LogsParam.timestamps());
 				else
-					stream = copyClient.logs(id, LogsParameter.FOLLOW,
-							LogsParameter.STDOUT, LogsParameter.STDERR);
+					stream = copyClient.logs(id, LogsParam.follow(),
+							LogsParam.stdout(), LogsParam.stderr());
 
 				// First time through, don't sleep before showing log data
 				int delayTime = 100;
@@ -928,7 +929,8 @@ public class DockerConnection implements IDockerConnection, Closeable {
 			final DockerProgressHandler d = new DockerProgressHandler(handler);
 			final java.nio.file.Path p = FileSystems.getDefault()
 					.getPath(path.makeAbsolute().toOSString());
-			return getClientCopy().build(p, d, BuildParameter.FORCE_RM);
+			return getClientCopy().build(p, d,
+					BuildParam.create("forcerm", "true")); //$NON-NLS-1$ //$NON-NLS-2$
 		} catch (com.spotify.docker.client.DockerRequestException e) {
 			throw new DockerException(e.message());
 		} catch (com.spotify.docker.client.DockerException | IOException e) {
@@ -945,7 +947,8 @@ public class DockerConnection implements IDockerConnection, Closeable {
 			DockerProgressHandler d = new DockerProgressHandler(handler);
 			java.nio.file.Path p = FileSystems.getDefault().getPath(
 					path.makeAbsolute().toOSString());
-			return getClientCopy().build(p, name, d, BuildParameter.FORCE_RM);
+			return getClientCopy().build(p, name, d,
+					BuildParam.create("forcerm", "true")); //$NON-NLS-1$ $NON-NLS-2$
 		} catch (com.spotify.docker.client.DockerRequestException e) {
 			throw new DockerException(e.message());
 		} catch (com.spotify.docker.client.DockerException | IOException e) {
@@ -997,30 +1000,30 @@ public class DockerConnection implements IDockerConnection, Closeable {
 	 *            the build options
 	 * @return an array of relevant {@link BuildParameter}
 	 */
-	private BuildParameter[] getBuildParameters(
+	private BuildParam[] getBuildParameters(
 			final Map<String, Object> buildOptions) {
-		final List<BuildParameter> buildParameters = new ArrayList<>();
+		final List<BuildParam> buildParameters = new ArrayList<>();
 		for (Entry<String, Object> entry : buildOptions.entrySet()) {
 			final Object optionName = entry.getKey();
 			final Object optionValue = entry.getValue();
 
 			if (optionName.equals(IDockerImageBuildOptions.QUIET_BUILD)
 					&& optionValue.equals(true)) {
-				buildParameters.add(BuildParameter.QUIET);
+				buildParameters.add(BuildParam.create("q", "true")); //$NON-NLS-1$ $NON-NLS-2$
 			} else if (optionName.equals(IDockerImageBuildOptions.NO_CACHE)
 					&& optionValue.equals(true)) {
-				buildParameters.add(BuildParameter.NO_CACHE);
+				buildParameters.add(BuildParam.create("nocache", "true")); //$NON-NLS-1$ $NON-NLS-2$
 			} else if (optionName
 					.equals(IDockerImageBuildOptions.RM_INTERMEDIATE_CONTAINERS)
 					&& optionValue.equals(false)) {
-				buildParameters.add(BuildParameter.NO_RM);
+				buildParameters.add(BuildParam.create("rm", "false")); //$NON-NLS-1$ $NON-NLS-2$
 			} else if (optionName
 					.equals(IDockerImageBuildOptions.FORCE_RM_INTERMEDIATE_CONTAINERS)
 					&& optionValue.equals(true)) {
-				buildParameters.add(BuildParameter.FORCE_RM);
+				buildParameters.add(BuildParam.create("forcerm", "true")); //$NON-NLS-1$ $NON-NLS-2$
 			}
 		}
-		return buildParameters.toArray(new BuildParameter[0]);
+		return buildParameters.toArray(new BuildParam[0]);
 	}
 
 	public void save() {
@@ -1147,12 +1150,24 @@ public class DockerConnection implements IDockerConnection, Closeable {
 				builder = builder.onBuild(c.onBuild());
 			}
 
+			/*
+			 * Workaround error message thrown to stderr due to
+			 * lack of Guava 18.0. Remove this when we begin
+			 * using Guava 18.0.
+			 */
+			PrintStream oldErr = System.err;
+			System.setErr(new PrintStream(new OutputStream() {
+				@Override
+				public void write(int b) {
+				}
+			}));
 			// create container with default random name if an empty/null
 			// containerName argument was passed
 			final ContainerCreation creation = client
 					.createContainer(builder.build(),
 					(containerName != null && !containerName.isEmpty())
 							? containerName : null);
+			System.setErr(oldErr);
 			final String id = creation.id();
 			// force a refresh of the current containers to include the new one
 			listContainers();
