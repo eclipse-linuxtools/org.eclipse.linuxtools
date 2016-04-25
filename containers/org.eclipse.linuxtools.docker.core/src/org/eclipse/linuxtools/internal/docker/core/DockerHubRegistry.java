20d8e2685a803538ed1d8e53b21ed98a23bfc319
Alexander Kurtakov <akurtako@redhat.com>
2016-04-25 12:31:25 +0300

    docker: Remove redundant type.
    Change-Id: I623687a112725a0cbeb9d24866b9417e050ea0f7
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/71313
    Tested-by: Hudson CI

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerHubRegistry.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerHubRegistry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Red Hat.
+ * Copyright (c) 2015, 2016 Red Hat Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -78,7 +78,7 @@ public class DockerHubRegistry implements IDockerRegistry {
 				.request(APPLICATION_JSON_TYPE).async()
 				.method(GET, REPOSITORY_TAGS_RESULT_LIST).get();
 
-		return new ArrayList<IRepositoryTag>(result);
+		return new ArrayList<>(result);
 	}
 
 }
