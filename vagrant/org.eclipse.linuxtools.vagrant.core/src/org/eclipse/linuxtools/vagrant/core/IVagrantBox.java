a897b92fb2d317f6f949ed1f7e141570aaa83c6f
Alexander Kurtakov <akurtako@redhat.com>
2016-05-13 12:24:21 +0300

    vagrant: Consistent modifiers in interfaces.
    Change-Id: I74dfbab904b22c8430b13f215e89235111519c21
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/72690
    Tested-by: Hudson CI

--- vagrant/org.eclipse.linuxtools.vagrant.core/src/org/eclipse/linuxtools/vagrant/core/IVagrantBox.java
+++ vagrant/org.eclipse.linuxtools.vagrant.core/src/org/eclipse/linuxtools/vagrant/core/IVagrantBox.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Red Hat.
+ * Copyright (c) 2015, 2016 Red Hat.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,9 +14,9 @@ import org.osgi.framework.Version;
 
 public interface IVagrantBox {
 
-	public String getName();
+	String getName();
 
-	public String getProvider();
+	String getProvider();
 
-	public Version getVersion();
+	Version getVersion();
 }
