1c45f31347afd09cc326c9455140db0c3e35f6dc
Jeff Johnston <jjohnstn@redhat.com>
2016-06-10 12:44:04 -0400

    Bug 495840 - Memory leak if Docker Views left open for long period
    - additional fixes where images are not being disposed of properly
      in Docker
    
    Change-Id: I06ae37c7ebd21217ca70cc8d1b1b081b55b9df95
    Reviewed-on: https://git.eclipse.org/r/75087
    Tested-by: Hudson CI
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/NewDockerConnectionPage.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/NewDockerConnectionPage.java
@@ -977,6 +977,10 @@ public class NewDockerConnectionPage extends WizardPage {
 	}
 
 	private class ConnectionSelectionLabelProvider implements ILabelProvider {
+
+		private Image CONNECTION_IMAGE = SWTImagesFactory.DESC_REPOSITORY_MIDDLE
+				.createImage();
+
 		@Override
 		public void removeListener(ILabelProviderListener listener) {
 		}
@@ -988,6 +992,7 @@ public class NewDockerConnectionPage extends WizardPage {
 
 		@Override
 		public void dispose() {
+			CONNECTION_IMAGE.dispose();
 		}
 
 		@Override
@@ -1001,7 +1006,7 @@ public class NewDockerConnectionPage extends WizardPage {
 
 		@Override
 		public Image getImage(Object element) {
-			return SWTImagesFactory.DESC_REPOSITORY_MIDDLE.createImage();
+			return CONNECTION_IMAGE;
 		}
 	}
 
