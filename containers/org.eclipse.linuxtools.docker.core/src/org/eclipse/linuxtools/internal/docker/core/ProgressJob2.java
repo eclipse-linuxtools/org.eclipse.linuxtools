15e39e8f51e5deea25eeae1ddb405b207fb32848
Jeff Johnston <jjohnstn@redhat.com>
2016-11-03 14:33:13 -0400

    Bug 506980 - Pull Image from private registry returns error msg
    - fix DefaultImagePullProgressHandler to see if download has
      total amount or not
    - for downloads that don't post a total, use new ProgressJob2 class
      which has UNKNOWN total and update with message.progress()
    - add new ProgressJob2 class
    - add check for regular jobs to not calculate percentage if total
      is 0
    
    Change-Id: Ib85b50eda1f207ae49449d8b8ea26503bec2345d
    Reviewed-on: https://git.eclipse.org/r/84433
    Tested-by: Hudson CI
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>

---
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/ProgressJob2.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Red Hat.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat - Initial Contribution
+ *******************************************************************************/
+package org.eclipse.linuxtools.internal.docker.core;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+public class ProgressJob2 extends ProgressJob {
+
+	private int percentageDone = 0;
+	private String statusMessage = "";
+
+	private Object lockObject = new Object();
+
+	private String jobName;
+
+	public ProgressJob2(String name, String jobName) {
+		super(name, jobName);
+		this.jobName = jobName;
+	}
+
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		monitor.beginTask(jobName, IProgressMonitor.UNKNOWN);
+		boolean done = false;
+
+		while (!done) {
+			if (monitor.isCanceled()) {
+				return Status.CANCEL_STATUS;
+			}
+			// if work percentage has changed...add new amount
+			String status = getStatusMessage();
+			if (status != null && !status.isEmpty()) {
+				monitor.subTask(statusMessage);
+			}
+			// if we are 100% or more done, then we are done
+			if (percentageDone >= 100) {
+				done = true;
+			}
+			// otherwise, sleep and then loop again
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				done = true;
+			}
+		}
+		monitor.done();
+		return Status.OK_STATUS;
+	}
+
+	private String getStatusMessage() {
+		synchronized (lockObject) {
+			return statusMessage;
+		}
+	}
+
+	public void setStatusMessage(String statusMessage) {
+		synchronized (lockObject) {
+			this.statusMessage = statusMessage;
+		}
+	}
+
+	@Override
+	public void setPercentageDone(int percentage) {
+		synchronized (lockObject) {
+			if (percentage > percentageDone) {
+				percentageDone = percentage;
+			}
+		}
+	}
+
+}
