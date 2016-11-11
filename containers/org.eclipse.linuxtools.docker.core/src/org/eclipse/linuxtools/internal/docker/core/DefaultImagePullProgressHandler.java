2209af38f2f1e9c83a4364a8a390a08b29810c92
Jeff Johnston <jjohnstn@redhat.com>
2016-11-10 19:15:20 -0500

    Bug 507386 - Improve DefaultImagePullProgressHandler
    - add support in the DefaultImagePullProgressHandler to handle
      the extraction phase as well as the downloading
    - after setting the percentage of a job to 100%, remove the job
      from the progress job list
    - look for Verifying Checksum message as well as Pull complete as
      end of current job
    
    Change-Id: Ife66c923f9a1354faadd922c3a9c3eaa19bc778c
    Reviewed-on: https://git.eclipse.org/r/84847
    Tested-by: Hudson CI
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DefaultImagePullProgressHandler.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DefaultImagePullProgressHandler.java
@@ -27,6 +27,10 @@ public class DefaultImagePullProgressHandler implements IDockerProgressHandler {
 	private final static String IMAGE_DOWNLOADING_JOBNAME = "ImageDownloadingJobName.msg"; //$NON-NLS-1$
 	private final static String IMAGE_DOWNLOADING_IMAGE = "ImageDownloadingImage.msg"; //$NON-NLS-1$
 	private final static String IMAGE_DOWNLOADING = "ImageDownloading.msg"; //$NON-NLS-1$
+	private final static String IMAGE_VERIFYING_CHECKSUM = "ImageVerifyingChecksum.msg"; //$NON-NLS-1$
+	private final static String IMAGE_EXTRACTING_JOBNAME = "ImageExtractingJobName.msg"; //$NON-NLS-1$
+	private final static String IMAGE_EXTRACTING_IMAGE = "ImageExtractingImage.msg"; //$NON-NLS-1$
+	private final static String IMAGE_EXTRACTING = "ImageExtracting.msg"; //$NON-NLS-1$
 	private final static String IMAGE_PULLING = "ImagePulling.msg"; //$NON-NLS-1$
 	private final static String IMAGE_PULL_COMPLETE = "ImagePullComplete.msg"; //$NON-NLS-1$
 	private final static String IMAGE_DOWNLOADING_ALREADY_EXISTS = "ImageDownloadingAlreadyExists.msg"; //$NON-NLS-1$
@@ -62,6 +66,8 @@ public class DefaultImagePullProgressHandler implements IDockerProgressHandler {
 								.getString(IMAGE_DOWNLOADING_ALREADY_EXISTS))
 						|| status.contains(DockerMessages
 								.getString(IMAGE_DOWNLOADING_VERIFIED))
+						|| status.contains(DockerMessages
+								.getString(IMAGE_VERIFYING_CHECKSUM))
 						|| status.equals(
 								DockerMessages.getString(IMAGE_PULL_COMPLETE))) {
 					// an image is fully loaded, update the image list
@@ -70,15 +76,15 @@ public class DefaultImagePullProgressHandler implements IDockerProgressHandler {
 						.startsWith(DockerMessages.getString(IMAGE_DOWNLOADING))) {
 					IDockerProgressDetail detail = message.progressDetail();
 					if (detail == null || detail.total() == 0) {
-						// we have a new download in progress, track it
+						// We have a new extraction in progress with no
+						// details of what the total should be. Track it.
 						ProgressJob2 newJob = new ProgressJob2(
 								DockerMessages.getFormattedString(
 										IMAGE_DOWNLOADING_JOBNAME, image),
 								DockerMessages.getFormattedString(
 										IMAGE_DOWNLOADING_IMAGE, id));
 						// job.setUser(false) will show all pull job (one per
-						// image
-						// layer) in the progress
+						// image layer) in the progress
 						// view but not in multiple dialog
 						newJob.setUser(false);
 						newJob.setPriority(Job.LONG);
@@ -86,12 +92,49 @@ public class DefaultImagePullProgressHandler implements IDockerProgressHandler {
 						progressJobs.put(id, newJob);
 
 					} else {
-						// we have a new download in progress, track it
+						// We have a new download in progress and it
+						// provides us with a total so we can calculate
+						// percentage done. Track it.
 						ProgressJob newJob = new ProgressJob(
 								DockerMessages.getFormattedString(
 										IMAGE_DOWNLOADING_JOBNAME, image),
 								DockerMessages.getFormattedString(
 										IMAGE_DOWNLOADING_IMAGE, id));
+						// job.setUser(false) will show all pull job (one per
+						// image layer) in the progress
+						// view but not in multiple dialog
+						newJob.setUser(false);
+						newJob.setPriority(Job.LONG);
+						newJob.schedule();
+						progressJobs.put(id, newJob);
+					}
+				} else if (status.startsWith(
+						DockerMessages.getString(IMAGE_EXTRACTING))) {
+					IDockerProgressDetail detail = message.progressDetail();
+					if (detail == null || detail.total() == 0) {
+						// We have a new extraction in progress with no
+						// details of what the total should be. Track it.
+						ProgressJob2 newJob = new ProgressJob2(
+								DockerMessages.getFormattedString(
+										IMAGE_EXTRACTING_JOBNAME, image),
+								DockerMessages.getFormattedString(
+										IMAGE_EXTRACTING_IMAGE, id));
+						// job.setUser(false) will show all pull job (one per
+						// image layer) in the progress
+						// view but not in multiple dialog
+						newJob.setUser(false);
+						newJob.setPriority(Job.LONG);
+						newJob.schedule();
+						progressJobs.put(id, newJob);
+					} else {
+						// We have a new extraction in progress and it
+						// provides us with a total so we can calculate
+						// percentage done. Track it.
+						ProgressJob newJob = new ProgressJob(
+								DockerMessages.getFormattedString(
+										IMAGE_EXTRACTING_JOBNAME, image),
+								DockerMessages.getFormattedString(
+										IMAGE_EXTRACTING_IMAGE, id));
 						// job.setUser(false) will show all pull job (one per
 						// image
 						// layer) in the progress
@@ -109,11 +152,36 @@ public class DefaultImagePullProgressHandler implements IDockerProgressHandler {
 						|| status.contains(DockerMessages
 								.getString(IMAGE_DOWNLOADING_ALREADY_EXISTS))
 						|| status.contains(DockerMessages
-								.getString(IMAGE_DOWNLOADING_VERIFIED))) {
+								.getString(IMAGE_DOWNLOADING_VERIFIED))
+						|| status.contains(DockerMessages
+								.getString(IMAGE_VERIFYING_CHECKSUM))
+						|| status.contains(DockerMessages
+								.getString(IMAGE_PULL_COMPLETE))) {
+					// Download or pull is complete for this id so set the job
+					// percentage 100 and
+					// remove the job from list. Removing the job allows
+					// extraction job to be
+					// created after a download is complete.
 					p.setPercentageDone(100);
+					progressJobs.put(id, null);
 					connection.getImages(true);
 				} else if (status
 						.startsWith(DockerMessages.getString(IMAGE_DOWNLOADING))) {
+					// Update download progress
+					IDockerProgressDetail detail = message.progressDetail();
+					if (detail != null) {
+						if (p instanceof ProgressJob2) {
+							((ProgressJob2) p)
+									.setStatusMessage(message.progress());
+						} else if (detail.current() > 0 && detail.total() > 0) {
+							long percentage = (detail.current() * 100)
+									/ detail.total();
+							p.setPercentageDone((int) percentage);
+						}
+					}
+				} else if (status.startsWith(
+						DockerMessages.getString(IMAGE_EXTRACTING))) {
+					// Update extracting progress
 					IDockerProgressDetail detail = message.progressDetail();
 					if (detail != null) {
 						if (p instanceof ProgressJob2) {
@@ -132,7 +200,9 @@ public class DefaultImagePullProgressHandler implements IDockerProgressHandler {
 
 	private void stopAllJobs() {
 		for (ProgressJob j : progressJobs.values()) {
-			j.cancel();
+			if (j != null) {
+				j.cancel();
+			}
 		}
 
 	}
