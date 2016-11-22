8d0a7cc0a1fe74c173299848925fcaafe866ca55
Jeff Johnston <jjohnstn@redhat.com>
2016-11-22 17:17:52 -0500

    Bug 507915 - Add support to copy files from host to container
    - add new ContainerCopyTo wizard plus ContainerCopyToPage classes
    - add new CopyToContainerCommandHandler class
    - modify MinimizedFileSystemElement to expose isPopulated method
    - add new ResourceInfoUtils, IDEFileSystemStructureProvider, and
      FileSystemStructureProvider classes
    
    Change-Id: I82f82f88332307710e2af609364d64cd46c23026
    Reviewed-on: https://git.eclipse.org/r/85541
    Tested-by: Hudson CI
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    (cherry picked from commit 397292fc1decfba806284f095a78dde0c3e34b7f)
    Reviewed-on: https://git.eclipse.org/r/85546

---
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ContainerCopyTo.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Red Hat.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat - Initial Contribution
+ *******************************************************************************/
+package org.eclipse.linuxtools.internal.docker.ui.wizards;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.linuxtools.docker.core.IDockerConnection;
+import org.eclipse.linuxtools.docker.core.IDockerContainer;
+import org.eclipse.linuxtools.docker.ui.Activator;
+import org.eclipse.linuxtools.internal.docker.core.ContainerFileProxy;
+import org.eclipse.linuxtools.internal.docker.core.DockerConnection;
+import org.eclipse.linuxtools.internal.docker.ui.ContainerFileSystemProvider;
+import org.eclipse.linuxtools.internal.docker.ui.PopulateContainerFilesOperation;
+
+public class ContainerCopyTo extends Wizard {
+
+	private DockerConnection connection;
+	private IDockerContainer container;
+	private String target;
+	private List<Object> sources;
+	private ContainerCopyToPage mainPage;
+
+	public ContainerCopyTo(IDockerConnection connection,
+			IDockerContainer container) {
+		this.connection = (DockerConnection) connection;
+		this.container = container;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public List<Object> getSources() {
+		return sources;
+	}
+
+	@Override
+	public void addPages() {
+		ProgressMonitorDialog pd = new ProgressMonitorDialog(
+				Activator.getActiveWorkbenchShell());
+		ContainerFileSystemProvider provider = new ContainerFileSystemProvider(
+				connection, container.id());
+		PopulateContainerFilesOperation sfo = new PopulateContainerFilesOperation(
+				new ContainerFileProxy("", "", true), //$NON-NLS-1$ //$NON-NLS-2$
+				null, provider);
+		try {
+			pd.run(true, true, sfo);
+		} catch (InvocationTargetException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		mainPage = new ContainerCopyToPage(sfo.getResult(), provider,
+				container.name());
+		addPage(mainPage);
+	}
+
+	@Override
+	public boolean canFinish() {
+		return mainPage.isPageComplete();
+	}
+
+	@Override
+	public boolean performFinish() {
+		boolean finished = mainPage.finish();
+		if (finished) {
+			target = mainPage.getDestination().toOSString();
+			sources = mainPage.getFilesToCopy();
+		}
+		return finished;
+	}
+}
---
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/CopyToContainerMessages.java
@@ -0,0 +1,67 @@
+package org.eclipse.linuxtools.internal.docker.ui.wizards;
+
+/*******************************************************************************
+ * Copyright (c) 2005, 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM - Initial API and implementation
+ * Red Hat, Inc - WizardProjectsImportPage[_ArchiveSelectTitle, _SelectArchiveDialogTitle]
+ * Red Hat, Inc - copied to Linux Tools Docker UI
+ *******************************************************************************/
+
+import org.eclipse.osgi.util.NLS;
+
+public class CopyToContainerMessages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.linuxtools.internal.docker.ui.wizards.CopyToContainerMessages"; //$NON-NLS-1$
+
+	// ==============================================================================
+	// Data Transfer Wizards
+	// ==============================================================================
+	public static String DataTransfer_fileSystemTitle;
+
+	public static String DataTransfer_browse;
+	public static String DataTransfer_selectTypes;
+	public static String DataTransfer_selectAll;
+	public static String DataTransfer_deselectAll;
+	public static String DataTransfer_refresh;
+	public static String DataTransfer_cannotOverwrite;
+	public static String DataTransfer_emptyString;
+	public static String DataTransfer_scanningMatching;
+	public static String DataTransfer_information;
+
+	public static String ImportPage_filterSelections;
+
+	public static String ContainerCopyTo_description;
+	public static String ContainerCopyTo_title;
+
+	public static String ContainerCopyTo_selectSource;
+	public static String ContainerCopyTo_selectSourceTitle;
+	public static String ContainerCopyTo_fromDirectory;
+	public static String ContainerCopyTo_importFileSystem;
+	public static String ContainerCopyTo_overwriteExisting;
+	public static String ContainerCopyTo_createTopLevel;
+	public static String ContainerCopyTo_createVirtualFolders;
+	public static String ContainerCopyTo_importElementsAs;
+	public static String ContainerCopyTo_createVirtualFoldersTooltip;
+	public static String ContainerCopyTo_createLinksInWorkspace;
+	public static String ContainerCopyTo_advanced;
+	public static String ContainerCopyTo_noneSelected;
+	public static String ContainerCopyTo_cannotImportFilesUnderAVirtualFolder;
+	public static String ContainerCopyTo_haveToCreateLinksUnderAVirtualFolder;
+	public static String ContainerCopyTo_invalidSource;
+	public static String ContainerCopyTo_sourceEmpty;
+	public static String ContainerCopyTo_destinationEmpty;
+	public static String ContainerCopyTo_importProblems;
+	public static String ContainerCopyTo_showAdvanced;
+	public static String ContainerCopyTo_hideAdvanced;
+
+	static {
+		// load message values from bundle file
+		NLS.initializeMessages(BUNDLE_NAME, CopyToContainerMessages.class);
+	}
+}
+
