4777ce9f4671df8888d303522f54d797b6767e29
Alexander Kurtakov <akurtako@redhat.com>
2017-02-14 19:15:50 +0200

    docker: More Guava deprecated updates.
    Change-Id: I25087bb89bfb907792efcba5daaf91ea20e002f9
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/91079
    Tested-by: Hudson CI

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/ImageSearchResultV2.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/ImageSearchResultV2.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Red Hat.
+ * Copyright (c) 2016, 2017 Red Hat.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,7 +18,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Objects;
+import com.google.common.base.MoreObjects;
 import com.spotify.docker.client.messages.ImageSearchResult;
 
 /**
@@ -43,7 +43,7 @@ public class ImageSearchResultV2 {
 
 	@Override
 	public String toString() {
-		return Objects.toStringHelper(this)
+		return MoreObjects.toStringHelper(this)
 				.add("results", getRepositories()).toString(); //$NON-NLS-1$
 	}
 
--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/RepositoryTag.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/RepositoryTag.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Red Hat.
+ * Copyright (c) 2014, 2017 Red Hat.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,7 +17,7 @@ import org.eclipse.linuxtools.docker.core.IRepositoryTag;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Objects;
+import com.google.common.base.MoreObjects;
 
 /**
  * Repository tag retrieved from Docker Registry version 0.6.3
@@ -84,7 +84,7 @@ public class RepositoryTag
 
 	@Override
 	public String toString() {
-		return Objects.toStringHelper(this).add("name", getName()) //$NON-NLS-1$
+		return MoreObjects.toStringHelper(this).add("name", getName()) //$NON-NLS-1$
 				.add("layer", getLayer()).toString(); //$NON-NLS-1$
 	}
 
