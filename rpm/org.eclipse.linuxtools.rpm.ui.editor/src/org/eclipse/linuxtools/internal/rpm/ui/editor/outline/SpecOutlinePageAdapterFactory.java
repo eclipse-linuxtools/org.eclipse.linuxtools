e72037e8e5393934d4aa8ea26e2ab41924e0ee74
Alexander Kurtakov <akurtako@redhat.com>
2017-03-15 01:08:09 +0200

    rpm: Add outline support in generic editor.
    Change-Id: I9f82e6d5fbd6cd0b2251f84e2e6d2c00b99a33d8
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/93075
    Tested-by: Hudson CI

---
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/internal/rpm/ui/editor/outline/SpecOutlinePageAdapterFactory.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Red Hat - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.internal.rpm.ui.editor.outline;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.linuxtools.rpm.ui.editor.SpecfileEditor;
+import org.eclipse.ui.internal.genericeditor.ExtensionBasedTextEditor;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
+
+public class SpecOutlinePageAdapterFactory implements IAdapterFactory {
+
+	private static final Class<?>[] ADAPTERS = new Class[] { IContentOutlinePage.class };
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T getAdapter(Object adaptableObject, Class<T> adapterType) {
+		if (IContentOutlinePage.class.equals(adapterType)) {
+			if (adaptableObject instanceof ExtensionBasedTextEditor || adaptableObject instanceof SpecfileEditor) {
+				ITextEditor specEditor = (ITextEditor) adaptableObject;
+				IFile editorFile = specEditor.getEditorInput().getAdapter(IFile.class);
+				if (editorFile.getLocation().toOSString().endsWith(".spec")) { //$NON-NLS-1$
+					SpecfileContentOutlinePage page = new SpecfileContentOutlinePage(specEditor);
+					page.setInput(specEditor.getEditorInput());
+					return (T) page;
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Class<?>[] getAdapterList() {
+		return ADAPTERS;
+	}
+
+}
--- rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/internal/rpm/ui/editor/outline/SpecfileContentProvider.java
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/internal/rpm/ui/editor/outline/SpecfileContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 Red Hat, Inc.
+ * Copyright (c) 2007, 2017 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,95 +21,92 @@ import org.eclipse.linuxtools.rpm.ui.editor.SpecfileEditor;
 import org.eclipse.linuxtools.rpm.ui.editor.parser.Specfile;
 import org.eclipse.linuxtools.rpm.ui.editor.parser.SpecfilePackage;
 import org.eclipse.linuxtools.rpm.ui.editor.parser.SpecfilePackageContainer;
+import org.eclipse.linuxtools.rpm.ui.editor.parser.SpecfileParser;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 public class SpecfileContentProvider implements ITreeContentProvider {
 
-    private IDocumentProvider documentProvider;
-    private Specfile specfile;
-    private SpecfileEditor specEditor;
-    protected static final String SECTION_POSITIONS = "section_positions"; //$NON-NLS-1$
-    protected IPositionUpdater positionUpdater = new DefaultPositionUpdater(
-            SECTION_POSITIONS);
+	private IDocumentProvider documentProvider;
+	private Specfile specfile;
+	private SpecfileEditor specEditor;
+	protected static final String SECTION_POSITIONS = "section_positions"; //$NON-NLS-1$
+	protected IPositionUpdater positionUpdater = new DefaultPositionUpdater(SECTION_POSITIONS);
 
-    public SpecfileContentProvider(ITextEditor editor) {
-        if (editor instanceof SpecfileEditor) {
-            specEditor = (SpecfileEditor) editor;
-            specfile = specEditor.getSpecfile();
-        }
-        this.documentProvider = editor.getDocumentProvider();
-    }
+	public SpecfileContentProvider(ITextEditor editor) {
+		specfile = new SpecfileParser().parse(editor.getDocumentProvider().getDocument(editor.getEditorInput()));
+		this.documentProvider = editor.getDocumentProvider();
+	}
 
-    @Override
-    public void dispose() {
-    }
+	@Override
+	public void dispose() {
+	}
 
-    @Override
-    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-        if (oldInput != null) {
-            IDocument document = documentProvider.getDocument(oldInput);
-            if (document != null) {
-                try {
-                    document.removePositionCategory(SECTION_POSITIONS);
-                } catch (BadPositionCategoryException x) {
-                }
-                document.removePositionUpdater(positionUpdater);
-            }
-        }
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		if (oldInput != null) {
+			IDocument document = documentProvider.getDocument(oldInput);
+			if (document != null) {
+				try {
+					document.removePositionCategory(SECTION_POSITIONS);
+				} catch (BadPositionCategoryException x) {
+				}
+				document.removePositionUpdater(positionUpdater);
+			}
+		}
 
-        if (newInput != null) {
-            IDocument document = documentProvider.getDocument(newInput);
-            if (document != null) {
-                document.addPositionCategory(SECTION_POSITIONS);
-                document.addPositionUpdater(positionUpdater);
-                if (specEditor != null) {
-                    specfile = specEditor.getSpecfile();
-                }
-            }
-        }
-    }
+		if (newInput != null) {
+			IDocument document = documentProvider.getDocument(newInput);
+			if (document != null) {
+				document.addPositionCategory(SECTION_POSITIONS);
+				document.addPositionUpdater(positionUpdater);
+				if (specEditor != null) {
+					specfile = specEditor.getSpecfile();
+				}
+			}
+		}
+	}
 
-    @Override
-    public Object[] getChildren(Object parentElement) {
-        if (parentElement == specfile) {
-            int elmsSize = 1 + 1 + specfile.getSections().size();
-            Object[] elms = new Object[elmsSize];
-            elms[0] = specfile.getPreamble();
-            Object[] sections = specfile.getSections().toArray();
-            for (int i = 0; i < sections.length; i++) {
-                elms[i + 1] = sections[i];
-            }
-            elms[elmsSize - 1] = specfile.getPackages();
-            return elms;
-        } else if (parentElement instanceof SpecfilePackageContainer) {
-            return ((SpecfilePackageContainer) parentElement).getPackages();
-        } else if (parentElement instanceof SpecfilePackage) {
-            return ((SpecfilePackage) parentElement).getSections();
-        }
-        return new Object[0];
-    }
+	@Override
+	public Object[] getChildren(Object parentElement) {
+		if (parentElement == specfile) {
+			int elmsSize = 1 + 1 + specfile.getSections().size();
+			Object[] elms = new Object[elmsSize];
+			elms[0] = specfile.getPreamble();
+			Object[] sections = specfile.getSections().toArray();
+			for (int i = 0; i < sections.length; i++) {
+				elms[i + 1] = sections[i];
+			}
+			elms[elmsSize - 1] = specfile.getPackages();
+			return elms;
+		} else if (parentElement instanceof SpecfilePackageContainer) {
+			return ((SpecfilePackageContainer) parentElement).getPackages();
+		} else if (parentElement instanceof SpecfilePackage) {
+			return ((SpecfilePackage) parentElement).getSections();
+		}
+		return new Object[0];
+	}
 
-    @Override
-    public Object getParent(Object element) {
-        return null;
-    }
+	@Override
+	public Object getParent(Object element) {
+		return null;
+	}
 
-    @Override
-    public boolean hasChildren(Object element) {
-        if (element == specfile) {
-            return true;
-        } else if (element instanceof SpecfilePackageContainer) {
-            return ((SpecfilePackageContainer) element).hasChildren();
-        } else if (element instanceof SpecfilePackage) {
-            return ((SpecfilePackage) element).hasChildren();
-        }
-        return false;
-    }
+	@Override
+	public boolean hasChildren(Object element) {
+		if (element == specfile) {
+			return true;
+		} else if (element instanceof SpecfilePackageContainer) {
+			return ((SpecfilePackageContainer) element).hasChildren();
+		} else if (element instanceof SpecfilePackage) {
+			return ((SpecfilePackage) element).hasChildren();
+		}
+		return false;
+	}
 
-    @Override
-    public Object[] getElements(Object inputElement) {
-        return this.getChildren(specfile);
-    }
+	@Override
+	public Object[] getElements(Object inputElement) {
+		return this.getChildren(specfile);
+	}
 
 }
