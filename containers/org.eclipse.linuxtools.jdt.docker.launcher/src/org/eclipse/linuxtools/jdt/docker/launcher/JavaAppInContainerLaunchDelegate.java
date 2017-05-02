32a4a7d5905a42450535fe50471b2f36fe6ef3f5
Roland Grunberg <rgrunber@redhat.com>
2017-05-02 12:23:02 -0400

    Add the boilerplate classes and extension points.
    - launch shortcut, delegate, launch configuration type, and their
    necessary dependencies
    
    Change-Id: I3892ed275d59e5cc79a0a68c52b2dba82aa51916
    Reviewed-on: https://git.eclipse.org/r/98930
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>
    Tested-by: Roland Grunberg <rgrunber@redhat.com>

---
+++ containers/org.eclipse.linuxtools.jdt.docker.launcher/src/org/eclipse/linuxtools/jdt/docker/launcher/JavaAppInContainerLaunchDelegate.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat - Initial Contribution
+ *******************************************************************************/
+package org.eclipse.linuxtools.jdt.docker.launcher;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
+
+public class JavaAppInContainerLaunchDelegate extends AbstractJavaLaunchConfigurationDelegate {
+
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
+		System.out.println("Launching From Delegate.");
+	}
+
+}
