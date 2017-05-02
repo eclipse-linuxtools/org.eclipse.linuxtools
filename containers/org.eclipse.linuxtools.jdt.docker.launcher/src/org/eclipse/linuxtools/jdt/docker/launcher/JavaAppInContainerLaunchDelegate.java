ffbfd06e929fb8290ff38be44337a20de6823760
Roland Grunberg <rgrunber@redhat.com>
2017-05-02 12:55:51 -0400

    Make our launch delegate mimic JavaLaunchDelegate exactly.
    Change-Id: If5e845e65364edb57a94032b21fc98d02b79d9ae
    Reviewed-on: https://git.eclipse.org/r/98931
    Reviewed-by: Roland Grunberg <rgrunber@redhat.com>
    Tested-by: Roland Grunberg <rgrunber@redhat.com>

--- containers/org.eclipse.linuxtools.jdt.docker.launcher/src/org/eclipse/linuxtools/jdt/docker/launcher/JavaAppInContainerLaunchDelegate.java
+++ containers/org.eclipse.linuxtools.jdt.docker.launcher/src/org/eclipse/linuxtools/jdt/docker/launcher/JavaAppInContainerLaunchDelegate.java
@@ -10,18 +10,101 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.jdt.docker.launcher;
 
+import java.io.File;
+import java.util.Map;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
+import org.eclipse.jdt.launching.ExecutionArguments;
+import org.eclipse.jdt.launching.IVMRunner;
+import org.eclipse.jdt.launching.VMRunnerConfiguration;
+import org.eclipse.osgi.util.NLS;
 
 public class JavaAppInContainerLaunchDelegate extends AbstractJavaLaunchConfigurationDelegate {
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.core.model.ILaunchConfigurationDelegate#launch(org.eclipse.debug.core.ILaunchConfiguration, java.lang.String, org.eclipse.debug.core.ILaunch, org.eclipse.core.runtime.IProgressMonitor)
+	 */
 	@Override
-	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
-			throws CoreException {
-		System.out.println("Launching From Delegate.");
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
+
+		if (monitor == null) {
+			monitor = new NullProgressMonitor();
+		}
+
+		monitor.beginTask(NLS.bind("{0}...", new String[]{configuration.getName()}), 3); //$NON-NLS-1$
+		// check for cancellation
+		if (monitor.isCanceled()) {
+			return;
+		}
+		try {
+			monitor.subTask("Verifying launch attributes...");
+
+			String mainTypeName = verifyMainTypeName(configuration);
+			IVMRunner runner = getVMRunner(configuration, mode);
+
+			File workingDir = verifyWorkingDirectory(configuration);
+			String workingDirName = null;
+			if (workingDir != null) {
+				workingDirName = workingDir.getAbsolutePath();
+			}
+
+			// Environment variables
+			String[] envp= getEnvironment(configuration);
+
+			// Program & VM arguments
+			String pgmArgs = getProgramArguments(configuration);
+			String vmArgs = getVMArguments(configuration);
+			ExecutionArguments execArgs = new ExecutionArguments(vmArgs, pgmArgs);
+
+			// VM-specific attributes
+			Map<String, Object> vmAttributesMap = getVMSpecificAttributesMap(configuration);
+
+			// Classpath
+			String[] classpath = getClasspath(configuration);
+
+			// Create VM config
+			VMRunnerConfiguration runConfig = new VMRunnerConfiguration(mainTypeName, classpath);
+			runConfig.setProgramArguments(execArgs.getProgramArgumentsArray());
+			runConfig.setEnvironment(envp);
+			runConfig.setVMArguments(execArgs.getVMArgumentsArray());
+			runConfig.setWorkingDirectory(workingDirName);
+			runConfig.setVMSpecificAttributesMap(vmAttributesMap);
+
+			// Bootpath
+			runConfig.setBootClassPath(getBootpath(configuration));
+
+			// check for cancellation
+			if (monitor.isCanceled()) {
+				return;
+			}
+
+			// stop in main
+			prepareStopInMain(configuration);
+
+			// done the verification phase
+			monitor.worked(1);
+
+			monitor.subTask("Creating source locator...");
+			// set the default source locator if required
+			setDefaultSourceLocator(launch, configuration);
+			monitor.worked(1);
+
+			// Launch the configuration - 1 unit of work
+			runner.run(runConfig, launch, monitor);
+
+			// check for cancellation
+			if (monitor.isCanceled()) {
+				return;
+			}
+		}
+		finally {
+			monitor.done();
+		}
 	}
 
 }
