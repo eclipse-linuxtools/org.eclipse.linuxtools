2498f759da160ea7fa02c062f6b31358ddd3574a
Jeff Johnston <jjohnstn@redhat.com>
2017-06-05 18:22:28 -0400

    Bug 517909 - Save last selected connection and restore it on restart
    - modify DockerConnectionWatcher to save the active connection and
      restore on start-up
    
    Change-Id: I2bae1c99bf12bedae91a69dc6df1e81628ebc7a1
    Reviewed-on: https://git.eclipse.org/r/98662
    Tested-by: Hudson CI
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/DockerConnectionWatcher.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/DockerConnectionWatcher.java
@@ -10,13 +10,32 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.docker.ui;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.linuxtools.docker.core.DockerConnectionManager;
 import org.eclipse.linuxtools.docker.core.IDockerConnection;
+import org.eclipse.linuxtools.docker.ui.Activator;
 import org.eclipse.linuxtools.internal.docker.ui.views.DockerExplorerView;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * Singleton class to track current connection set by Docker Explorer View
@@ -25,6 +44,8 @@ import org.eclipse.ui.PlatformUI;
  *
  */
 public class DockerConnectionWatcher implements ISelectionListener {
+
+	public final static String CONNECTION_FILE_NAME = "dockerselectedconnection.xml"; //$NON-NLS-1$
 
 	private static DockerConnectionWatcher instance;
 	private IDockerConnection connection;
@@ -41,7 +62,10 @@ public class DockerConnectionWatcher implements ISelectionListener {
 		PlatformUI.getWorkbench().getActiveWorkbenchWindow()
 				.getSelectionService()
 				.addSelectionListener(DockerExplorerView.VIEW_ID, this);
-
+		IDockerConnection selectedConnection = loadConnection();
+		if (selectedConnection != null) {
+			this.connection = selectedConnection;
+		}
 	}
 
 	public void dispose() {
@@ -60,6 +84,93 @@ public class DockerConnectionWatcher implements ISelectionListener {
 	 */
 	public void setConnection(IDockerConnection connection) {
 		this.connection = connection;
+		if (connection != null) {
+			saveConnection(connection);
+		}
+	}
+
+	// Save the current selected connection for when Eclipse restarts
+	private void saveConnection(IDockerConnection connection) {
+		final IPath stateLocation = Activator.getDefault().getStateLocation();
+		final File connectionFile = stateLocation.append(CONNECTION_FILE_NAME)
+				.toFile();
+		if (!connection.isOpen()) {
+			return;
+		}
+		try {
+			if (!connectionFile.exists()) {
+				connectionFile.createNewFile();
+			}
+			try (final PrintWriter p = new PrintWriter(
+					new BufferedWriter(new FileWriter(connectionFile)))) {
+				p.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"); //$NON-NLS-1$
+				String name = connection.getName();
+				if (name.equals(Messages.getString("Unnamed"))) { //$NON-NLS-1$
+					name = ""; //$NON-NLS-1$
+				}
+				p.print("<connection name=\"" + name + //$NON-NLS-1$
+						"\" uri=\"" + connection.getUri()); //$NON-NLS-1$
+				if (connection.getUsername() != null) {
+					p.print("\" username=\"" + connection.getUsername()); //$NON-NLS-1$
+				}
+				p.println("\"/>"); //$NON-NLS-1$
+			}
+		} catch (Exception e) {
+			Activator.log(e);
+		}
+	}
+
+	// Load the previously selected connection at start-up
+	private IDockerConnection loadConnection() {
+		IDockerConnection connection = null;
+		final IPath stateLocation = Activator.getDefault().getStateLocation();
+		final File connectionFile = stateLocation.append(CONNECTION_FILE_NAME)
+				.toFile();
+		final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			if (connectionFile.exists()) {
+				Document d = db.parse(connectionFile);
+				Element e = d.getDocumentElement();
+				// Get the stored configuration data
+				NodeList connectionNodes = e.getElementsByTagName("connection"); // $NON-NLS-1$
+				if (connectionNodes == null
+						|| connectionNodes.getLength() == 0) {
+					return null;
+				}
+				Node n = connectionNodes.item(0);
+				NamedNodeMap attrs = n.getAttributes();
+				Node nameNode = attrs.getNamedItem("name"); //$NON-NLS-1$
+				Node uriNode = attrs.getNamedItem("uri"); //$NON-NLS-1$
+				Node usernameNode = attrs.getNamedItem("username"); //$NON-NLS-1$
+				if (uriNode != null) {
+					String uri = uriNode.getNodeValue();
+					String name = nameNode.getNodeValue();
+					String username = null;
+
+					if (usernameNode != null) {
+						username = usernameNode.getNodeValue();
+					}
+					IDockerConnection[] connections = DockerConnectionManager
+							.getInstance().getConnections();
+					for (IDockerConnection c : connections) {
+						if (c.getUri().equals(uri)) {
+							if (c.getName().equals(name)) {
+								if (c.getUsername() == null
+										|| c.getUsername().equals(username)) {
+									connection = c;
+									break;
+								}
+							}
+						}
+					}
+				}
+
+			}
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			Activator.log(e);
+		}
+		return connection;
 	}
 
 	/**
