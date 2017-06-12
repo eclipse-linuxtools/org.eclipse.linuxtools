2340318d60fdc83945e0de71d6251ef221388c3e
Jeff Johnston <jjohnstn@redhat.com>
2017-06-12 17:19:15 -0400

    Bug 518154 - ChangeLog is issuing debug statements concerning editor ids
    - fix InsertChangeLogKeyHandler.isEnabled to not print out editor ids
    
    Change-Id: I3c6a8b90cb9ac9effd5f72bc7c1a3c8f169bbb62
    Reviewed-on: https://git.eclipse.org/r/99178
    Tested-by: Hudson CI
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    (cherry picked from commit 520c8e6ed86ef64f5fd2979a344dee624cb3c8c6)
    Reviewed-on: https://git.eclipse.org/r/99185

--- changelog/org.eclipse.linuxtools.changelog.core/src/org/eclipse/linuxtools/internal/changelog/core/actions/InsertChangeLogKeyHandler.java
+++ changelog/org.eclipse.linuxtools.changelog.core/src/org/eclipse/linuxtools/internal/changelog/core/actions/InsertChangeLogKeyHandler.java
@@ -166,12 +166,13 @@ public class InsertChangeLogKeyHandler extends ChangeLogAction implements
 
     @Override
     public boolean isEnabled() {
-        IEditorReference[] refs = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getEditorReferences();
-        for (int i = 0; i < refs.length; ++i) {
-            IEditorReference ref = refs[i];
-            String id = ref.getId();
-            System.out.println(id);
-        }
+        @SuppressWarnings("unused")
+		IEditorReference[] refs = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getEditorReferences();
+//        for (int i = 0; i < refs.length; ++i) {
+//            IEditorReference ref = refs[i];
+//            String id = ref.getId();
+//            System.out.println(id);
+//        }
         return true;
     }
 
