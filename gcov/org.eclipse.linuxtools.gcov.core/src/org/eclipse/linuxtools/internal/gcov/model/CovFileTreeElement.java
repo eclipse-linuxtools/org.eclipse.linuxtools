7b1da32dfaf4339b2cab52730c2d8328554dcded
Alexander Kurtakov <akurtako@redhat.com>
2018-05-10 19:15:08 +0300

    Update gcov license headers to EPL 2.0.
    Change-Id: I49f7fcaad0609e8fd1e419ab20504e26a13d228d
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/122436
    Tested-by: CI Bot

--- gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/model/CovFileTreeElement.java
+++ gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/model/CovFileTreeElement.java
@@ -1,9 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2009 STMicroelectronics.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009, 2018 STMicroelectronics and others.
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *    Xavier Raynaud <xavier.raynaud@st.com> - initial API and implementation
@@ -13,9 +15,6 @@ package org.eclipse.linuxtools.internal.gcov.model;
 
 public class CovFileTreeElement extends AbstractTreeElement {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = -5017234616535899796L;
 
     public CovFileTreeElement(TreeElement parent,
--- gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/parser/Line.java
+++ gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/parser/Line.java
@@ -1,9 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2009 STMicroelectronics.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009, 2018 STMicroelectronics and others.
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *    Xavier Raynaud <xavier.raynaud@st.com> - initial API and implementation
@@ -15,7 +17,6 @@ import java.io.Serializable;
 
 public class Line implements Serializable{
 
-    /**     */
     private static final long serialVersionUID = 8804878976767948267L;
     private boolean exists = false;
     private long count = 0;
