dcdb889ff8f78e8568e659a21afe719064f32787
Alexander Kurtakov <akurtako@redhat.com>
2018-06-05 13:22:15 +0300

    gcov: Remove unused imports.
    And enable save actions so this doesn't happen in the future.
    
    Change-Id: I3d1673b9038abad3695d922aade569f588daa990
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/123985

--- gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/view/CovView.java
+++ gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/view/CovView.java
@@ -20,13 +20,9 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.eclipse.core.filesystem.URIUtil;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
