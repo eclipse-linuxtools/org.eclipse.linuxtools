cfe8674883bd2da95502225907b3b45e3c7c4b57
Jeff Johnston <jjohnstn@redhat.com>
2019-11-12 15:10:11 -0500

    Bug 552738 - DockerConnectionManager blocks the UI thread at app startup
    - add nohup testing to script-macosx.sh and don't use -i if that is the case
    
    Change-Id: I8da981f490c9b0c2915c9368bb5c66e5d837fe02
    Reviewed-on: https://git.eclipse.org/r/152517
    Tested-by: Linux Tools Bot <linuxtools-bot@eclipse.org>
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>

--- containers/org.eclipse.linuxtools.docker.core/resources/script-macosx.sh
+++ containers/org.eclipse.linuxtools.docker.core/resources/script-macosx.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env sh
-$SHELL -i -l -c "env | grep DOCKER"
+pid=`echo $$`
+if [ `ls -l /proc/$pid/fd | grep -c nohup.out` -eq 0 ]
+  then
+    OPT="-i"
+fi
+$SHELL $OPT -l -c "env | grep DOCKER"
