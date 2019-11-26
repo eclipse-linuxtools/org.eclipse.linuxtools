ea461d73e6388e8aa5186b5cea79bad92ce94fcc
Jeff Johnston <jjohnstn@redhat.com>
2019-11-26 15:30:52 -0500

    Bug 552738 - DockerConnectionManager blocks the UI thread at app startup
    - revert to old script-macosx.sh without -i option specified
    
    Change-Id: I0aabe4351b25a7947af2917919e6326130b4a11a
    Reviewed-on: https://git.eclipse.org/r/153432
    Tested-by: Linux Tools Bot <linuxtools-bot@eclipse.org>
    Reviewed-by: Jeff Johnston <jjohnstn@redhat.com>
    (cherry picked from commit 1c71ef09fc3edc9dbbdb935d788ae7c1428a0dd9)
    Reviewed-on: https://git.eclipse.org/r/153503

--- containers/org.eclipse.linuxtools.docker.core/resources/script-macosx.sh
+++ containers/org.eclipse.linuxtools.docker.core/resources/script-macosx.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env sh
-pid=`echo $$`
-if [ `ls -l /proc/$pid/fd | grep -c nohup.out` -eq 0 ]
-  then
-    OPT="-i"
-fi
-$SHELL $OPT -l -c "env | grep DOCKER"
+$SHELL -l -c "env | grep DOCKER"
