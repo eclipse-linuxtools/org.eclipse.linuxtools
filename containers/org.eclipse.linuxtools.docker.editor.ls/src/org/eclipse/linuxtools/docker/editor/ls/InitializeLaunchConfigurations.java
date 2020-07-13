b78e177ea2352a0a26533a6b0af3cb9ac8ee65ee
Alexander Kurtakov <akurtako@redhat.com>
2020-07-13 22:04:15 +0300

    Use WWD node fragments.
    For the ease of use let's rely on WWD node.js redistributable so users
    don't have to manually install node.js.
    
    Change-Id: I32779335ee3f5c94d733c3fa68d1b593f9206667
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/c/linuxtools/org.eclipse.linuxtools/+/166215
    Tested-by: Linux Tools Bot <linuxtools-bot@eclipse.org>

--- containers/org.eclipse.linuxtools.docker.editor.ls/src/org/eclipse/linuxtools/docker/editor/ls/InitializeLaunchConfigurations.java
+++ containers/org.eclipse.linuxtools.docker.editor.ls/src/org/eclipse/linuxtools/docker/editor/ls/InitializeLaunchConfigurations.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2016 Red Hat Inc. and others.
- * 
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -10,6 +10,7 @@
 package org.eclipse.linuxtools.docker.editor.ls;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
@@ -18,17 +19,23 @@ import java.nio.file.Paths;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.wildwebdeveloper.embedder.node.NodeJSManager;
 
 public class InitializeLaunchConfigurations {
 
 	private static boolean alreadyWarned;
 
 	public static String getNodeJsLocation() {
-		String res = null;
-		String[] command = new String [] { "/bin/bash", "-c", "which node" };
 
+		File nodeJsRuntime = NodeJSManager.getNodeJsLocation();
+		if (nodeJsRuntime != null) {
+			return nodeJsRuntime.getAbsolutePath();
+		}
+
+		String res = null;
+		String[] command = new String[] { "/bin/bash", "-c", "which node" };
 		if (Platform.getOS().equals(Platform.OS_WIN32)) {
-			command = new String [] { "cmd", "/c", "where node" };
+			command = new String[] { "cmd", "/c", "where node" };
 		}
 
 		BufferedReader reader = null;
@@ -47,7 +54,7 @@ public class InitializeLaunchConfigurations {
 
 		if (res != null && Files.exists(Paths.get(res))) {
 			return res;
-		} else if (!alreadyWarned){
+		} else if (!alreadyWarned) {
 			warnNodeJSMissing();
 			alreadyWarned = true;
 		}
@@ -57,10 +64,9 @@ public class InitializeLaunchConfigurations {
 
 	private static void warnNodeJSMissing() {
 		Display.getDefault().asyncExec(() -> {
-			MessageDialog.openWarning(Display.getCurrent().getActiveShell(),
-					"Missing node.js",
-					"Could not find node.js. This will result in editors missing key features.\n" +
-					"Please make sure node.js is installed and that your PATH environement variable contains the location to the `node` executable.");
+			MessageDialog.openWarning(Display.getCurrent().getActiveShell(), "Missing node.js",
+					"Could not find node.js. This will result in editors missing key features.\n"
+							+ "Please make sure node.js is installed and that your PATH environement variable contains the location to the `node` executable.");
 		});
 	}
 
