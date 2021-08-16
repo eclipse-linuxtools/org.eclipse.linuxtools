bcfd82c87fd3e27e80b09dcedde5808f74a2a827
Alexander Kurtakov <akurtako@redhat.com>
2021-08-16 15:42:18 +0300

    rpm: Use method reference
    Change-Id: If9f9ea62dd2fdde0431060d3f9063cd4b7fbd8b9
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/c/linuxtools/org.eclipse.linuxtools/+/184072
    Tested-by: Linux Tools Bot <linuxtools-bot@eclipse.org>

--- rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/listener/CreaterepoResourceChangeListener.java
+++ rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/listener/CreaterepoResourceChangeListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2018 Red Hat Inc. and others.
+ * Copyright (c) 2013, 2021 Red Hat Inc. and others.
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -46,7 +46,7 @@ public class CreaterepoResourceChangeListener implements
         case IResourceChangeEvent.POST_CHANGE:
         case IResourceChangeEvent.PRE_CLOSE:
         case IResourceChangeEvent.PRE_DELETE:
-            PlatformUI.getWorkbench().getDisplay().asyncExec(() -> closeEditors());
+            PlatformUI.getWorkbench().getDisplay().asyncExec(this::closeEditors);
             break;
         }
     }
