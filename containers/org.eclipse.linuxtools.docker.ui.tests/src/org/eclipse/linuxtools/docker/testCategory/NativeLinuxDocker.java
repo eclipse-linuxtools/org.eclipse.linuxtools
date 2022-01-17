891275d91f623a28e22ba36a0a44319bb1195627
Moritz 'Morty' Strübe <moritz.struebe@mathema.de>
2022-01-17 08:42:03 +0100

    DockerJob: Add Testcases

---
+++ containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/docker/testCategory/NativeLinuxDocker.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Mathema.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.docker.testCategory;
+
+/**
+ * A marker to annotate tests that need a native linux ducker to run tests
+ */
+public interface NativeLinuxDocker {
+}
