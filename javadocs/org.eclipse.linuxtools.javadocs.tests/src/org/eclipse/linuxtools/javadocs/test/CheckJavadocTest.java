d0e6ed28b440cb5d36e7a015652a78a153413f16
Александър Куртаков <akurtakov@gmail.com>
2023-08-08 15:17:35 +0300

    Convert javadoc tests to JUnit 5

--- javadocs/org.eclipse.linuxtools.javadocs.tests/src/org/eclipse/linuxtools/javadocs/test/CheckJavadocTest.java
+++ javadocs/org.eclipse.linuxtools.javadocs.tests/src/org/eclipse/linuxtools/javadocs/test/CheckJavadocTest.java
@@ -9,8 +9,10 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.javadocs.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 
@@ -25,9 +27,9 @@ import org.eclipse.linuxtools.internal.javadocs.ui.JavaDocPlugin;
 import org.eclipse.linuxtools.internal.javadocs.ui.JavaDocToc;
 import org.eclipse.linuxtools.internal.javadocs.ui.JavaDocTocProvider;
 import org.eclipse.linuxtools.internal.javadocs.ui.preferences.PreferenceConstants;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class CheckJavadocTest {
+class CheckJavadocTest {
 
 	/**
 	 * Create a valid Toc (topic contribution) using JavaDocTocProvider. A valid Toc
@@ -35,24 +37,24 @@ public class CheckJavadocTest {
 	 * and linkTo.
 	 */
     @Test
-    public void testTocProvider() {
+    void testTocProvider() {
         JavaDocTocProvider provider = new JavaDocTocProvider();
         ITocContribution[] contributions = provider
                 .getTocContributions("en_US"); //$NON-NLS-1$
 
-        assertTrue(contributions.length == 1);
+        assertEquals(1, contributions.length);
         ITocContribution con = contributions[0];
 
         assertEquals(con.getId(),
                 "org.eclipse.linuxtools.javadocs.toc"); //$NON-NLS-1$
 
         assertTrue(con.isPrimary());
-        assertEquals(con.getCategoryId(), null);
+        assertNull(con.getCategoryId());
         assertEquals(con.getLocale(), "en_US"); //$NON-NLS-1$
 
         String[] docs = con.getExtraDocuments();
         assertEquals(docs.length, 0);
-        assertTrue(con.getLinkTo().equals("org.eclipse.linuxtools.javadocs/"));
+        assertEquals(con.getLinkTo(), "org.eclipse.linuxtools.javadocs/");
         assertEquals(con.getContributorId(),
                 "org.eclipse.linuxtools.javadocs"); //$NON-NLS-1$
     }
@@ -67,7 +69,7 @@ public class CheckJavadocTest {
 	 *
 	 */
     @Test
-    public void testHelpTopic() throws IOException {
+    void testHelpTopic() throws IOException {
 
     	// Get current directory and append our test doc directory to it.
     	// Set plugin preferences to read/detect Javadocs from this directory
@@ -84,8 +86,7 @@ public class CheckJavadocTest {
         ITopic[] topics = toc.getTopics();
 
         // Verify we have the test topic, in this case jnr-unixsocket
-        assertTrue(topics.length > 0);
-        assertTrue(topics.length == 1);
+        assertEquals(topics.length, 1);
 
         // Set topics and check labels
         ITopic topicJnr = topics[0];
@@ -96,9 +97,9 @@ public class CheckJavadocTest {
         // Javadocs directories are only one level deep
         ITopic[] subtopicsJnr = topicJnr.getSubtopics();
 
-        assertTrue(subtopicsJnr.length == 0);
+        assertEquals(subtopicsJnr.length, 0);
         IUAElement[] elementsJnr = topicJnr.getChildren();
-        assertTrue(elementsJnr.length == 0);
+        assertEquals(elementsJnr.length, 0);
 
 
         // Get hrefs and check for correctness according to Plugin-ID
