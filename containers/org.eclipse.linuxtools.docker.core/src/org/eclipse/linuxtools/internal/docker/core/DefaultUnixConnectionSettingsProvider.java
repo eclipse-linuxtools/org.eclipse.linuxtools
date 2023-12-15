b00e4a7fc29fe5dc60e040adbccfb59714022077
Александър Куртаков <akurtakov@gmail.com>
2023-12-15 16:49:34 +0200

    Use unixsocket support from Java 16+
    Allows us to drop direct usage of jnr-unixsocket but docker-client still
    uses it internally so not complete removal.

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DefaultUnixConnectionSettingsProvider.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DefaultUnixConnectionSettingsProvider.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2016, 2018 Red Hat.
- * 
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -14,6 +14,9 @@ package org.eclipse.linuxtools.internal.docker.core;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.UnixDomainSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -21,20 +24,18 @@ import java.util.List;
 import org.eclipse.linuxtools.docker.core.IDockerConnectionSettings;
 import org.eclipse.linuxtools.docker.core.IDockerConnectionSettingsProvider;
 
-import jnr.unixsocket.UnixSocketAddress;
-import jnr.unixsocket.UnixSocketChannel;
-
-public class DefaultUnixConnectionSettingsProvider implements IDockerConnectionSettingsProvider {
+public class DefaultUnixConnectionSettingsProvider
+		implements IDockerConnectionSettingsProvider {
 
 	@Override
 	public List<IDockerConnectionSettings> getConnectionSettings() {
-		final File unixSocketFile = new File("/var/run/docker.sock"); //$NON-NLS-1$
+		final Path unixSocketPath = Path.of("/var/run/docker.sock"); //$NON-NLS-1$
+		File unixSocketFile = unixSocketPath.toFile();
 		if (unixSocketFile.exists() && unixSocketFile.canRead()
 				&& unixSocketFile.canWrite()) {
-			final UnixSocketAddress address = new UnixSocketAddress(
-					unixSocketFile);
-			try (final UnixSocketChannel channel = UnixSocketChannel
-					.open(address)) {
+			final UnixDomainSocketAddress address = UnixDomainSocketAddress
+					.of(unixSocketPath);
+			try (final SocketChannel channel = SocketChannel.open(address)) {
 				// assume socket works
 				final UnixSocketConnectionSettings socket = new UnixSocketConnectionSettings(
 						DefaultDockerConnectionSettingsFinder.Defaults.DEFAULT_UNIX_SOCKET_PATH);
