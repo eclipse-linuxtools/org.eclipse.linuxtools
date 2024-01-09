259edbe6b581ea092a31cc2254c591001e76e3ec
Александър Куртаков <akurtakov@gmail.com>
2024-01-09 15:08:36 +0200

    Fix WrongDeallocationResolutionTest with latest valgrind
    Adjust to changes in valgrind output and thus markers generated by the
    plugin.
    In order to make understanding the test simpler - different cases are
    splitted to be handled separately instead of all in one source file.

--- valgrind/org.eclipse.linuxtools.valgrind.ui.tests/resources/wrongDeallocTest/wrongDealloc.cpp
+++ valgrind/org.eclipse.linuxtools.valgrind.ui.tests/resources/wrongDeallocTest/wrongDealloc.cpp
@@ -7,20 +7,7 @@ using namespace std;
 
 int main()
 {
-	char *p1 = (char *)malloc(sizeof(char) * SIZE);
-	delete(p1);
-
-	char* p2 = (char *)malloc(5 * sizeof(char) * SIZE);
-	delete[] p2;
-
-	char *p3 = new char;
-	free(p3);
-
-	char *p4 = new char[5];
-	free(p4);
-
-	char* p5 = new char[5];
-	delete p5;
+	__VALGRIND__
 
 	return 0;
 }
