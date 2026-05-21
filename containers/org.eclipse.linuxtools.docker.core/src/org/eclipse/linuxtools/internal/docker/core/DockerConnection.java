8563916831d2daa7a157892aadec3470e7ca5c1f
Jeff Johnston <jjohnstn@redhat.com>
2026-05-21 11:28:18 -0400

    Create point release 8.22.1 (#576)
    - cherry-pick patch to use read timeout of 0 for Docker client copy
    - bump up versions to 8.22.1
    - remove PTP support from tests as PTP has been removed

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
@@ -572,7 +572,7 @@ public class DockerConnection
 	 */
 	private DockerClient getClientCopy() throws DockerException {
 		try {
-			return dockerClientFactory.getClient(this.connectionSettings);
+			return dockerClientFactory.getClient(this.connectionSettings, true);
 		} catch (org.mandas.docker.client.exceptions.DockerCertificateException e) {
 			throw new DockerException(NLS.bind(Messages.Open_Connection_Failure,
 					this.name, this.getUri()));
